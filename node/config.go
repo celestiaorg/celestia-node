8e4a39cabe6c20bc28101154d370dd93190298af
rene <41963722+renaynay@users.noreply.github.com>
2021-09-03 15:24:15 +0200

    rpc2 -> rpc

--- node/config.go
+++ node/config.go
@@ -2,14 +2,14 @@ package node
 
 import (
 	"github.com/celestiaorg/celestia-node/node/p2p"
-	rpc2 "github.com/celestiaorg/celestia-node/node/rpc"
+	"github.com/celestiaorg/celestia-node/node/rpc"
 )
 
 // Config is main configuration structure for a Node.
 // It combines configuration units for all Node subsystems.
 type Config struct {
 	P2P *p2p.Config
-	RPC *rpc2.Config
+	RPC *rpc.Config
 }
 
 // DefaultConfig provides a default Node Config.
