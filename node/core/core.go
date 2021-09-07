a91a2973a1d7d2ce523e9cc59b66b8620753c8cc
Wondertan <hlibwondertan@gmail.com>
2021-09-07 20:08:00 +0300

    node/core: introduce node subpackage which builds Core Node and Client with configuration

--- node/core/core.go
+++ node/core/core.go
@@ -1,1 +1,40 @@
 package core
+
+import (
+	"go.uber.org/fx"
+
+	"github.com/celestiaorg/celestia-node/core"
+	"github.com/celestiaorg/celestia-node/rpc"
+)
+
+// Config combines all configuration fields for Core subsystem.
+type Config struct {
+	EnableRemote bool
+	Remote       struct {
+		Protocol   string
+		RemoteAddr string
+	}
+	EmbeddedConfig *core.Config
+}
+
+// DefaultConfig returns default configuration for Core subsystem.
+func DefaultConfig() *Config {
+	return &Config{
+		EmbeddedConfig: core.DefaultConfig(),
+		EnableRemote:   false,
+	}
+}
+
+// Components collects all the components and services related to the Core node.
+func Components(cfg *Config) fx.Option {
+	return fx.Options(
+		fx.Provide(rpc.NewClient),
+		fx.Provide(func() (core.Client, error) {
+			if cfg.EnableRemote {
+				return core.NewRemote(cfg.Remote.Protocol, cfg.Remote.RemoteAddr)
+			}
+
+			return core.NewEmbedded(cfg.EmbeddedConfig)
+		}),
+	)
+}
