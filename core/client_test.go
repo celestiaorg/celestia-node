0e29403455b9ea22706bbf75b46b6040914cc4e3
rene <41963722+renaynay@users.noreply.github.com>
2021-09-13 15:50:18 +0200

    core: add tests for remote node (#55)
    * embed node add tests
    
    * clean up tests + rename
    
    * fix doc

--- core/client_test.go
+++ core/client_test.go
@@ -9,14 +9,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestClientLifecycle(t *testing.T) {
-	client := MockClient()
-	err := client.Stop()
-	require.NoError(t, err)
+var newBlockSubscriber = "NewBlock/Events"
+
+func TestEmbeddedClientLifecycle(t *testing.T) {
+	client := MockEmbeddedClient()
+	require.NoError(t, client.Stop())
 }
 
-func TestClient_Status(t *testing.T) {
-	client := MockClient()
+func TestEmbeddedClient_Status(t *testing.T) {
+	client := MockEmbeddedClient()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
@@ -25,17 +26,16 @@ func TestClient_Status(t *testing.T) {
 	require.NoError(t, err)
 	assert.NotNil(t, status)
 
-	err = client.Stop()
-	require.NoError(t, err)
+	require.NoError(t, client.Stop())
 }
 
-func TestClient_StartBlockSubscription_And_GetBlock(t *testing.T) {
-	client := MockClient()
+func TestEmbeddedClient_StartBlockSubscription_And_GetBlock(t *testing.T) {
+	client := MockEmbeddedClient()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
-	eventChan, err := client.Subscribe(ctx, "NewBlock/Events", types.QueryForEvent(types.EventNewBlock).String())
+	eventChan, err := client.Subscribe(ctx, newBlockSubscriber, types.QueryForEvent(types.EventNewBlock).String())
 	require.NoError(t, err)
 
 	for i := 1; i <= 3; i++ {
@@ -47,6 +47,62 @@ func TestClient_StartBlockSubscription_And_GetBlock(t *testing.T) {
 	}
 
 	// unsubscribe to event channel
-	err = client.Unsubscribe(ctx, "NewBlock/Events", types.QueryForEvent(types.EventNewBlock).String())
+	require.NoError(t, client.Unsubscribe(ctx, newBlockSubscriber, types.QueryForEvent(types.EventNewBlock).String()))
+	require.NoError(t, client.Stop())
+}
+
+func TestRemoteClientLifecycle(t *testing.T) {
+	remote, client, err := StartRemoteClient()
 	require.NoError(t, err)
+
+	require.NoError(t, client.Start())
+	require.NoError(t, client.Stop())
+	require.NoError(t, remote.Stop())
+}
+
+func TestRemoteClient_Status(t *testing.T) {
+	remote, client, err := StartRemoteClient()
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	// client must be `started` before performing requests
+	err = client.Start()
+	require.NoError(t, err)
+
+	status, err := client.Status(ctx)
+	require.NoError(t, err)
+	assert.NotNil(t, status)
+
+	require.NoError(t, client.Stop())
+	require.NoError(t, remote.Stop())
+}
+
+func TestRemoteClient_StartBlockSubscription_And_GetBlock(t *testing.T) {
+	remote, client, err := StartRemoteClient()
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	// client must be `started` before subscribing to new block events.
+	err = client.Start()
+	require.NoError(t, err)
+
+	eventChan, err := client.Subscribe(ctx, newBlockSubscriber, types.QueryForEvent(types.EventNewBlock).String())
+	require.NoError(t, err)
+
+	for i := 1; i <= 3; i++ {
+		<-eventChan
+		// check that `Block` works as intended (passing nil to get block at latest height)
+		block, err := client.Block(ctx, nil)
+		require.NoError(t, err)
+		require.Equal(t, int64(i), block.Block.Height)
+	}
+
+	// unsubscribe to event channel
+	require.NoError(t, client.Unsubscribe(ctx, newBlockSubscriber, types.QueryForEvent(types.EventNewBlock).String()))
+	require.NoError(t, client.Stop())
+	require.NoError(t, remote.Stop())
 }
