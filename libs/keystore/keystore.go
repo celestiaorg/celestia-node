45b6d010388b9c96f01a72fb049297bb2c67c4aa
Wondertan <hlibwondertan@gmail.com>
2021-09-17 02:02:50 +0300

    feat(libs/keystore): introduce Keystore interface to manage private keys

---
+++ libs/keystore/keystore.go
@@ -0,0 +1,53 @@
+package keystore
+
+import (
+	"fmt"
+
+	"github.com/multiformats/go-base32"
+)
+
+type (
+	// KeyName represents private key name.
+	KeyName string
+
+	// PrivKey represents private key with arbitrary body.
+	PrivKey struct {
+		Body []byte `json:"body"`
+
+		// TODO(@Wondertan): At later point, it might make sense to have a Type.
+	}
+)
+
+// Keystore is meant to manage private keys.
+type Keystore interface {
+	// Put stores given PrivKey.
+	Put(KeyName, PrivKey) error
+
+	// Get reads PrivKey using given KeyName.
+	Get(KeyName) (PrivKey, error)
+
+	// Delete erases PrivKey using given KeyName.
+	Delete(name KeyName) error
+
+	// List lists all stored key names.
+	List() ([]KeyName, error)
+}
+
+// KeyNameFromBase32 decodes KeyName from Base32 format.
+func KeyNameFromBase32(bs string) (KeyName, error) {
+	name, err := base32.RawStdEncoding.DecodeString(bs)
+	if err != nil {
+		return "", fmt.Errorf("keystore: can't convert base32 string to key name: %w", err)
+	}
+
+	return KeyName(name), nil
+}
+
+// Base32 formats KeyName to Base32 format.
+func (kn KeyName) Base32() string {
+	return base32.RawStdEncoding.EncodeToString([]byte(kn))
+}
+
+func (kn KeyName) String() string {
+	return string(kn)
+}
