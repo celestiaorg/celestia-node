90c913c11b249f75c716bfc06a9ae5d09d0449bc
Wondertan <hlibwondertan@gmail.com>
2021-09-23 03:50:41 +0300

    feat(core): Repository for all on disk data generate by the Core node. On-disk and in-mem versions introduced

---
+++ core/repo_mem.go
@@ -0,0 +1,29 @@
+package core
+
+import "sync"
+
+// NewMemRepository creates in memory implementation of Repository
+func NewMemRepository() Repository {
+	return &memRepository{}
+}
+
+type memRepository struct {
+	cfg *Config
+	lock sync.Mutex
+}
+
+func (m *memRepository) Config() (*Config, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	return m.cfg, nil
+}
+
+func (m *memRepository) PutConfig(cfg *Config) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.cfg = cfg
+	return nil
+}
+
+// not to accidentally forget to update mem implementation when Repository will be extended.
+var _ = (Repository)(&memRepository{})
