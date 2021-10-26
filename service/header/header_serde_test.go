3a8e0f42b657f00f67672c22495d7ce0ff8f2ef7
Wondertan <hlibwondertan@gmail.com>
2021-10-26 23:54:06 +0300

    test(service/header): testing for ExtendedHeader serialization

---
+++ service/header/header_serde_test.go
@@ -0,0 +1,22 @@
+package header
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMarshalUnmarshalExtendedHeader(t *testing.T) {
+	in := RandExtendedHeader(t)
+	data, err := in.MarshalBinary()
+	require.NoError(t, err)
+
+	out := &ExtendedHeader{}
+	err = out.UnmarshalBinary(data)
+	require.NoError(t, err)
+	assert.Equal(t, in.ValidatorSet, out.ValidatorSet)
+	// not the check for equality as time.Time is not serialized exactly 1:1
+	assert.NotZero(t, out.RawHeader)
+	assert.NotNil(t, out.Commit)
+}
---
+++ service/header/testing.go
@@ -0,0 +1,60 @@
+package header
+
+import (
+	mrand "math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	tmrand "github.com/celestiaorg/celestia-core/libs/rand"
+	tmproto "github.com/celestiaorg/celestia-core/proto/tendermint/types"
+	"github.com/celestiaorg/celestia-core/proto/tendermint/version"
+	core "github.com/celestiaorg/celestia-core/types"
+)
+
+func RandExtendedHeader(t *testing.T) *ExtendedHeader {
+	rh := RandRawHeader(t)
+	valSet, vals := core.RandValidatorSet(5, 1)
+	voteSet := core.NewVoteSet(rh.ChainID, rh.Height, 0, tmproto.PrecommitType, valSet)
+	commit, err := core.MakeCommit(RandBlockID(t), rh.Height, 0, voteSet, vals, time.Now())
+	require.NoError(t, err)
+	return &ExtendedHeader{
+		RawHeader:    *rh,
+		Commit:       commit,
+		ValidatorSet: valSet,
+	}
+}
+
+func RandRawHeader(t *testing.T) *RawHeader {
+	randHash := tmrand.Bytes(32)
+	return &RawHeader{
+		Version:            version.Consensus{Block: 11, App: 1},
+		ChainID:            "test",
+		Height:             mrand.Int63(), //nolint:gosec
+		Time:               time.Now(),
+		LastBlockID:        RandBlockID(t),
+		LastCommitHash:     randHash,
+		DataHash:           randHash,
+		ValidatorsHash:     randHash,
+		NextValidatorsHash: randHash,
+		ConsensusHash:      randHash,
+		AppHash:            randHash,
+		LastResultsHash:    randHash,
+		EvidenceHash:       randHash,
+		ProposerAddress:    tmrand.Bytes(20),
+	}
+}
+
+func RandBlockID(t *testing.T) core.BlockID {
+	bid := core.BlockID{
+		Hash: make([]byte, 32),
+		PartSetHeader: core.PartSetHeader{
+			Total: 123,
+			Hash:  make([]byte, 32),
+		},
+	}
+	mrand.Read(bid.Hash)               //nolint:gosec
+	mrand.Read(bid.PartSetHeader.Hash) //nolint:gosec
+	return bid
+}
