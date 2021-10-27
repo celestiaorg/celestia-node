869241ec7b4badb24d68131a583ea255a5264ff5
OrlandoRomo <34588445+OrlandoRomo@users.noreply.github.com>
2021-10-27 16:43:49 -0500

    add version command to celestia command

---
+++ cmd/celestia/version.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	buildTime       string
+	lastCommit      string
+	semanticVersion string
+)
+
+func init() {
+	versionCmd.AddCommand()
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show information about the current binary build",
+	Args:  cobra.NoArgs,
+	Run:   getBinaryInfo,
+}
+
+func getBinaryInfo(cmd *cobra.Command, args []string) {
+	fmt.Printf("Semantic version: %s\n", semanticVersion)
+	fmt.Printf("Commit: %s\n", lastCommit)
+	fmt.Printf("Build Date: %s\n", buildTime)
+	fmt.Printf("System version: %s/%s\n", runtime.GOARCH, runtime.GOOS)
+	fmt.Printf("Golang version: %s\n", runtime.Version())
+}
