aabd4f718cd5c2bd664649a2dff67cd1b8b33ea1
Wondertan <hlibwondertan@gmail.com>
2021-12-23 01:24:08 +0200

    feat(cmd): introduce Enc - an environment for CLI commands

---
+++ cmd/env.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/celestiaorg/celestia-node/node"
+)
+
+type Env struct {
+	ndType    node.Type
+	storePath string
+
+	opts []node.Option
+}
+
+func WithEnv(ctx context.Context) context.Context {
+	_, err := GetEnv(ctx)
+	if err == nil {
+		panic("cmd: only one Env is allowed to be set in a ctx")
+	}
+
+	return context.WithValue(ctx, env, &Env{})
+}
+
+func GetEnv(ctx context.Context) (*Env, error) {
+	env, ok := ctx.Value(env).(*Env)
+	if !ok {
+		return nil, fmt.Errorf("cmd: Env is not set in ctx.Context")
+	}
+
+	return env, nil
+}
+
+func (env *Env) SetNodeType(tp node.Type) {
+	env.ndType = tp
+}
+
+func (env *Env) Options() []node.Option {
+	return env.opts
+}
+
+func (env *Env) addOption(opt node.Option) {
+	env.opts = append(env.opts, opt)
+}
+
+type envKey struct{}
+
+var env = envKey{}
