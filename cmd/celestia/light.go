9100163945962a1fc694a7d225577b5f4f3650ef
Wondertan <hlibwondertan@gmail.com>
2021-12-23 01:42:03 +0200

    refactor(cmd/celestia): bridge and light commands with customizable sets of flags; make them parse flags into Env with persistent pre prun func

--- cmd/celestia/light.go
+++ cmd/celestia/light.go
@@ -3,24 +3,57 @@ package main
 import (
 	"github.com/spf13/cobra"
 
-	"github.com/celestiaorg/celestia-node/cmd"
+	cmdnode "github.com/celestiaorg/celestia-node/cmd"
 	"github.com/celestiaorg/celestia-node/node"
 )
 
 func init() {
 	lightCmd.AddCommand(
-		cmd.Init(storeFlagName, node.Light),
-		cmd.Start(storeFlagName, node.Light),
-	)
-	lightCmd.PersistentFlags().StringP(
-		storeFlagName,
-		storeFlagShort,
-		"~/.celestia-light",
-		"The root/home directory of your Celestial Light Node",
+		cmdnode.Init(
+			cmdnode.NodeFlags(node.Light),
+			cmdnode.P2PFlags(),
+			cmdnode.HeadersFlags(),
+			cmdnode.MiscFlags(),
+		),
+		cmdnode.Start(
+			cmdnode.NodeFlags(node.Light),
+			cmdnode.P2PFlags(),
+			cmdnode.HeadersFlags(),
+			cmdnode.MiscFlags(),
+		),
 	)
 }
 
 var lightCmd = &cobra.Command{
 	Use:  "light [subcommand]",
 	Args: cobra.NoArgs,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		env, err := cmdnode.GetEnv(cmd.Context())
+		if err != nil {
+			return err
+		}
+		env.SetNodeType(node.Light)
+
+		err = cmdnode.ParseNodeFlags(cmd, env)
+		if err != nil {
+			return err
+		}
+
+		err = cmdnode.ParseP2PFlags(cmd, env)
+		if err != nil {
+			return err
+		}
+
+		err = cmdnode.ParseHeadersFlags(cmd, env)
+		if err != nil {
+			return err
+		}
+
+		err = cmdnode.ParseMiscFlags(cmd)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
 }
