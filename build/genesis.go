562d8a49f3a8d7c5994a8d94734e45841e7995b0
Wondertan <hlibwondertan@gmail.com>
2022-01-13 14:02:01 +0200

    feat(build): a utility to get hardcoded genesis hashes for networks

---
+++ build/genesis.go
@@ -0,0 +1,15 @@
+package build
+
+// GenesisFor reports a hash of a genesis block for a given network.
+func GenesisFor(net Network) (string, error) {
+	if err := net.Validate(); err != nil {
+		return "", err
+	}
+
+	return genesisList[net], nil
+}
+
+// NOTE: Everytime we add a new long-running network, it's genesis hash has to be added it here.
+var genesisList = map[Network]string{
+	DevNet: "4632277C441CA6155C4374AC56048CF4CFE3CBB2476E07A548644435980D5E17",
+}
