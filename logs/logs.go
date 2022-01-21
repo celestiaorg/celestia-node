421e62107a5a03e61ed807332bd68425ca4145d7
Wondertan <hlibwondertan@gmail.com>
2022-01-21 22:22:37 +0200

    chore(logs): reduce log verbocity for bs:sess

--- logs/logs.go
+++ logs/logs.go
@@ -4,6 +4,7 @@ import logging "github.com/ipfs/go-log/v2"
 
 func SetAllLoggers(level logging.LogLevel) {
 	logging.SetAllLoggers(level)
+	_ = logging.SetLogLevel("bs:sess", "WARN")
 	_ = logging.SetLogLevel("addrutil", "INFO")
 	_ = logging.SetLogLevel("dht", "ERROR")
 	_ = logging.SetLogLevel("swarm2", "WARN")
