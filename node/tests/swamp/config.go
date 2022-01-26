7442ca23cf2c33a5d746d5e692443c3b0ee6eb62
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-01-26 14:38:52 +0200

    add comments to config.go

--- node/tests/swamp/config.go
+++ node/tests/swamp/config.go
@@ -8,11 +8,14 @@ import (
 	tn "github.com/tendermint/tendermint/config"
 )
 
+// Config struct represents a set of pre-requisite attributes from the test scenario
 type Config struct {
 	App     types.Application
 	CoreCfg *tn.Config
 }
 
+// DefaultConfig creates a KvStore with a block retention of 200
+// In addition, the empty block interval is set to 100ms
 func DefaultConfig() *Config {
 	app := core.CreateKvStore(200)
 	tnCfg := tn.TestConfig()
