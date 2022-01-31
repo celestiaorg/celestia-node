89722fd7281184582982445c5fe22581f940b68d
rene <41963722+renaynay@users.noreply.github.com>
2022-01-31 16:13:50 +0100

    lint

--- core/client_test.go
+++ core/client_test.go
@@ -2,8 +2,9 @@ package core
 
 import (
 	"context"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestEmbeddedClientLifecycle(t *testing.T) {
