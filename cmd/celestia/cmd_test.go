f185ff344d4c19f67c7be726c85090d5073513a4
Josh Bowen <jskybowen@gmail.com>
2022-02-06 22:37:04 -1000

    fix lints

--- cmd/celestia/cmd_test.go
+++ cmd/celestia/cmd_test.go
@@ -7,8 +7,9 @@ import (
 	"os"
 	"testing"
 
-	cmdnode "github.com/celestiaorg/celestia-node/cmd"
 	"github.com/stretchr/testify/require"
+
+	cmdnode "github.com/celestiaorg/celestia-node/cmd"
 )
 
 func TestLight(t *testing.T) {
@@ -17,15 +18,14 @@ func TestLight(t *testing.T) {
 	require.NoError(t, err, "error creating a temporary test directory")
 	testDir, err := os.Getwd()
 	require.NoError(t, err, "error getting the current working directory")
-	defer os.Chdir(testDir)
 	err = os.Chdir(tmpDir)
 	require.NoError(t, err, "error changing to the temporary test directory")
-		
+
 	t.Run("init", func(t *testing.T) {
 		output := &bytes.Buffer{}
 		rootCmd.SetOut(output)
 		rootCmd.SetArgs([]string{
-			"bridge", 
+			"bridge",
 			"--node.store", ".celestia-light",
 			"init",
 		})
@@ -33,23 +33,29 @@ func TestLight(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	// Commented out until a dry-run option can be implemented
-	/*
-	t.Run("start", func(t *testing.T) {
-		output := &bytes.Buffer{}
-		rootCmd.SetOut(output)
-		rootCmd.SetArgs([]string{
-			"light", 
-			"--node.store", ".celestia-light",
-			"start",
-			"--headers.trusted-peer",
-            "/ip4/192.167.10.6/tcp/2121/p2p/12D3KooWL8z3KARAYJcmExhDsGwKbjChKeGaJpFPENyADdxmEHzw",
-            "--headers.trusted-hash", 
-            "54A8B66D2BEF13850D67C8D474E196BD7485FE5A79989E31B17169371B0A9C96",
-		})
-		err := rootCmd.ExecuteContext(cmdnode.WithEnv(context.Background()))
-		require.NoError(t, err)
+	t.Cleanup(func() {
+		if err := os.Chdir(testDir); err != nil {
+			t.Error("error resetting:", err)
+		}
 	})
+
+	// TODO @jbowen93: Commented out until a dry-run option can be implemented
+	/*
+			t.Run("start", func(t *testing.T) {
+				output := &bytes.Buffer{}
+				rootCmd.SetOut(output)
+				rootCmd.SetArgs([]string{
+					"light",
+					"--node.store", ".celestia-light",
+					"start",
+					"--headers.trusted-peer",
+		            "/ip4/192.167.10.6/tcp/2121/p2p/12D3KooWL8z3KARAYJcmExhDsGwKbjChKeGaJpFPENyADdxmEHzw",
+		            "--headers.trusted-hash",
+		            "54A8B66D2BEF13850D67C8D474E196BD7485FE5A79989E31B17169371B0A9C96",
+				})
+				err := rootCmd.ExecuteContext(cmdnode.WithEnv(context.Background()))
+				require.NoError(t, err)
+			})
 	*/
 }
 
@@ -59,7 +65,6 @@ func TestBridge(t *testing.T) {
 	require.NoError(t, err, "error creating a temporary test directory")
 	testDir, err := os.Getwd()
 	require.NoError(t, err, "error getting the current working directory")
-	defer os.Chdir(testDir)
 	err = os.Chdir(tmpDir)
 	require.NoError(t, err, "error changing to the temporary test directory")
 
@@ -67,7 +72,7 @@ func TestBridge(t *testing.T) {
 		output := &bytes.Buffer{}
 		rootCmd.SetOut(output)
 		rootCmd.SetArgs([]string{
-			"bridge", 
+			"bridge",
 			"--node.store", ".celestia-bridge",
 			"init",
 		})
@@ -75,22 +80,28 @@ func TestBridge(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	// Commented out until a dry-run option can be implemented
-	/*
-	t.Run("start", func(t *testing.T) {
-		output := &bytes.Buffer{}
-		rootCmd.SetOut(output)
-		rootCmd.SetArgs([]string{
-			"bridge", 
-			"--node.store", ".celestia-bridge",
-			"start",
-			"--core.remote", 
-            "tcp://192.167.10.2:26657",
-			"--headers.trusted-hash", 
-			"54A8B66D2BEF13850D67C8D474E196BD7485FE5A79989E31B17169371B0A9C96",
-		})
-		err := rootCmd.ExecuteContext(cmdnode.WithEnv(context.Background()))
-		require.NoError(t, err)
+	t.Cleanup(func() {
+		if err := os.Chdir(testDir); err != nil {
+			t.Error("error resetting:", err)
+		}
 	})
+
+	// TODO @jbowen93: Commented out until a dry-run option can be implemented
+	/*
+			t.Run("start", func(t *testing.T) {
+				output := &bytes.Buffer{}
+				rootCmd.SetOut(output)
+				rootCmd.SetArgs([]string{
+					"bridge",
+					"--node.store", ".celestia-bridge",
+					"start",
+					"--core.remote",
+		            "tcp://192.167.10.2:26657",
+					"--headers.trusted-hash",
+					"54A8B66D2BEF13850D67C8D474E196BD7485FE5A79989E31B17169371B0A9C96",
+				})
+				err := rootCmd.ExecuteContext(cmdnode.WithEnv(context.Background()))
+				require.NoError(t, err)
+			})
 	*/
 }
