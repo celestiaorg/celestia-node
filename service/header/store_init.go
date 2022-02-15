40e7859262a824fc3d728ae6f1bc5a49b8e5301a
Hlib Kanunnikov <hlibwondertan@gmail.com>
2022-02-15 18:02:21 +0200

    Update service/header/store_init.go
    Co-authored-by: Ismail Khoffi <Ismail.Khoffi@gmail.com>

--- service/header/store_init.go
+++ service/header/store_init.go
@@ -6,8 +6,8 @@ import (
 	tmbytes "github.com/tendermint/tendermint/libs/bytes"
 )
 
-// InitStore ensures a Store is initialized. If not, it initializes the Store
-// with a header requests using the given hash.
+// InitStore ensures a Store is initialized. If it is not already initialized,
+// it initializes the Store by requesting the header with the given hash.
 func InitStore(ctx context.Context, store Store, ex Exchange, hash tmbytes.HexBytes) error {
 	_, err := store.Head(ctx)
 	switch err {
