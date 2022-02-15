9399b27b4107073d0105c48505c2765068de51c3
rene <41963722+renaynay@users.noreply.github.com>
2022-02-15 13:53:29 +0100

    remove unnecessary type aliases

--- service/state/state.go
+++ service/state/state.go
@@ -1,19 +1,12 @@
 package state
 
 import (
-	sdk_client "github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	coretypes "github.com/tendermint/tendermint/types"
 )
 
-// Account is an alias to the Account interface from Cosmos-SDK.
-type Account = sdk_client.Account
-
 // Balance is an alias to the Coin type from Cosmos-SDK.
 type Balance = sdk.Coin
-
-// Msg is an alias to the Msg interface from Cosmos-SDK. // TODO @renaynay: do we need this?
-type Msg = sdk.Msg
 
 // Tx is an alias to the Tx type from celestia-core.
 type Tx = coretypes.Tx
