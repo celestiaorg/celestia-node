317f26819b1910450bd1f6ea2d4dca5530d1207f
Wondertan <hlibwondertan@gmail.com>
2022-02-21 21:57:26 +0200

    fix(node): start watchdog once only so that in test we don't get an error of watchdog being alread started

--- node/components.go
+++ node/components.go
@@ -2,6 +2,7 @@ package node
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
@@ -53,33 +54,44 @@ func baseComponents(cfg *Config, store Store) fxutil.Option {
 		fxutil.Provide(services.HeaderSyncer),
 		fxutil.ProvideAs(services.P2PSubscriber, new(header.Broadcaster), new(header.Subscriber)),
 		fxutil.Provide(services.HeaderP2PExchangeServer),
-		fxutil.Invoke(invkWatchdog(store.Path())),
+		fxutil.Invoke(invokeWatchdog(store.Path())),
 		p2p.Components(cfg.P2P),
 	)
 }
 
-func invkWatchdog(pprofdir string) func(lc fx.Lifecycle) error {
-	return func(lc fx.Lifecycle) error {
-		// to get watchdog information logged out
-		watchdog.Logger = logWatchdog
-		// these set up heap pprof auto capturing on disk when threshold hit 90% usage
-		watchdog.HeapProfileDir = pprofdir
-		watchdog.HeapProfileMaxCaptures = 10
-		watchdog.HeapProfileThreshold = 0.9
+// invokeWatchdog starts the memory watchdog that helps to prevent some of OOMs by forcing GCing
+// It also collects heap profiles in the given directory when heap grows to more than 90% of memory usage
+func invokeWatchdog(pprofdir string) func(lc fx.Lifecycle) error {
+	return func(lc fx.Lifecycle) (errOut error) {
+		onceWatchdog.Do(func() {
+			// to get watchdog information logged out
+			watchdog.Logger = logWatchdog
+			// these set up heap pprof auto capturing on disk when threshold hit 90% usage
+			watchdog.HeapProfileDir = pprofdir
+			watchdog.HeapProfileMaxCaptures = 10
+			watchdog.HeapProfileThreshold = 0.9
 
-		policy := watchdog.NewWatermarkPolicy(0.50, 0.60, 0.70, 0.85, 0.90, 0.925, 0.95)
-		err, stop := watchdog.SystemDriven(0, time.Second*5, policy)
-		if err != nil {
-			return err
-		}
-		lc.Append(fx.Hook{
-			OnStop: func(context.Context) error {
-				stop()
-				return nil
-			},
+			policy := watchdog.NewWatermarkPolicy(0.50, 0.60, 0.70, 0.85, 0.90, 0.925, 0.95)
+			err, stop := watchdog.SystemDriven(0, time.Second*5, policy)
+			if err != nil {
+				errOut = err
+				return
+			}
+
+			lc.Append(fx.Hook{
+				OnStop: func(context.Context) error {
+					stop()
+					return nil
+				},
+			})
 		})
-		return nil
+		return
 	}
 }
+
+// TODO(@Wondetan): We must start watchdog only once. This is needed for tests where we run multiple instance
+//  of the Node. Ideally, the Node should have some testing options instead, so we can check for it and run without
+//  such utilities but it does not hurt to run one instance of watchdog per test.
+var onceWatchdog = sync.Once{}
 
 var logWatchdog = logging.Logger("watchdog")
