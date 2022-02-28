52cd504955167d73e5f3e61590059baf958fc07c
rene <41963722+renaynay@users.noreply.github.com>
2022-02-28 09:34:36 +0100

    refactor: remove Head method from HeaderGetter interface

--- das/interface.go
+++ das/interface.go
@@ -11,8 +11,6 @@ import (
 // syncing process in order to perform data availability sampling
 // over headers from the past.
 type HeaderGetter interface {
-	// Head returns the ExtendedHeader of the chain head.
-	Head(context.Context) (*header.ExtendedHeader, error)
 	// GetByHeight returns the ExtendedHeader corresponding to the given
 	// block height.
 	GetByHeight(context.Context, uint64) (*header.ExtendedHeader, error)
