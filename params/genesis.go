9bc6992ab797603772abb50c7cdf8d261c07ae1a
Wondertan <hlibwondertan@gmail.com>
2022-03-28 00:38:40 +0200

    feat(params): add new Private network for private/testing setups and env var to enable it

--- params/genesis.go
+++ params/genesis.go
@@ -1,6 +1,8 @@
 package params
 
 // GenesisFor reports a hash of a genesis block for a given network.
+// Genesis is strictly defined and can't be modified.
+// To run a custom genesis private network use CELESTIA_PRIVATE_GENESIS env var.
 func GenesisFor(net Network) (string, error) {
 	if err := net.Validate(); err != nil {
 		return "", err
@@ -11,5 +13,6 @@ func GenesisFor(net Network) (string, error) {
 
 // NOTE: Every time we add a new long-running network, its genesis hash has to be added here.
 var genesisList = map[Network]string{
-	DevNet: "4632277C441CA6155C4374AC56048CF4CFE3CBB2476E07A548644435980D5E17",
+	DevNet:  "4632277C441CA6155C4374AC56048CF4CFE3CBB2476E07A548644435980D5E17",
+	Private: "",
 }
