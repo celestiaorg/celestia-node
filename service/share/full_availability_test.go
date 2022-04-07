c7cbe50d09f2df0d3961f50719162949bf8b1e4f
Wondertan <hlibwondertan@gmail.com>
2022-04-07 18:38:25 +0200

    tests(service/share): add more complex share reconstruction scenario

--- service/share/full_availability_test.go
+++ service/share/full_availability_test.go
@@ -73,8 +73,60 @@ func TestShareAvailable_FullOverLights(t *testing.T) {
 
 	// ensure there is no connection between source and full nodes
 	// so that full reconstructs from the light nodes only
-	net.Disconnect(0, 1)
+	net.Disconnect(source.ID(), full.ID())
 
 	err := full.SharesAvailable(ctx, root)
+	assert.NoError(t, err)
+}
+
+func TestShareAvailable_MultipleFullOverLights(t *testing.T) {
+	const (
+		origSquareSize = 8
+		lightNodes     = 192
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	net := NewTestDAGNet(ctx, t)
+	source, root := net.RandFullNode(origSquareSize)
+
+	full1 := net.FullNode()
+	lights1 := make([]*node, lightNodes/2)
+	for i := 0; i < len(lights1); i++ {
+		light1 := net.LightNode()
+		net.Connect(light1.ID(), source.ID())
+		net.Connect(light1.ID(), full1.ID())
+		lights1[i] = light1
+	}
+
+	full2 := net.FullNode()
+	lights2 := make([]Service, lightNodes/2)
+	for i := 0; i < len(lights2); i++ {
+		light2 := net.LightNode()
+		net.Connect(light2.ID(), source.ID())
+		net.Connect(light2.ID(), full2.ID())
+		lights2[i] = light2
+	}
+
+	for i := 0; i < len(lights1); i++ {
+		err := lights1[i].SharesAvailable(ctx, root)
+		require.NoError(t, err)
+	}
+
+	for i := 0; i < len(lights2); i++ {
+		err := lights2[i].SharesAvailable(ctx, root)
+		require.NoError(t, err)
+	}
+
+	net.Connect(full1.ID(), full2.ID())
+
+	net.Disconnect(full1.ID(), source.ID())
+	net.Disconnect(full2.ID(), source.ID())
+
+	err := full1.SharesAvailable(ctx, root)
+	assert.NoError(t, err)
+
+	err = full2.SharesAvailable(ctx, root)
 	assert.NoError(t, err)
 }
