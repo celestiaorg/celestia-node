343e0b048bc7055aeab4b1cbfeeaba41648978e1
rene <41963722+renaynay@users.noreply.github.com>
2022-04-12 13:57:16 +0200

    doc: fix TODO doc for rpc component construciton

--- node/rpc/component.go
+++ node/rpc/component.go
@@ -4,11 +4,11 @@ import (
 	"go.uber.org/fx"
 )
 
+// RPC constructs a new RPC Server from the given Config.
+// TODO @renaynay @Wondertan: this component is meant to be removed on implementation
+//  of https://github.com/celestiaorg/celestia-node/pull/506.
 func RPC(cfg Config) func(lc fx.Lifecycle) *Server {
 	return func(lc fx.Lifecycle) *Server {
-		// TODO @renaynay @Wondertan: not providing any custom config
-		//  functionality here as this component is meant to be removed on
-		//  implementation of https://github.com/celestiaorg/celestia-node/pull/506.
 		serv := NewServer(cfg)
 		lc.Append(fx.Hook{
 			OnStart: serv.Start,
