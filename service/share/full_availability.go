ba9b19bf0833753aa4a1e008c0d4d8eca38e07ef
Wondertan <hlibwondertan@gmail.com>
2022-04-14 23:38:37 +0200

    feat(service/share): improve availability implemenations * Use ipld.Retriever for FullAvailability and GetShares in the Service * Increase AvailabilityTimeout * Defined global DefaultCodec and use it where possible

--- service/share/full_availability.go
+++ service/share/full_availability.go
@@ -2,30 +2,41 @@ package share
 
 import (
 	"context"
+	"errors"
 
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/celestiaorg/celestia-node/ipld"
-	"github.com/celestiaorg/rsmt2d"
 )
 
 // fullAvailability implements Availability using the full data square
 // recovery technique. It is considered "full" because it is required
 // to download enough shares to fully reconstruct the data square.
 type fullAvailability struct {
-	service format.DAGService
+	rtrv *ipld.Retriever
 }
 
 // NewFullAvailability creates a new full Availability.
-func NewFullAvailability(service format.DAGService) Availability {
+func NewFullAvailability(dag format.DAGService) Availability {
 	return &fullAvailability{
-		service: service,
+		rtrv: ipld.NewRetriever(dag, DefaultCodec()),
 	}
 }
 
 // SharesAvailable reconstructs the data committed to the given Root by requesting
 // enough Shares from the network.
 func (fa *fullAvailability) SharesAvailable(ctx context.Context, root *Root) error {
-	_, err := ipld.RetrieveData(ctx, root, fa.service, rsmt2d.NewRSGF8Codec())
+	ctx, cancel := context.WithTimeout(ctx, AvailabilityTimeout)
+	defer cancel()
+
+	_, err := fa.rtrv.Retrieve(ctx, root)
+	if err != nil {
+		log.Errorw("availability validation failed", "root", root.Hash(), "err", err)
+		if errors.Is(err, format.ErrNotFound) || errors.Is(err, context.DeadlineExceeded) {
+			return ErrNotAvailable
+		}
+
+		return err
+	}
 	return err
 }
