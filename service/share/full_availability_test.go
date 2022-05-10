2e02e42e37568ef8221d4f0a48be1c023105108f
Wondertan <hlibwondertan@gmail.com>
2022-05-10 18:20:50 +0200

    test(share): add reconstrustrution test asserting that Full Node can reconstruct data from light nodes only

--- service/share/full_availability_test.go
+++ service/share/full_availability_test.go
@@ -3,8 +3,13 @@ package share
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/celestiaorg/celestia-node/ipld"
 )
 
 func TestSharesAvailable_Full(t *testing.T) {
@@ -27,5 +32,48 @@ func TestShareAvailableOverMocknet_Full(t *testing.T) {
 	net.ConnectAll()
 
 	err := nd.SharesAvailable(ctx, root)
+	assert.NoError(t, err)
+}
+
+func TestShareAvailable_FullOverLights(t *testing.T) {
+	ipld.RetrieveQuadrantTimeout = time.Millisecond * 200
+	DefaultSampleAmount = 20 // s
+
+	const (
+		origSquareSize = 16 // k
+		lightNodes     = 69 // c
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel()
+
+	net := NewTestDAGNet(ctx, t)
+	source, root := net.RandFullNode(origSquareSize) // make a source node, a.k.a bridge
+	full := net.FullNode()                           // make a full availability service which reconstructs data
+
+	lights := make([]*node, lightNodes)
+	for i := 0; i < len(lights); i++ {
+		light := net.LightNode()
+		net.Connect(light.ID(), full.ID())
+		net.Connect(light.ID(), source.ID())
+		lights[i] = light
+	}
+
+	errg, errCtx := errgroup.WithContext(ctx)
+	for i := 0; i < len(lights); i++ {
+		i := i
+		errg.Go(func() error {
+			return lights[i].SharesAvailable(errCtx, root)
+		})
+	}
+
+	err := errg.Wait()
+	require.NoError(t, err)
+
+	// ensure there is no connection between source and full nodes
+	// so that full reconstructs from the light nodes only
+	net.Disconnect(source.ID(), full.ID())
+
+	err = full.SharesAvailable(ctx, root)
 	assert.NoError(t, err)
 }
