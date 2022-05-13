2b5af0b59cde0c365430a1a435f048df1a10d7ba
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2022-05-13 16:53:18 +0300

    chore: remove header maker from testing

--- node/testing.go
+++ node/testing.go
@@ -1,19 +1,11 @@
 package node
 
 import (
-	"context"
 	"testing"
 
-	format "github.com/ipfs/go-ipld-format"
 	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/pkg/consts"
-	"github.com/tendermint/tendermint/pkg/da"
-	"github.com/tendermint/tendermint/types"
-	"go.uber.org/fx"
 
 	"github.com/celestiaorg/celestia-node/core"
-	"github.com/celestiaorg/celestia-node/header"
-	"github.com/celestiaorg/celestia-node/ipld"
 	"github.com/celestiaorg/celestia-node/params"
 )
 
@@ -33,42 +25,4 @@ func TestNode(t *testing.T, tp Type, opts ...Option) *Node {
 	nd, err := New(tp, store, opts...)
 	require.NoError(t, err)
 	return nd
-}
-
-// WithFaultMaker is used to enable test cases with invalid blocks
-func WithFaultMaker(faultHeight int64) Option {
-	log.Info("assign fault maker to get incorrect block at height ", faultHeight)
-	return func(sets *settings) {
-		sets.opts = append(sets.opts, fx.Replace(func(ctx context.Context,
-			b *types.Block,
-			comm *types.Commit,
-			vals *types.ValidatorSet,
-			dag format.NodeAdder) (*header.ExtendedHeader, error) {
-			var dah da.DataAvailabilityHeader
-
-			namespacedShares, _ := b.Data.ComputeShares()
-
-			extended, err := ipld.AddShares(ctx, namespacedShares.RawShares(), dag)
-			if err != nil {
-				return nil, err
-			}
-
-			if b.Height == faultHeight {
-				t := testing.T{}
-				size := len(namespacedShares.RawShares()) * 2
-				extended = ipld.RandEDS(&t, size)
-				shares := ipld.ExtractEDS(extended)
-				copy(shares[0][consts.NamespaceSize:], shares[1][consts.NamespaceSize:])
-				dah = ipld.CreateDAH(ctx, shares, uint64(extended.Width()), dag)
-			}
-			dah = da.NewDataAvailabilityHeader(extended)
-			eh := &header.ExtendedHeader{
-				RawHeader:    b.Header,
-				DAH:          &dah,
-				Commit:       comm,
-				ValidatorSet: vals,
-			}
-			return eh, nil
-		}))
-	}
 }
