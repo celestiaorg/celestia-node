c436d6dac1166181f8933c09c69b6ee8311590c4
Tomasz Zdybał <tomek@zdybal.lap.pl>
2022-05-15 20:17:23 +0200

    feat(service/rcp): unified error handling (#710)
    * error handling is unified for all error types and endpoints
    * duplicated code is extracted into function
    * errors are returned as valid JSON, not plain text
    
    Co-authored-by: rene <41963722+renaynay@users.noreply.github.com>

--- service/rpc/share.go
+++ service/rpc/share.go
@@ -35,16 +35,14 @@ func (h *Handler) handleSharesByNamespaceRequest(w http.ResponseWriter, r *http.
 	if strHeight, ok := vars[heightKey]; ok {
 		height, err = strconv.Atoi(strHeight)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			log.Errorw("serving request", "endpoint", namespacedSharesEndpoint, "err", err)
+			writeError(w, http.StatusBadRequest, namespacedSharesEndpoint, err)
 			return
 		}
 	}
 	hexNID := vars[nIDKey]
 	nID, err := hex.DecodeString(hexNID)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		log.Errorw("serving request", "endpoint", namespacedSharesEndpoint, "err", err)
+		writeError(w, http.StatusBadRequest, namespacedSharesEndpoint, err)
 		return
 	}
 	// get header
@@ -56,19 +54,13 @@ func (h *Handler) handleSharesByNamespaceRequest(w http.ResponseWriter, r *http.
 		header, err = h.header.GetByHeight(r.Context(), uint64(height))
 	}
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Errorw("serving request", "endpoint", namespacedSharesEndpoint, "err", err)
+		writeError(w, http.StatusInternalServerError, namespacedSharesEndpoint, err)
 		return
 	}
 	// perform request
 	shares, err := h.share.GetSharesByNamespace(r.Context(), header.DAH, nID)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, werr := w.Write([]byte(err.Error()))
-		if werr != nil {
-			log.Errorw("writing response", "endpoint", namespacedSharesEndpoint, "err", werr)
-		}
-		log.Errorw("serving request", "endpoint", namespacedSharesEndpoint, "err", err)
+		writeError(w, http.StatusInternalServerError, namespacedSharesEndpoint, err)
 		return
 	}
 	resp, err := json.Marshal(&NamespacedSharesResponse{
@@ -76,8 +68,7 @@ func (h *Handler) handleSharesByNamespaceRequest(w http.ResponseWriter, r *http.
 		Height: uint64(header.Height),
 	})
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Errorw("serving request", "endpoint", namespacedSharesEndpoint, "err", err)
+		writeError(w, http.StatusInternalServerError, namespacedSharesEndpoint, err)
 		return
 	}
 	_, err = w.Write(resp)
