26bd82ac85859f4a6fcbdb24f1f9465fb1eb3f39
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-05-23 14:58:37 +0200

    swamp: change eventBlock query

--- node/tests/swamp/config.go
+++ node/tests/swamp/config.go
@@ -3,10 +3,9 @@ package swamp
 import (
 	"time"
 
+	"github.com/celestiaorg/celestia-node/core"
 	"github.com/tendermint/tendermint/abci/types"
 	tn "github.com/tendermint/tendermint/config"
-
-	"github.com/celestiaorg/celestia-node/core"
 	rpctest "github.com/tendermint/tendermint/rpc/test"
 )
 
