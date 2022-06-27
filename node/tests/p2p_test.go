a9e66a915ce331b6069687be8f9114ce051f4a74
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2022-06-27 15:08:03 +0300

    swamp: add test for conn gater

--- node/tests/p2p_test.go
+++ node/tests/p2p_test.go
@@ -37,3 +37,30 @@ func TestUseBridgeNodeAsBootstraper(t *testing.T) {
 		assert.True(t, nodes[index].Host.Network().Connectedness(addr.ID) == network.Connected)
 	}
 }
+
+/*
+ Test-Case: Add peer to blacklist
+ Steps:
+ 1. Create a Full Node(BN)
+ 2. Start a FN
+ 3. Create a Light Node(LN)
+ 5. Start a LN
+ 6. Explicitly block FN id by LN
+ 7. Check FN is allowed to dial with LN
+ 8. Check LN is not allowed to dial with FN
+*/
+func TestAddPeerToBlackList(t *testing.T) {
+	sw := swamp.NewSwamp(t)
+	full := sw.NewFullNode()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	t.Cleanup(cancel)
+	require.NoError(t, full.Start(ctx))
+
+	addr := host.InfoFromHost(full.Host)
+	light := sw.NewLightNode()
+	require.NoError(t, light.Start(ctx))
+	require.NoError(t, light.ConnGater.BlockPeer(addr.ID))
+
+	require.True(t, full.ConnGater.InterceptPeerDial(host.InfoFromHost(light.Host).ID))
+	require.False(t, light.ConnGater.InterceptPeerDial(addr.ID))
+}
