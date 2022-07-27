2d56c7d5b932b10a4ae4a1755b6765284c352da8
rene <41963722+renaynay@users.noreply.github.com>
2022-07-27 15:51:19 +0200

    service/state: Implement balance verification against `AppHash` (#911)
    * refactor: extract HeaderGetter interface from daser into header pkg
    
    * refactor: delete interface.go file from das pkg
    
    * refactor: Store interface wraps getter methods
    
    * chore: rebase fixes
    
    * feat: add `/verified_balance` endpoint
    
    * chore: lint
    
    * refactor: dedup handleBalanceRequest
    
    * docs: update state ADR
    
    * refactor: consolidate verified_balance and balance endpoints --> verify balance by default
    
    * chore: replace TODOs with PR refs
    
    * refactor: apply john review comments
    
    * refactor: extract sdk error func into helpers file, rename import aliases
    
    * doc: update ADR header.Store -> header.Getter

--- node/state/state.go
+++ node/state/state.go
@@ -7,7 +7,9 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/celestiaorg/celestia-node/fraud"
+	"github.com/celestiaorg/celestia-node/header"
 	"github.com/celestiaorg/celestia-node/libs/fxutil"
+	"github.com/celestiaorg/celestia-node/node/core"
 	"github.com/celestiaorg/celestia-node/node/key"
 	"github.com/celestiaorg/celestia-node/node/services"
 	"github.com/celestiaorg/celestia-node/service/state"
@@ -17,17 +19,23 @@ var log = logging.Logger("state-access-constructor")
 
 // Components provides all components necessary to construct the
 // state service.
-func Components(coreIP, grpcPort string, cfg key.Config) fx.Option {
+func Components(coreCfg core.Config, keyCfg key.Config) fx.Option {
 	return fx.Options(
-		fx.Provide(Keyring(cfg)),
-		fx.Provide(CoreAccessor(coreIP, grpcPort)),
+		fx.Provide(Keyring(keyCfg)),
+		fx.Provide(CoreAccessor(coreCfg.IP, coreCfg.RPCPort, coreCfg.GRPCPort)),
 		fx.Provide(Service),
 	)
 }
 
 // Service constructs a new state.Service.
-func Service(ctx context.Context, lc fx.Lifecycle, accessor state.Accessor, fservice fraud.Service) *state.Service {
-	serv := state.NewService(accessor)
+func Service(
+	ctx context.Context,
+	lc fx.Lifecycle,
+	accessor state.Accessor,
+	store header.Store,
+	fservice fraud.Service,
+) *state.Service {
+	serv := state.NewService(accessor, store)
 	lifecycleCtx := fxutil.WithLifecycle(ctx, lc)
 	lc.Append(fx.Hook{
 		OnStart: func(startCtx context.Context) error {
