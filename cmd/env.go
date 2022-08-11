0066d5dfaf8c2374a84dc8ae77f4b8cb6c97dca1
Ryan <ryanford@poluglottos.com>
2022-08-11 21:09:53 +0200

    fix: no strings as keys for context

--- cmd/env.go
+++ cmd/env.go
@@ -5,42 +5,37 @@ import (
 	"github.com/celestiaorg/celestia-node/node"
 )
 
-// Env is an environment for CLI commands.
-// It can be used to:
-// 1. Propagate values from parent to child commands.
-// 2. To group common logic that multiple commands rely on.
-// Usage can be extended.
-// TODO(@Wondertan): We should move to using context only instead.
-//  Env, in fact, only keeps some additional fields which should be
-//  kept in the context directly using WithValue (#965)
-
 // NodeType reads the node.Type from the context.
 func NodeType(ctx context.Context) node.Type {
-	return ctx.Value("NodeType").(node.Type)
+	return ctx.Value(nodeTypeKey{}).(node.Type)
 }
 
 // StorePath reads the store path from the context.
 func StorePath(ctx context.Context) string {
-	return ctx.Value("StorePath").(string)
+	return ctx.Value(storePathKey{}).(string)
 }
 
 // SetNodeType sets Node Type to the Env.
 func SetNodeType(ctx context.Context, tp node.Type) context.Context {
-	return context.WithValue(ctx, "NodeType", tp)
+	return context.WithValue(ctx, nodeTypeKey{}, tp)
 }
 
 // SetStorePath sets Store Path to the Env.
 func SetStorePath(ctx context.Context, storePath string) context.Context {
-	return context.WithValue(ctx, "StorePath", storePath)
+	return context.WithValue(ctx, storePathKey{}, storePath)
 }
 
 // Options returns Node Options parsed from Environment(Flags, ENV vars, etc)
 func Options(ctx context.Context) []node.Option {
-	return ctx.Value("Options").([]node.Option)
+	return ctx.Value(optionsKey{}).([]node.Option)
 }
 
 // AddOptions add new options to Env.
 func AddOptions(ctx context.Context, opts ...node.Option) context.Context {
 	options := Options(ctx)
-	return context.WithValue(ctx, "Options", append(options, opts...))
+	return context.WithValue(ctx, optionsKey{}, append(options, opts...))
 }
+
+type optionsKey struct{}
+type storePathKey struct{}
+type nodeTypeKey struct{}
