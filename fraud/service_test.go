f650de4cc71bc2277bfd26cf49b6f6d5cc42616d
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2022-08-26 12:20:45 +0300

    chore: make re-gossiping test great again

--- fraud/service_test.go
+++ fraud/service_test.go
@@ -14,6 +14,8 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	pubsubpb "github.com/libp2p/go-libp2p-pubsub/pb"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+
+	"github.com/celestiaorg/celestia-node/header"
 )
 
 func TestService_Subscribe(t *testing.T) {
@@ -102,6 +104,82 @@ func TestService_processIncoming(t *testing.T) {
 		res := fserviceA.processIncoming(ctx, test.proof.Type(), net.Hosts()[1].ID(), msg)
 		require.True(t, res == test.validationResult)
 	}
+}
+
+func TestService_ReGossiping(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	t.Cleanup(cancel)
+	// create mock network
+	net, err := mocknet.FullMeshLinked(3)
+	require.NoError(t, err)
+
+	// create first fraud service that will broadcast incorrect Fraud Proof
+	pserviceA, _ := createServiceWithHost(ctx, t, net.Hosts()[0])
+	require.NoError(t, err)
+	serviceA := pserviceA.(*service)
+
+	// create pub sub in order to listen for Fraud Proof
+	psB, err := pubsub.NewGossipSub(ctx, net.Hosts()[1], // -> B
+		pubsub.WithMessageSignaturePolicy(pubsub.StrictNoSign))
+	require.NoError(t, err)
+	// create second service that will receive and validate Fraud Proof
+	pserviceB := NewService(
+		psB,
+		func(ctx context.Context, u uint64) (*header.ExtendedHeader, error) {
+			return &header.ExtendedHeader{}, nil
+		},
+		sync.MutexWrap(datastore.NewMapDatastore()),
+	)
+	addrB := host.InfoFromHost(net.Hosts()[1]) // -> B
+
+	// create pub sub in order to listen for Fraud Proof
+	psC, err := pubsub.NewGossipSub(ctx, net.Hosts()[2], // -> C
+		pubsub.WithMessageSignaturePolicy(pubsub.StrictNoSign))
+	require.NoError(t, err)
+	pserviceC := NewService(
+		psC,
+		func(ctx context.Context, u uint64) (*header.ExtendedHeader, error) {
+			return &header.ExtendedHeader{}, nil
+		},
+		sync.MutexWrap(datastore.NewMapDatastore()),
+	)
+
+	// establish connections
+	// connect peers: A -> B -> C, so A and C are not connected to each other
+	require.NoError(t, net.Hosts()[0].Connect(ctx, *addrB)) // host[0] is A
+	require.NoError(t, net.Hosts()[2].Connect(ctx, *addrB)) // host[2] is C
+
+	befp := newValidProof()
+	bin, err := befp.MarshalBinary()
+	require.NoError(t, err)
+
+	subsA, err := pserviceA.Subscribe(mockProofType)
+	require.NoError(t, err)
+	defer subsA.Cancel()
+
+	subsB, err := pserviceB.Subscribe(mockProofType)
+	require.NoError(t, err)
+	defer subsB.Cancel()
+
+	subsC, err := pserviceC.Subscribe(mockProofType)
+	require.NoError(t, err)
+	defer subsC.Cancel()
+	// we cannot avoid sleep because it helps to avoid flakiness
+	time.Sleep(time.Millisecond * 100)
+
+	err = serviceA.topics[mockProofType].Publish(ctx, bin, pubsub.WithReadiness(pubsub.MinTopicSize(1)))
+	require.NoError(t, err)
+
+	newCtx, cancel := context.WithTimeout(ctx, time.Millisecond*100)
+	t.Cleanup(cancel)
+
+	_, err = subsB.Proof(newCtx)
+	require.NoError(t, err)
+
+	_, err = subsC.Proof(ctx)
+	require.NoError(t, err)
+	// we cannot avoid sleep because it helps to avoid flakiness
+	time.Sleep(time.Millisecond * 100)
 }
 
 func TestService_Get(t *testing.T) {
