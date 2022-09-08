c4d10e33158ad2dc747244aa1f92927a4656b704
Ryan <ryanford@poluglottos.com>
2022-09-08 09:06:20 +0200

    feat: writing error out as json for API friendliness

--- service/rpc/util.go
+++ service/rpc/util.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 )
 
+type JSONError struct {
+	Err string `json:"error"`
+}
+
 func writeError(w http.ResponseWriter, statusCode int, endpoint string, err error) {
 	log.Errorw("serving request", "endpoint", endpoint, "err", err)
 
 	w.WriteHeader(statusCode)
-	errBody, jerr := json.Marshal(err.Error())
+	errBody, jerr := json.Marshal(JSONError{Err: err.Error()})
 	if jerr != nil {
 		log.Errorw("serializing error", "endpoint", endpoint, "err", jerr)
 		return
