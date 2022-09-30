f5a23ce9a109db1671cece643bbd544c50cf6e10
evan-forbes <evan.samuel.forbes@gmail.com>
2022-09-29 23:45:53 -0500

    fix: message submission functions to work as before

--- nodebuilder/tests/swamp/swamp_tx.go
+++ nodebuilder/tests/swamp/swamp_tx.go
@@ -6,19 +6,22 @@ import (
 	"time"
 
 	"github.com/celestiaorg/celestia-app/testutil/testnode"
+	"github.com/celestiaorg/celestia-app/x/payment"
+	paytypes "github.com/celestiaorg/celestia-app/x/payment/types"
+	"github.com/tendermint/tendermint/pkg/consts"
 )
 
 // SubmitData submits given data in the block.
 // TODO(@Wondertan): This must be a real PFD using celestia-app, once we can run App in the Swamp.
 func (s *Swamp) SubmitData(ctx context.Context, data []byte) error {
-	result, err := s.ClientContext.Client.BroadcastTxSync(ctx, append([]byte("key="), data...))
+	signer := paytypes.NewKeyringSigner(s.ClientContext.Keyring, s.accounts[0], s.ClientContext.ChainID)
+	resp, err := payment.SubmitPayForData(ctx, signer, s.ClientContext.GRPCClient, data[:consts.NamespaceSize], data, 10000000000)
 	if err != nil {
 		return err
 	}
-	if result.Code != 0 {
-		return fmt.Errorf("invalid status code: %d", result.Code)
+	if resp.Code != 0 {
+		return fmt.Errorf("invalid status code: %d", resp.Code)
 	}
-
 	return nil
 }
 
@@ -26,7 +29,15 @@ func (s *Swamp) FillBlocks(ctx context.Context, bsize, blocks int) error {
 	btime := s.comps.CoreCfg.Consensus.CreateEmptyBlocksInterval
 	timer := time.NewTimer(btime)
 	defer timer.Stop()
-
-	_, err := testnode.FillBlock(s.ClientContext, bsize, s.accounts)
-	return err
+	for i := 0; i < blocks; i++ {
+		_, err := testnode.FillBlock(s.ClientContext, bsize, s.accounts)
+		if err != nil {
+			return err
+		}
+		err = testnode.WaitForNextBlock(s.ClientContext)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
