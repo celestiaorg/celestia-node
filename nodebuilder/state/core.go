15a5eaafba8d1c776d8f2ebe3a10fd469289e0e4
Ryan <ryanford@poluglottos.com>
2022-10-05 13:15:46 +0200

    fix(state): returning CoreAccessor from nodebuilder for state.WithMetrics (#1200)
    #1083 introduced `state.WithMetrics` that takes a `CoreAccessor` instead
    of the `Module`, so the DI currently fails to build. This fixes the DI
    for nodes running metrics

--- nodebuilder/state/core.go
+++ nodebuilder/state/core.go
@@ -1,15 +1,7 @@
 package state
 
 import (
-	"context"
-
-	"go.uber.org/fx"
-
 	apptypes "github.com/celestiaorg/celestia-app/x/payment/types"
-	"github.com/celestiaorg/celestia-node/fraud"
-	"github.com/celestiaorg/celestia-node/libs/fxutil"
-	fraudServ "github.com/celestiaorg/celestia-node/nodebuilder/fraud"
-
 	"github.com/celestiaorg/celestia-node/header/sync"
 	"github.com/celestiaorg/celestia-node/nodebuilder/core"
 	"github.com/celestiaorg/celestia-node/state"
@@ -18,21 +10,9 @@ import (
 // CoreAccessor constructs a new instance of state.Module over
 // a celestia-core connection.
 func CoreAccessor(
-	lc fx.Lifecycle,
-	fService fraudServ.Module,
 	corecfg core.Config,
 	signer *apptypes.KeyringSigner,
 	sync *sync.Syncer,
-) Module {
-	service := state.NewCoreAccessor(signer, sync, corecfg.IP, corecfg.RPCPort, corecfg.GRPCPort)
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			lifecycleCtx := fxutil.WithLifecycle(ctx, lc)
-			return fraudServ.Lifecycle(ctx, lifecycleCtx, fraud.BadEncoding, fService, service.Start, service.Stop)
-		},
-		OnStop: func(ctx context.Context) error {
-			return service.Stop(ctx)
-		},
-	})
-	return service
+) *state.CoreAccessor {
+	return state.NewCoreAccessor(signer, sync, corecfg.IP, corecfg.RPCPort, corecfg.GRPCPort)
 }
