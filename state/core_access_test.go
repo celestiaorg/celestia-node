1584502451352613ad9e77278c9a67147764af17
rene <41963722+renaynay@users.noreply.github.com>
2022-10-19 18:02:58 +0200

    test(state): add test for accessor lifecycle to ensure top level context does not affect accessor lifecycle management

---
+++ state/core_access_test.go
@@ -0,0 +1,33 @@
+package state
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLifecycle(t *testing.T) {
+	ca := NewCoreAccessor(nil, nil, "", "", "")
+	ctx, cancel := context.WithCancel(context.Background())
+	// start the accessor
+	err := ca.Start(ctx)
+	require.NoError(t, err)
+	// ensure accessor isn't stopped
+	require.False(t, ca.IsStopped())
+	// cancel the top level context (this should not affect the lifecycle of the
+	// accessor as it should manage its own internal context)
+	cancel()
+	// ensure accessor was unaffected by top-level context cancellation
+	require.False(t, ca.IsStopped())
+	// stop the accessor
+	stopCtx, stopCancel := context.WithCancel(context.Background())
+	t.Cleanup(stopCancel)
+	err = ca.Stop(stopCtx)
+	require.NoError(t, err)
+	// ensure accessor is stopped
+	require.True(t, ca.IsStopped())
+	// ensure that stopping the accessor again does not return an error
+	err = ca.Stop(stopCtx)
+	require.NoError(t, err)
+}
