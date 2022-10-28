a2ed6ef0a6ef5f39f8408326a0d728b01f38626a
Ryan <ryanford@poluglottos.com>
2022-10-28 14:55:30 +0200

    feat: custom JSON marshal/unmarshal for ExtendedHeader wrapping PubKey w/ amino codec

--- header/serde_test.go
+++ header/serde_test.go
@@ -9,13 +9,29 @@ import (
 
 func TestMarshalUnmarshalExtendedHeader(t *testing.T) {
 	in := RandExtendedHeader(t)
-	data, err := in.MarshalBinary()
+	binaryData, err := in.MarshalBinary()
 	require.NoError(t, err)
 
 	out := &ExtendedHeader{}
-	err = out.UnmarshalBinary(data)
+	err = out.UnmarshalBinary(binaryData)
 	require.NoError(t, err)
-	assert.Equal(t, in.ValidatorSet, out.ValidatorSet)
+	equalExtendedHeader(t, in, out)
+
+	// A custom JSON marshal/unmarshal is necessary which wraps the ValidatorSet with amino
+	// encoding, to be able to marshal the crypto.PubKey type back from JSON.
+	jsonData, err := in.MarshalJSON()
+	require.NoError(t, err)
+
+	out = &ExtendedHeader{}
+	err = out.UnmarshalJSON(jsonData)
+	require.NoError(t, err)
+	equalExtendedHeader(t, in, out)
+}
+
+func equalExtendedHeader(t *testing.T, in, out *ExtendedHeader) {
+	// ValidatorSet.totalVotingPower is not set (is a cached value that can be recomputed client side)
+	assert.Equal(t, in.ValidatorSet.Validators, out.ValidatorSet.Validators)
+	assert.Equal(t, in.ValidatorSet.Proposer, out.ValidatorSet.Proposer)
 	assert.True(t, in.DAH.Equals(out.DAH))
 	// not the check for equality as time.Time is not serialized exactly 1:1
 	assert.NotZero(t, out.RawHeader)
