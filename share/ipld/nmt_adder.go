ab201bfd7131eaaeb006e8f4b707133c9f0284dd
Vlad <vlad@celestia.org>
2022-11-02 17:01:47 +0300

    protect ipld.Batch with mutex inside NmtNodeAdder

--- share/ipld/nmt_adder.go
+++ share/ipld/nmt_adder.go
@@ -2,6 +2,8 @@ package ipld
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
@@ -12,6 +14,7 @@ import (
 // NmtNodeAdder adds ipld.Nodes to the underlying ipld.Batch if it is inserted
 // into a nmt tree.
 type NmtNodeAdder struct {
+	lock   sync.Mutex
 	ctx    context.Context
 	add    *ipld.Batch
 	leaves *cid.Set
@@ -32,13 +35,18 @@ func NewNmtNodeAdder(ctx context.Context, bs blockservice.BlockService, opts ...
 // Visit is a NodeVisitor that can be used during the creation of a new NMT to
 // create and add ipld.Nodes to the Batch while computing the root of the NMT.
 func (n *NmtNodeAdder) Visit(hash []byte, children ...[]byte) {
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
 	if n.err != nil {
 		return // protect from further visits if there is an error
 	}
 	id := MustCidFromNamespacedSha256(hash)
 	switch len(children) {
 	case 1:
-		n.err = n.add.Add(n.ctx, newNMTNode(id, children[0]))
+		if n.leaves.Visit(id) {
+			n.err = n.add.Add(n.ctx, newNMTNode(id, children[0]))
+		}
 	case 2:
 		n.err = n.add.Add(n.ctx, newNMTNode(id, append(children[0], children[1]...)))
 	default:
@@ -48,6 +56,9 @@ func (n *NmtNodeAdder) Visit(hash []byte, children ...[]byte) {
 
 // VisitInnerNodes is a NodeVisitor that does not store leaf nodes to the blockservice.
 func (n *NmtNodeAdder) VisitInnerNodes(hash []byte, children ...[]byte) {
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
 	if n.err != nil {
 		return // protect from further visits if there is an error
 	}
@@ -65,11 +76,18 @@ func (n *NmtNodeAdder) VisitInnerNodes(hash []byte, children ...[]byte) {
 
 // Commit checks for errors happened during Visit and if absent commits data to inner Batch.
 func (n *NmtNodeAdder) Commit() error {
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
 	if n.err != nil {
-		return n.err
+		return fmt.Errorf("before commit: %w", n.err)
 	}
 
-	return n.add.Commit()
+	n.err = n.add.Commit()
+	if n.err != nil {
+		return fmt.Errorf("after commit: %w", n.err)
+	}
+	return nil
 }
 
 // MaxSizeBatchOption sets the maximum amount of buffered data before writing
