6cd3149bc0905862449bdcbf3bc4c83d53113572
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-11-15 18:41:32 +0100

    chore!(nodebuilder/p2p): change genesis hash for upgraded arabica-2

--- nodebuilder/p2p/genesis.go
+++ nodebuilder/p2p/genesis.go
@@ -21,7 +21,7 @@ func GenesisFor(net Network) (string, error) {
 
 // NOTE: Every time we add a new long-running network, its genesis hash has to be added here.
 var genesisList = map[Network]string{
-	Arabica: "FAAA4B939173B4F8567E812FDC570FF186355326879017A611C58267A5D6EF17",
+	Arabica: "04EE55B212745B88F29943D7B9528C415473A211F12EEF6E9333EF32E4DEAF3C",
 	Mamaki:  "41BBFD05779719E826C4D68C4CCBBC84B2B761EB52BC04CFDE0FF8603C9AA3CA",
 	Private: "",
 }
