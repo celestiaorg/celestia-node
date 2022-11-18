559e409fe113294ce571ca5d440dd4cb63bfb71a
Wondertan <hlibwondertan@gmail.com>
2022-11-18 18:32:04 +0100

    feat(module/das): add WaitCatchUp method to the module
    The #1380 made a blocking helper to wait until DASer catching up is done. To use it for tests and further, we have to expose it on the Module either.

--- nodebuilder/das/daser.go
+++ nodebuilder/das/daser.go
@@ -14,12 +14,18 @@ import (
 
 var _ Module = (*daserStub)(nil)
 
+var errStub = fmt.Errorf("module/das: stubbed: dasing is not available on bridge nodes")
+
 // daserStub is a stub implementation of the DASer that is used on bridge nodes, so that we can
 // provide a friendlier error when users try to access the daser over the API.
 type daserStub struct{}
 
 func (d daserStub) SamplingStats(context.Context) (das.SamplingStats, error) {
-	return das.SamplingStats{}, fmt.Errorf("moddas: dasing is not available on bridge nodes")
+	return das.SamplingStats{}, errStub
+}
+
+func (d daserStub) WaitCatchUp(context.Context) error {
+	return errStub
 }
 
 func newDaserStub() Module {
