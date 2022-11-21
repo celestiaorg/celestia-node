a93a75342178069c88dc2ada8a086d9768b38839
Hlib Kanunnikov <hlibwondertan@gmail.com>
2022-11-21 14:48:54 +0100

    feat(module/das): add WaitCatchUp method to the module (#1383)
    Also moves the stub test into the das module

--- nodebuilder/node_bridge_test.go
+++ nodebuilder/node_bridge_test.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/celestiaorg/celestia-node/core"
-	"github.com/celestiaorg/celestia-node/das"
 	coremodule "github.com/celestiaorg/celestia-node/nodebuilder/core"
 	"github.com/celestiaorg/celestia-node/nodebuilder/node"
 	"github.com/celestiaorg/celestia-node/nodebuilder/p2p"
@@ -29,25 +27,5 @@ func TestBridge_WithMockedCoreClient(t *testing.T) {
 	require.NoError(t, err)
 
 	err = node.Stop(ctx)
-	require.NoError(t, err)
-}
-
-// TestBridge_HasStubDaser verifies that a bridge node implements a stub daser that returns an
-// error and empty das.SamplingStats
-func TestBridge_HasStubDaser(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	t.Cleanup(cancel)
-
-	_, client := core.StartTestClient(ctx, t)
-	nd := TestNode(t, node.Bridge, coremodule.WithClient(client))
-	require.NotNil(t, nd)
-	err := nd.Start(ctx)
-	require.NoError(t, err)
-
-	stats, err := nd.DASer.SamplingStats(ctx)
-	assert.EqualError(t, err, "moddas: dasing is not available on bridge nodes")
-	assert.Equal(t, stats, das.SamplingStats{})
-
-	err = nd.Stop(ctx)
 	require.NoError(t, err)
 }
