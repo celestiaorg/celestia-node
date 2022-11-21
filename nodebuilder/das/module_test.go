a5a3893899c02db257859cfe61d2aa6a87b97ac3
Wondertan <hlibwondertan@gmail.com>
2022-11-21 14:31:54 +0100

    tests(module/das): move the daser stub test

---
+++ nodebuilder/das/module_test.go
@@ -0,0 +1,31 @@
+package das
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+
+	"github.com/celestiaorg/celestia-node/nodebuilder/node"
+)
+
+// TestConstructModule_DASBridgeStub verifies that a bridge node implements a stub daser that
+// returns an error and empty das.SamplingStats
+func TestConstructModule_DASBridgeStub(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	var mod Module
+
+	cfg := DefaultConfig()
+	app := fxtest.New(t,
+		ConstructModule(node.Bridge, &cfg),
+		fx.Populate(&mod)).
+		RequireStart()
+	defer app.RequireStop()
+
+	_, err := mod.SamplingStats(ctx)
+	assert.ErrorIs(t, err, errStub)
+}
