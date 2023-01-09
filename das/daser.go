a9ae7e1f67ef235617fbc3e0042331115588adea
Vlad <vlad@celestia.org>
2023-01-09 14:34:00 +0300

    rework subscriber to sync shrex.Sub with header Sub

--- das/daser.go
+++ das/daser.go
@@ -55,7 +55,6 @@ func NewDASer(
 		hsub:           hsub,
 		getter:         getter,
 		store:          newCheckpointStore(dstore),
-		subscriber:     newSubscriber(),
 		subscriberDone: make(chan struct{}),
 	}
 
@@ -104,8 +103,9 @@ func (d *DASer) Start(ctx context.Context) error {
 	runCtx, cancel := context.WithCancel(context.Background())
 	d.cancel = cancel
 
+	d.subscriber = newSubscriber(runCtx, d.da, d.sampler.listen)
 	go d.sampler.run(runCtx, cp)
-	go d.subscriber.run(runCtx, sub, d.sampler.listen)
+	go d.subscriber.subscribeHeader(sub)
 	go d.store.runBackgroundStore(runCtx, d.params.BackgroundStoreInterval, d.sampler.getCheckpoint)
 
 	return nil
