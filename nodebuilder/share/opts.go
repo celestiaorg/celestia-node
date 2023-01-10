677c26083cb44701bd86ce7eb5e0d367988f5ef8
derrandz <ouaghad.hamza@gmail.com>
2023-01-10 21:18:18 +0100

    test: add test doubles to test the proxying pattern

--- nodebuilder/share/opts.go
+++ nodebuilder/share/opts.go
@@ -2,6 +2,8 @@ package share
 
 import (
 	"fmt"
+
+	"github.com/celestiaorg/celestia-node/share"
 )
 
 func WithBlackBoxMetrics(ss Module) (Module, error) {
@@ -19,6 +21,27 @@ func WithBlackBoxMetrics(ss Module) (Module, error) {
 	// availability instance here
 	// once it's implemented
 	mod.Getter = insShareGetter
+	pmod, err := Proxy(mod, "Getter", insShareGetter)
+	if err != nil {
+		return nil, err
+	}
 
-	return Module(mod), nil
+	return Module(pmod), nil
+}
+
+func Proxy(mod *module, field string, proxy any) (Module, error) {
+	switch field {
+	case "Getter":
+		getter, ok := proxy.(share.Getter)
+		if !ok {
+			return nil, fmt.Errorf("trying to proxy `Getter` but provided a different type")
+		}
+
+		mod.Getter = getter
+		return mod, nil
+
+	default:
+		// add logging
+		return mod, nil
+	}
 }
