91219a723bc691058aff6f50807bbfcd28a3341e
Viacheslav <viacheslavgonkivskyi@gmail.com>
2023-01-25 09:28:28 +0100

    chore(swamp): replace cosmos prefix with celestia in swamp tests (#1583)

---
+++ core/config.go
@@ -0,0 +1,16 @@
+package core
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/celestiaorg/celestia-app/app"
+)
+
+func init() {
+	// This is necessary to ensure that the account addresses are correctly prefixed
+	// as in the celestia application.
+	cfg := sdk.GetConfig()
+	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
+	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
+	cfg.Seal()
+}
