ec32b785feab53bd4b82db27c66e6d1e1e8ca86c
Tomasz Zdybał <tomek@zdybal.lap.pl>
2023-01-27 10:49:20 +0100

    test: parallel test for ranges.Add method

---
+++ libs/header/sync/ranges_test.go
@@ -0,0 +1,28 @@
+package sync
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/celestiaorg/celestia-node/libs/header/test"
+)
+
+func TestAddParallel(t *testing.T) {
+	var pending ranges[*test.DummyHeader]
+
+	n := 500
+	suite := test.NewTestSuite(t)
+	headers := suite.GenDummyHeaders(1000)
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			pending.Add(headers[i])
+		}(i)
+	}
+
+	last := uint64(0)
+	for _, r := range pending.ranges {
+		assert.Greater(t, r.start, last)
+		last = r.start
+	}
+}
