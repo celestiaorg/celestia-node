d25d0470fa52cecc6044f66147b759af9b87a92b
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-01-27 17:28:05 +0100

    fix(config): move sealing to the core accessor

--- nodebuilder/state/core.go
+++ nodebuilder/state/core.go
@@ -1,6 +1,9 @@
 package state
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/celestiaorg/celestia-app/app"
 	apptypes "github.com/celestiaorg/celestia-app/x/blob/types"
 	"github.com/celestiaorg/celestia-node/header"
 	"github.com/celestiaorg/celestia-node/libs/header/sync"
@@ -15,5 +18,16 @@ func CoreAccessor(
 	signer *apptypes.KeyringSigner,
 	sync *sync.Syncer[*header.ExtendedHeader],
 ) *state.CoreAccessor {
+	initConfig()
 	return state.NewCoreAccessor(signer, sync, corecfg.IP, corecfg.RPCPort, corecfg.GRPCPort)
+}
+
+// calling it from here helps to avoid issues with double sealing.
+func initConfig() {
+	// This is necessary to ensure that the account addresses are correctly prefixed
+	// as in the celestia application.
+	cfg := sdk.GetConfig()
+	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
+	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
+	cfg.Seal()
 }
