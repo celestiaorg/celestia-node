07717c7941999dce15dcb2b99a70f734009ecb94
Nhu Viet Nguyen <braveryandglory@gmail.com>
2023-02-07 15:38:34 +0100

    chore!: bump arabica-4 genesis hash

--- nodebuilder/p2p/genesis.go
+++ nodebuilder/p2p/genesis.go
@@ -23,7 +23,7 @@ func GenesisFor(net Network) (string, error) {
 
 // NOTE: Every time we add a new long-running network, its genesis hash has to be added here.
 var genesisList = map[Network]string{
-	Arabica: "6D2D85AC2817EC2CCA8C75893EFA398B7EEB988528AC403CD0102AE4230D415A",
+	Arabica: "C89DDAB34DB47895DB79F67BF28CE72570D6C586436FC3449284CA411DEFCC53",
 	Mocha:   "8038B21032C941372ED601699857043C12E5CC7D5945DCEEA4567D11B5712526",
 	Private: "",
 }
