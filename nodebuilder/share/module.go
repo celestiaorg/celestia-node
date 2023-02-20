145f095926a49afad0594e1c98c398672d88e16a
rene <41963722+renaynay@users.noreply.github.com>
2023-02-20 12:28:11 +0100

    chore(nodebuilder/share): remove todos

--- nodebuilder/share/module.go
+++ nodebuilder/share/module.go
@@ -131,7 +131,7 @@ func ConstructModule(tp node.Type, cfg *Config, options ...fx.Option) fx.Option 
 			// cacheAvailability's lifecycle continues to use a fx hook,
 			// since the LC requires a cacheAvailability but the constructor returns a share.Availability
 			fx.Provide(cacheAvailability[*light.ShareAvailability]),
-			fx.Invoke(ensurePeersLifecycle), // TODO doc why we need this in light
+			fx.Invoke(ensurePeersLifecycle),
 		)
 	case node.Bridge:
 		return fx.Module(
@@ -141,7 +141,7 @@ func ConstructModule(tp node.Type, cfg *Config, options ...fx.Option) fx.Option 
 			fx.Provide(func(ipldGetter *getters.IPLDGetter) share.Getter {
 				return ipldGetter
 			}),
-			fx.Invoke(ensurePeersLifecycle), // TODO doc why we need this in bridge
+			fx.Invoke(ensurePeersLifecycle),
 		)
 	case node.Full:
 		return fx.Module(
