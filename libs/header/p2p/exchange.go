b9935a28b4386e0487297a4e332119f8aba1dd7d
derrandz <ouaghad.hamza@gmail.com>
2023-02-23 16:53:11 +0100

    fix: handle error by only tracking metrics if no errors was encountered

--- libs/header/p2p/exchange.go
+++ libs/header/p2p/exchange.go
@@ -254,8 +254,6 @@ func (ex *Exchange[H]) request(
 ) ([]H, error) {
 	log.Debugw("requesting peer", "peer", to)
 	responses, size, duration, err := sendMessage(ctx, ex.host, to, ex.protocolID, req)
-	ex.metrics.observeResponse(ctx, size, duration)
-
 	if err != nil {
 		log.Debugw("err sending request", "peer", to, "err", err)
 		return nil, err
@@ -263,6 +261,7 @@ func (ex *Exchange[H]) request(
 	if len(responses) == 0 {
 		return nil, header.ErrNotFound
 	}
+	ex.metrics.observeResponse(ctx, size, duration)
 	headers := make([]H, 0, len(responses))
 	for _, response := range responses {
 		if err = convertStatusCodeToError(response.StatusCode); err != nil {
