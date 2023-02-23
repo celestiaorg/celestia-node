c5580ad51998802bfb549403ba58ba322775e17f
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-02-23 16:05:33 +0200

    fix: fix protocolID representation

--- libs/header/p2p/helpers.go
+++ libs/header/p2p/helpers.go
@@ -17,11 +17,11 @@ import (
 )
 
 func protocolID(networkID string) protocol.ID {
-	return protocol.ID(fmt.Sprintf("%s/header-ex/v0.0.3", networkID))
+	return protocol.ID(fmt.Sprintf("/%s/header-ex/v0.0.3", networkID))
 }
 
 func PubsubTopicID(networkID string) string {
-	return fmt.Sprintf("%s/header-sub/v0.0.1", networkID)
+	return fmt.Sprintf("/%s/header-sub/v0.0.1", networkID)
 }
 
 // sendMessage opens the stream to the given peers and sends HeaderRequest to fetch
