d111d6fe59bd02b012b052a33f7ac78b52e5961b
derrandz <ouaghad.hamza@gmail.com>
2023-02-23 13:25:06 +0100

    fix: cast once

--- nodebuilder/header/opts.go
+++ nodebuilder/header/opts.go
@@ -1,8 +1,6 @@
 package header
 
 import (
-	"fmt"
-
 	header "github.com/celestiaorg/celestia-node/header"
 	libhead "github.com/celestiaorg/celestia-node/libs/header"
 	p2p "github.com/celestiaorg/celestia-node/libs/header/p2p"
@@ -10,13 +8,9 @@ import (
 
 // WithMetrics provides sets `MetricsEnabled` to true on ClientParameters for the header exchange
 func WithMetrics(ex libhead.Exchange[*header.ExtendedHeader]) error {
-	switch any(ex).(type) {
+	switch p2pex := any(ex).(type) {
 	case *p2p.Exchange[*header.ExtendedHeader]:
-		exchange, ok := (ex).(*p2p.Exchange[*header.ExtendedHeader])
-		if !ok {
-			return fmt.Errorf("header.WithMetrics: type is to *p2p.Exchange but cast failed")
-		}
-		return exchange.RegisterMetrics()
+		return p2pex.RegisterMetrics()
 	default:
 		return nil
 	}
