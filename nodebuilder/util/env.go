35645fd3e6d8f79ca849675e66563fc40c030117
Smuu <18609909+Smuu@users.noreply.github.com>
2023-03-10 09:28:30 +0100

    feat: introduce bootstrapper env variable
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

---
+++ nodebuilder/util/env.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"os"
+)
+
+var IsBootstrapper bool
+
+func init() {
+	IsBootstrapper = getEnv("BOOTSTRAPPER", "false") == "true"
+}
+
+// Get environment variable or fallback to default value
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
