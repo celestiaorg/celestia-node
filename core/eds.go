1b99704f55ca8d101d1ba98014b57b3cf55bc012
rene <41963722+renaynay@users.noreply.github.com>
2023-03-22 08:44:16 +0100

    fix(core): Only assume empty square if reported square size is 1

--- core/eds.go
+++ core/eds.go
@@ -20,7 +20,7 @@ import (
 // ExtendedDataSquare (EDS). If there are no transactions in the block,
 // nil is returned in place of the eds.
 func extendBlock(data types.Data) (*rsmt2d.ExtendedDataSquare, error) {
-	if len(data.Txs) == 0 {
+	if len(data.Txs) == 0 && data.SquareSize == uint64(1) {
 		return nil, nil
 	}
 	shares, err := appshares.Split(data, true)
