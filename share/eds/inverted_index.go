fa6621b0843b527c0f94b973f5a4a77db2855a52
Ryan <ryanford@poluglottos.com>
2023-04-12 09:18:25 +0200

    feat(share/eds): add custom inverted index for badger as storage backend

---
+++ share/eds/inverted_index.go
@@ -0,0 +1,92 @@
+package eds
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/filecoin-project/dagstore/index"
+	"github.com/filecoin-project/dagstore/shard"
+	ds "github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore/namespace"
+	"github.com/multiformats/go-multihash"
+)
+
+// simpleInvertedIndex is an inverted index that only stores a single shard key per multihash. Its
+// implementation is modified from the default upstream implementation in dagstore/index.
+type simpleInvertedIndex struct {
+	mu sync.Mutex
+	ds ds.Batching
+}
+
+// newSimpleInvertedIndex returns a new inverted index that only stores a single shard key per
+// multihash. This is because we use badger as a storage backend, so updates are expensive, and we
+// don't care which shard is used to serve a cid.
+func newSimpleInvertedIndex(dts ds.Batching) *simpleInvertedIndex {
+	dts = namespace.Wrap(dts, ds.NewKey("/inverted/index"))
+	return &simpleInvertedIndex{
+		ds: dts,
+	}
+}
+
+func (s *simpleInvertedIndex) AddMultihashesForShard(
+	ctx context.Context,
+	mhIter index.MultihashIterator,
+	sk shard.Key,
+) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	batch, err := s.ds.Batch(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to create ds batch: %w", err)
+	}
+
+	if err := mhIter.ForEach(func(mh multihash.Multihash) error {
+		key := ds.NewKey(string(mh))
+		ok, err := s.ds.Has(ctx, key)
+		if err != nil {
+			return fmt.Errorf("failed to check if value for multihash exists %s, err: %w", mh, err)
+		}
+
+		if !ok {
+			bz, err := json.Marshal(sk)
+			if err != nil {
+				return fmt.Errorf("failed to marshal shard key to bytes: %w", err)
+			}
+			if err := batch.Put(ctx, key, bz); err != nil {
+				return fmt.Errorf("failed to put mh=%s, err=%w", mh, err)
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return fmt.Errorf("failed to add index entry: %w", err)
+	}
+
+	if err := batch.Commit(ctx); err != nil {
+		return fmt.Errorf("failed to commit batch: %w", err)
+	}
+
+	if err := s.ds.Sync(ctx, ds.Key{}); err != nil {
+		return fmt.Errorf("failed to sync puts: %w", err)
+	}
+
+	return nil
+}
+
+func (s *simpleInvertedIndex) GetShardsForMultihash(ctx context.Context, mh multihash.Multihash) ([]shard.Key, error) {
+	key := ds.NewKey(string(mh))
+	sbz, err := s.ds.Get(ctx, key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to lookup index for mh %s, err: %w", mh, err)
+	}
+
+	var shardKey shard.Key
+	if err := json.Unmarshal(sbz, &shardKey); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal shard key for mh=%s, err=%w", mh, err)
+	}
+
+	return []shard.Key{shardKey}, nil
+}
