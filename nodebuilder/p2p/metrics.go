e34ab890802d905720de4f8b701c09131d10c59b
Wondertan <hlibwondertan@gmail.com>
2023-04-14 13:55:09 +0200

    fix(modp2p): fix libp2p metrics along with cleanup
    move option back

--- nodebuilder/p2p/metrics.go
+++ nodebuilder/p2p/metrics.go
@@ -4,60 +4,41 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
-	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/core/network"
-	rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
 	rcmgrObs "github.com/libp2p/go-libp2p/p2p/host/resource-manager/obs"
-	ma "github.com/multiformats/go-multiaddr"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/fx"
-
-	"github.com/celestiaorg/celestia-node/nodebuilder/node"
 )
 
-const promAgentEndpoint = "/metrics"
-
-type MetricsConfig struct {
-	// Prometheus Agent http configuration
-	PrometheusAgentPort string
+// WithMetrics option sets up native libp2p metrics up.
+func WithMetrics() fx.Option {
+	return fx.Options(
+		fx.Provide(resourceManagerOpt(traceReporter)),
+		fx.Provide(prometheusRegisterer),
+		fx.Invoke(prometheusMetrics),
+	)
 }
 
-// DefaultMetricsConfig returns default configuration for P2P subsystem.
-func DefaultMetricsConfig() MetricsConfig {
-	return MetricsConfig{
-		PrometheusAgentPort: "8890",
-	}
-}
+const (
+	promAgentEndpoint = "/metrics"
+	promAgentPort     = "8890"
+)
 
-func (cfg *MetricsConfig) Validate() error {
-	if cfg.PrometheusAgentPort == "" {
-		return fmt.Errorf("p2p metrics: prometheus agent port cannot be empty")
-	}
+// prometheusMetrics option sets up native libp2p metrics up
+func prometheusMetrics(lifecycle fx.Lifecycle, registerer prometheus.Registerer) error {
+	rcmgrObs.MustRegisterWith(registerer)
 
-	if _, err := strconv.Atoi(cfg.PrometheusAgentPort); err != nil {
-		return fmt.Errorf("p2p metrics: prometheus agent port must be a number")
-	}
-
-	return nil
-}
-
-// WithMetrics option sets up native libp2p metrics up
-func WithMetrics(lifecycle fx.Lifecycle, cfg Config) error {
-	rcmgrObs.MustRegisterWith(prometheus.DefaultRegisterer)
-
-	reg := prometheus.DefaultRegisterer
-	registry := reg.(*prometheus.Registry)
+	registry := registerer.(*prometheus.Registry)
 
 	mux := http.NewServeMux()
-	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{Registry: reg})
+	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{Registry: registerer})
 	mux.Handle(promAgentEndpoint, handler)
 
+	// TODO(@Wondertan): Unify all the servers into one (See #2007)
 	promHTTPServer := &http.Server{
-		Addr:              fmt.Sprintf(":%s", cfg.Metrics.PrometheusAgentPort),
+		Addr:              fmt.Sprintf(":%s", promAgentPort),
 		Handler:           mux,
 		ReadHeaderTimeout: 10 * time.Second,
 	}
@@ -66,12 +47,11 @@ func WithMetrics(lifecycle fx.Lifecycle, cfg Config) error {
 		OnStart: func(ctx context.Context) error {
 			go func() {
 				if err := promHTTPServer.ListenAndServe(); err != nil {
-					log.Error("Error starting Prometheus metrics exporter http server")
-					panic(err)
+					log.Errorf("Error starting Prometheus metrics exporter http server: %s", err)
 				}
 			}()
 
-			log.Info("Prometheus agent started on :%s/%s", cfg.Metrics.PrometheusAgentPort, promAgentEndpoint)
+			log.Info("Prometheus agent started on :%s/%s", promAgentPort, promAgentEndpoint)
 			return nil
 		},
 		OnStop: func(ctx context.Context) error {
@@ -81,36 +61,6 @@ func WithMetrics(lifecycle fx.Lifecycle, cfg Config) error {
 	return nil
 }
 
-func WithMonitoredResourceManager(nodeType node.Type, allowlist []ma.Multiaddr) (network.ResourceManager, error) {
-	str, err := rcmgrObs.NewStatsTraceReporter()
-	if err != nil {
-		return nil, err
-	}
-
-	var monitoredRcmgr network.ResourceManager
-
-	switch nodeType {
-	case node.Full, node.Bridge:
-		monitoredRcmgr, err = rcmgr.NewResourceManager(
-			rcmgr.NewFixedLimiter(rcmgr.InfiniteLimits),
-			rcmgr.WithTraceReporter(str),
-		)
-
-	case node.Light:
-		limits := rcmgr.DefaultLimits
-		libp2p.SetDefaultServiceLimits(&limits)
-
-		monitoredRcmgr, err = rcmgr.NewResourceManager(
-			rcmgr.NewFixedLimiter(limits.AutoScale()),
-			rcmgr.WithAllowlistedMultiaddrs(allowlist),
-			rcmgr.WithTraceReporter(str),
-		)
-	default:
-		panic("invalid node type")
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return monitoredRcmgr, err
+func prometheusRegisterer() prometheus.Registerer {
+	return prometheus.NewRegistry()
 }
