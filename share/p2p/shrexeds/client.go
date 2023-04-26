62b0b5636e860f8ec5f36054f9c94801b47ee14a
Vlad <vlad@celestia.org>
2023-04-26 15:34:26 +0300

    rename and reorg structure

--- share/p2p/shrexeds/client.go
+++ share/p2p/shrexeds/client.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
 
