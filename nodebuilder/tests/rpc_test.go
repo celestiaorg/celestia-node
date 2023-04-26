cb85adbc237d1e9a4793de2f8a4fbcb54e07e7fd
rene <41963722+renaynay@users.noreply.github.com>
2023-04-26 18:07:07 +0200

    test(nodebuilder/tests): Add swamp integration test for headersub over rpc

---
+++ nodebuilder/tests/rpc_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/stretchr/testify/require"
+
+	"github.com/celestiaorg/celestia-node/nodebuilder"
+	"github.com/celestiaorg/celestia-node/nodebuilder/node"
+	"github.com/celestiaorg/celestia-node/nodebuilder/tests/swamp"
+)
+
+// TestHeaderSubscription ensures that the header subscription over RPC works
+// as intended and gets canceled successfully after rpc context cancellation.
+func TestHeaderSubscription(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), swamp.DefaultTestTimeout)
+	t.Cleanup(cancel)
+
+	sw := swamp.NewSwamp(t, swamp.WithBlockTime(btime))
+
+	// start a bridge node
+	bridge := sw.NewBridgeNode()
+	err := bridge.Start(ctx)
+	require.NoError(t, err)
+
+	cfg := nodebuilder.DefaultConfig(node.Light)
+	addrs, err := peer.AddrInfoToP2pAddrs(host.InfoFromHost(bridge.Host))
+	require.NoError(t, err)
+	cfg.Header.TrustedPeers = append(cfg.Header.TrustedPeers, addrs[0].String())
+
+	// start a light node that's connected to the bridge node
+	light := sw.NewNodeWithConfig(node.Light, cfg)
+	err = light.Start(ctx)
+	require.NoError(t, err)
+
+	// subscribe to headers via the light node's RPC header subscription
+	subctx, subcancel := context.WithCancel(ctx)
+	sub, err := light.HeaderServ.Subscribe(subctx)
+	require.NoError(t, err)
+	// listen for 5 headers
+	for i := 0; i < 5; i++ {
+		if ctx.Err() != nil {
+			t.Fatal(ctx.Err())
+		}
+		<-sub
+	}
+	// cancel subscription via context
+	subcancel()
+
+	// stop the light node and expect no outstanding subscription errors
+	err = light.Stop(ctx)
+	require.NoError(t, err)
+}
