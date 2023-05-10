f94cb4f0d9e140e078fd187afedf8a97b7a2a6bd
Vlad <vlad@celestia.org>
2023-05-10 11:58:04 +0300

    group defer and add comment

--- share/availability/discovery/discovery.go
+++ share/availability/discovery/discovery.go
@@ -241,15 +241,18 @@ func (d *Discovery) discover(ctx context.Context) bool {
 	}
 	log.Infow("discovering peers", "want", want)
 
-	// stop discovery when we are done
-	findCtx, findCancel := context.WithCancel(ctx)
-	defer findCancel()
-
 	// we use errgroup as it provide limits
 	var wg errgroup.Group
 	// limit to minimize chances of overreaching the limit
 	wg.SetLimit(int(d.set.Limit()))
-	defer wg.Wait() //nolint:errcheck
+
+	// stop discovery when we are done
+	findCtx, findCancel := context.WithCancel(ctx)
+	defer func() {
+		// some workers could still be running, wait them to finish before canceling findCtx
+		wg.Wait() //nolint:errcheck
+		findCancel()
+	}()
 
 	peers, err := d.disc.FindPeers(findCtx, rendezvousPoint)
 	if err != nil {
