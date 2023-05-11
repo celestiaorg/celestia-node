687704e2038b256f249e9cb565d62a47ed7727ba
Vlad <13818348+walldiss@users.noreply.github.com>
2023-05-11 17:52:53 +0800

    feat(share/discovery): add discovery metrics (#2155)
    ## Overview
    
    Adds metrics for discovery component

--- nodebuilder/share/opts.go
+++ nodebuilder/share/opts.go
@@ -1,16 +1,23 @@
 package share
 
 import (
+	disc "github.com/celestiaorg/celestia-node/share/availability/discovery"
 	"github.com/celestiaorg/celestia-node/share/getters"
 	"github.com/celestiaorg/celestia-node/share/p2p/peers"
 	"github.com/celestiaorg/celestia-node/share/p2p/shrexeds"
 	"github.com/celestiaorg/celestia-node/share/p2p/shrexnd"
 )
 
-// WithPeerManagerMetrics is a utility function that is expected to be
-// "invoked" by the fx lifecycle.
+// WithPeerManagerMetrics is a utility function to turn on peer manager metrics and that is
+// expected to be "invoked" by the fx lifecycle.
 func WithPeerManagerMetrics(m *peers.Manager) error {
 	return m.WithMetrics()
+}
+
+// WithDiscoveryMetrics is a utility function to turn on discovery metrics and that is expected to
+// be "invoked" by the fx lifecycle.
+func WithDiscoveryMetrics(d *disc.Discovery) error {
+	return d.WithMetrics()
 }
 
 func WithShrexClientMetrics(edsClient *shrexeds.Client, ndClient *shrexnd.Client) error {
