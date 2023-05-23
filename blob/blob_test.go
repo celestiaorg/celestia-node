9a4a1da9381acb22c028d5e6ccb42235ef74a96d
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-05-23 13:31:21 +0300

    chore(blob/tests): extend swamp tests

--- blob/blob_test.go
+++ blob/blob_test.go
@@ -1,16 +1,11 @@
 package blob
 
 import (
-	"bytes"
-	"sort"
 	"testing"
 
+	apptypes "github.com/celestiaorg/celestia-app/x/blob/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/types"
-
-	"github.com/celestiaorg/celestia-app/pkg/shares"
-	apptypes "github.com/celestiaorg/celestia-app/x/blob/types"
 )
 
 func TestBlob(t *testing.T) {
@@ -59,23 +54,4 @@ func TestBlob(t *testing.T) {
 	for _, tt := range test {
 		t.Run(tt.name, tt.expectedRes)
 	}
-}
-
-func splitBlob(t *testing.T, blobs ...*Blob) [][]byte {
-	t.Helper()
-	tendermintBlobs := make([]types.Blob, 0)
-	for _, blob := range blobs {
-		tendermintBlobs = append(tendermintBlobs, types.Blob{
-			NamespaceID:  blob.NamespaceID(),
-			Data:         blob.Data(),
-			ShareVersion: uint8(blob.Version()),
-		})
-	}
-	sort.Slice(tendermintBlobs, func(i, j int) bool {
-		val := bytes.Compare(tendermintBlobs[i].NamespaceID, tendermintBlobs[j].NamespaceID)
-		return val <= 0
-	})
-	rawShares, err := shares.SplitBlobs(0, nil, tendermintBlobs, false)
-	require.NoError(t, err)
-	return shares.ToBytes(rawShares)
 }
--- nodebuilder/tests/blob_test.go
+++ nodebuilder/tests/blob_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"bytes"
 	"context"
 	"reflect"
 	"testing"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/celestiaorg/celestia-node/blob"
@@ -85,4 +87,87 @@ func TestBlobModuleIncluded(t *testing.T) {
 	included, err := lightNode.BlobServ.Included(ctx, height, blob[0].NamespaceID(), proof, blob[0].Commitment())
 	require.NoError(t, err)
 	require.True(t, included)
+}
+
+func TestBlobModuleGetAll(t *testing.T) {
+	const (
+		btime = time.Millisecond * 300
+	)
+	sw := swamp.NewSwamp(t, swamp.WithBlockTime(btime))
+	ctx, cancel := context.WithTimeout(context.Background(), swamp.DefaultTestTimeout)
+	t.Cleanup(cancel)
+	blobs := blob.GenerateBlobs(t, []int{8, 8}, false)
+	bridge := sw.NewBridgeNode()
+	require.NoError(t, bridge.Start(ctx))
+
+	addrs, err := peer.AddrInfoToP2pAddrs(host.InfoFromHost(bridge.Host))
+	require.NoError(t, err)
+
+	fullCfg := sw.DefaultTestConfig(node.Full)
+	fullCfg.Header.TrustedPeers = append(fullCfg.Header.TrustedPeers, addrs[0].String())
+	fullNode := sw.NewNodeWithConfig(node.Full, fullCfg)
+	require.NoError(t, fullNode.Start(ctx))
+
+	addrsFull, err := peer.AddrInfoToP2pAddrs(host.InfoFromHost(fullNode.Host))
+	require.NoError(t, err)
+
+	lightCfg := sw.DefaultTestConfig(node.Light)
+	lightCfg.Header.TrustedPeers = append(fullCfg.Header.TrustedPeers, addrsFull[0].String())
+	lightNode := sw.NewNodeWithConfig(node.Light, lightCfg)
+	require.NoError(t, lightNode.Start(ctx))
+
+	height, err := fullNode.BlobServ.Submit(ctx, blobs...)
+	require.NoError(t, err)
+	_, err = fullNode.HeaderServ.WaitForHeight(ctx, height)
+	require.NoError(t, err)
+
+	_, err = lightNode.HeaderServ.WaitForHeight(ctx, height)
+	require.NoError(t, err)
+
+	newBlobs, err := fullNode.BlobServ.GetAll(ctx, height, blobs[0].NamespaceID(), blobs[1].NamespaceID())
+	require.NoError(t, err)
+	require.Len(t, newBlobs, 2)
+	require.True(t, bytes.Equal(blobs[0].Commitment(), newBlobs[0].Commitment()))
+	require.True(t, bytes.Equal(blobs[1].Commitment(), newBlobs[1].Commitment()))
+}
+
+func TestBlobModuleNotFound(t *testing.T) {
+	const (
+		btime = time.Millisecond * 300
+	)
+	sw := swamp.NewSwamp(t, swamp.WithBlockTime(btime))
+	ctx, cancel := context.WithTimeout(context.Background(), swamp.DefaultTestTimeout)
+	t.Cleanup(cancel)
+	blobs := blob.GenerateBlobs(t, []int{8, 8}, false)
+	bridge := sw.NewBridgeNode()
+	require.NoError(t, bridge.Start(ctx))
+
+	addrs, err := peer.AddrInfoToP2pAddrs(host.InfoFromHost(bridge.Host))
+	require.NoError(t, err)
+
+	fullCfg := sw.DefaultTestConfig(node.Full)
+	fullCfg.Header.TrustedPeers = append(fullCfg.Header.TrustedPeers, addrs[0].String())
+	fullNode := sw.NewNodeWithConfig(node.Full, fullCfg)
+	require.NoError(t, fullNode.Start(ctx))
+
+	addrsFull, err := peer.AddrInfoToP2pAddrs(host.InfoFromHost(fullNode.Host))
+	require.NoError(t, err)
+
+	lightCfg := sw.DefaultTestConfig(node.Light)
+	lightCfg.Header.TrustedPeers = append(fullCfg.Header.TrustedPeers, addrsFull[0].String())
+	lightNode := sw.NewNodeWithConfig(node.Light, lightCfg)
+	require.NoError(t, lightNode.Start(ctx))
+
+	height, err := fullNode.BlobServ.Submit(ctx, blobs[0])
+	require.NoError(t, err)
+	_, err = fullNode.HeaderServ.WaitForHeight(ctx, height)
+	require.NoError(t, err)
+
+	_, err = lightNode.HeaderServ.WaitForHeight(ctx, height)
+	require.NoError(t, err)
+
+	b, err := fullNode.BlobServ.Get(ctx, height, blobs[1].NamespaceID(), blobs[1].Commitment())
+	assert.Nil(t, b)
+	require.Error(t, err)
+	require.ErrorIs(t, err, blob.ErrBlobNotFound)
 }
