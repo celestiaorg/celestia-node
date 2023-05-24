6fed31bed042e12eaa719a995ff0a51c4dcecbf7
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-05-24 20:49:45 +0300

    chore(blob/module): change module creation

--- nodebuilder/blob/module.go
+++ nodebuilder/blob/module.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/celestiaorg/celestia-node/blob"
 	"github.com/celestiaorg/celestia-node/header"
-	"github.com/celestiaorg/celestia-node/libs/fxutil"
 	headerService "github.com/celestiaorg/celestia-node/nodebuilder/header"
 	"github.com/celestiaorg/celestia-node/share"
 	"github.com/celestiaorg/celestia-node/state"
@@ -15,15 +14,15 @@ import (
 
 func ConstructModule() fx.Option {
 	return fx.Module("blob",
-		fxutil.ProvideAs(
+		fx.Provide(
 			func(service headerService.Module) func(context.Context, uint64) (*header.ExtendedHeader, error) {
 				return service.GetByHeight
 			}),
-		fx.Provide(fx.Annotate(func(
+		fx.Provide(func(
 			state *state.CoreAccessor,
 			sGetter share.Getter,
 			getByHeightFn func(context.Context, uint64) (*header.ExtendedHeader, error),
 		) Module {
 			return blob.NewService(state, sGetter, getByHeightFn)
-		})))
+		}))
 }
