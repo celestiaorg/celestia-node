356a529337d4a1db679c23261d87e820c03801de
Ryan <ryanford@poluglottos.com>
2023-06-08 15:26:34 +0200

    making test table-driven

--- state/address_test.go
+++ state/address_test.go
@@ -9,28 +9,49 @@ import (
 )
 
 func TestAddressMarshalling(t *testing.T) {
-	accAddrString := "celestia1377k5an3f94v6wyaceu0cf4nq6gk2jtpc46g7h"
-	valAddrString := "celestiavaloper1q3v5cugc8cdpud87u4zwy0a74uxkk6u4q4gx4p"
-	accAddr, err := sdk.AccAddressFromBech32(accAddrString)
-	require.NoError(t, err)
-	valAddr, err := sdk.ValAddressFromBech32(valAddrString)
-	require.NoError(t, err)
+	testCases := []struct {
+		name           string
+		addressString  string
+		addressFromStr func(string) (interface{}, error)
+		marshalJSON    func(interface{}) ([]byte, error)
+		unmarshalJSON  func([]byte) (interface{}, error)
+	}{
+		{
+			name:           "Account Address",
+			addressString:  "celestia1377k5an3f94v6wyaceu0cf4nq6gk2jtpc46g7h",
+			addressFromStr: func(s string) (interface{}, error) { return sdk.AccAddressFromBech32(s) },
+			marshalJSON:    func(addr interface{}) ([]byte, error) { return addr.(sdk.AccAddress).MarshalJSON() },
+			unmarshalJSON: func(b []byte) (interface{}, error) {
+				var addr sdk.AccAddress
+				err := addr.UnmarshalJSON(b)
+				return addr, err
+			},
+		},
+		{
+			name:           "Validator Address",
+			addressString:  "celestiavaloper1q3v5cugc8cdpud87u4zwy0a74uxkk6u4q4gx4p",
+			addressFromStr: func(s string) (interface{}, error) { return sdk.ValAddressFromBech32(s) },
+			marshalJSON:    func(addr interface{}) ([]byte, error) { return addr.(sdk.ValAddress).MarshalJSON() },
+			unmarshalJSON: func(b []byte) (interface{}, error) {
+				var addr sdk.ValAddress
+				err := addr.UnmarshalJSON(b)
+				return addr, err
+			},
+		},
+	}
 
-	accAddrBytes, err := accAddr.MarshalJSON()
-	assert.NoError(t, err)
-	assert.Equal(t, []byte("\""+accAddrString+"\""), accAddrBytes)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			addr, err := tc.addressFromStr(tc.addressString)
+			require.NoError(t, err)
 
-	valAddrBytes, err := valAddr.MarshalJSON()
-	assert.NoError(t, err)
-	assert.Equal(t, []byte("\""+valAddrString+"\""), valAddrBytes)
+			addrBytes, err := tc.marshalJSON(addr)
+			assert.NoError(t, err)
+			assert.Equal(t, []byte("\""+tc.addressString+"\""), addrBytes)
 
-	var accAddrUnmarshalled Address
-	err = accAddrUnmarshalled.UnmarshalJSON(accAddrBytes)
-	assert.NoError(t, err)
-	assert.Equal(t, accAddr, accAddrUnmarshalled.Address)
-
-	var valAddrUnmarshalled Address
-	err = valAddrUnmarshalled.UnmarshalJSON(valAddrBytes)
-	assert.NoError(t, err)
-	assert.Equal(t, valAddr, valAddrUnmarshalled.Address)
+			addrUnmarshalled, err := tc.unmarshalJSON(addrBytes)
+			assert.NoError(t, err)
+			assert.Equal(t, addr, addrUnmarshalled)
+		})
+	}
 }
