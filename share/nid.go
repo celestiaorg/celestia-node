e077a22249a07a70386bd65968a3029d826c4336
Hlib Kanunnikov <hlibwondertan@gmail.com>
2023-06-21 16:28:47 +0200

    share: introduce Namespace type (#2367)
    Mostly copied from the app's Namespace type with multiple modifications.
    In preparation for
    https://github.com/celestiaorg/celestia-node/issues/2301.
    
    We want a more straightforward Namespace type from what the app
    provides:
    * Repsents 1:1 mapping of the serialized form of Namespace [version byte
    + namespace id]
    * So that it does not allocate on the hot path when getting data by
    namespace
    * and had simpler json serialization

--- share/nid.go
+++ share/nid.go
@@ -10,6 +10,7 @@ import (
 // NewNamespaceV0 takes a variable size byte slice and creates a version 0 Namespace ID.
 // The byte slice must be <= 10 bytes.
 // If it is less than 10 bytes, it will be left padded to size 10 with 0s.
+// TODO: Adapt for Namespace in the integration PR
 func NewNamespaceV0(subNId []byte) (namespace.ID, error) {
 	if lnid := len(subNId); lnid > appns.NamespaceVersionZeroIDSize {
 		return nil, fmt.Errorf("namespace id must be <= %v, but it was %v bytes", appns.NamespaceVersionZeroIDSize, lnid)
