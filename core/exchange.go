65462525ea6afb819d8189c6d21cec0146ae55c5
Vlad <13818348+walldiss@users.noreply.github.com>
2023-08-07 21:25:12 +0800

    feat(eds/store): cache proofs after first eds recompute on sampling (#2429)
    ## Overview
    
    Before this change eds was recomputed twice on sampling:
    1. inside sampling client to verify data integrity
     2. On storing EDS to storage to calculate merkle tree proof nodes.
    
    This PR adds `proofsAdder` cache, that will store proof nodes for later
    use with key features:
    - cache could be optionally added to context and will be respected by
    ipld/shrex getter to populate on eds recompute.
    - cache is safe from second recompute, since it will only allow single
    recompute write and all consequent recomputes over same cache will be
    noop.
     
    Also this PR reworks writeEDS to use use `proofsAdder` instead of
    creating new blockstore and iterating over all keys in it. This approach
    speeds up writeEDS even without pre collected cache.
     
    Since there are no more need for blockstore being created, I have
    simplified writeEDS and removed a lot of redundant code including whole
    struct of `writeSession`.
     
    The PR also adds benchmark that uses disk for badger store (existing
    benchmark uses in-memory datastore instead of badger).
     
    This PR brings significant performance increase. On disk store badger
    results are:
      - before PR store.Put operation: **~1.5s**
      - after PR store.Put with cached proofs: **800ms** 
      - after PR store.Put without cached proofs: **900ms**

--- core/exchange.go
+++ core/exchange.go
@@ -9,9 +9,11 @@ import (
 	"golang.org/x/sync/errgroup"
 
 	libhead "github.com/celestiaorg/go-header"
+	"github.com/celestiaorg/nmt"
 
 	"github.com/celestiaorg/celestia-node/header"
 	"github.com/celestiaorg/celestia-node/share/eds"
+	"github.com/celestiaorg/celestia-node/share/ipld"
 )
 
 const concurrencyLimit = 4
@@ -105,7 +107,8 @@ func (ce *Exchange) Get(ctx context.Context, hash libhead.Hash) (*header.Extende
 	}
 
 	// extend block data
-	eds, err := extendBlock(block.Data, block.Header.Version.App)
+	adder := ipld.NewProofsAdder(int(block.Data.SquareSize))
+	eds, err := extendBlock(block.Data, block.Header.Version.App, nmt.NodeVisitor(adder.VisitFn()))
 	if err != nil {
 		return nil, fmt.Errorf("extending block data for height %d: %w", &block.Height, err)
 	}
@@ -119,6 +122,8 @@ func (ce *Exchange) Get(ctx context.Context, hash libhead.Hash) (*header.Extende
 		return nil, fmt.Errorf("incorrect hash in header at height %d: expected %x, got %x",
 			&block.Height, hash, eh.Hash())
 	}
+
+	ctx = ipld.CtxWithProofsAdder(ctx, adder)
 	err = storeEDS(ctx, eh.DAH.Hash(), eds, ce.store)
 	if err != nil {
 		return nil, fmt.Errorf("storing EDS to eds.Store for height %d: %w", &block.Height, err)
@@ -142,7 +147,8 @@ func (ce *Exchange) getExtendedHeaderByHeight(ctx context.Context, height *int64
 	log.Debugw("fetched signed block from core", "height", b.Header.Height)
 
 	// extend block data
-	eds, err := extendBlock(b.Data, b.Header.Version.App)
+	adder := ipld.NewProofsAdder(int(b.Data.SquareSize))
+	eds, err := extendBlock(b.Data, b.Header.Version.App, nmt.NodeVisitor(adder.VisitFn()))
 	if err != nil {
 		return nil, fmt.Errorf("extending block data for height %d: %w", b.Header.Height, err)
 	}
@@ -151,6 +157,8 @@ func (ce *Exchange) getExtendedHeaderByHeight(ctx context.Context, height *int64
 	if err != nil {
 		return nil, fmt.Errorf("constructing extended header for height %d: %w", b.Header.Height, err)
 	}
+
+	ctx = ipld.CtxWithProofsAdder(ctx, adder)
 	err = storeEDS(ctx, eh.DAH.Hash(), eds, ce.store)
 	if err != nil {
 		return nil, fmt.Errorf("storing EDS to eds.Store for block height %d: %w", b.Header.Height, err)
