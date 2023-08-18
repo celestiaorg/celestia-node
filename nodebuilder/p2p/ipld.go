ee8e2a5b0de5219750b2385d3e324eb01b475a20
Vlad <13818348+walldiss@users.noreply.github.com>
2023-08-18 16:44:01 +0800

    chore(deps): bump ipfs networking deps (#2478)
    ## Overview
    Main change is update of github.com/ipfs/go-libipfs , that is now
    located in github.com/ipfs/boxo
    
    Updates libs and their dependancies:
    - github.com/libp2p/go-libp2p-kad-dht
    - github.com/libp2p/go-libp2p-routing-helpers
     - github.com/ipfs/go-block-format
    - 	github.com/ipfs/go-blockservice
     - 	github.com/ipfs/go-ipld-format
     - 	github.com/ipfs/go-merkledag
     - 	github.com/ipld/go-car
     - 	github.com/multiformats/go-multihash
    
    Supersedes:
    https://github.com/celestiaorg/celestia-node/pull/2268
    https://github.com/celestiaorg/celestia-node/pull/2378
    https://github.com/celestiaorg/celestia-node/pull/2379
    https://github.com/celestiaorg/celestia-node/pull/1957
    
    ---------
    
    Co-authored-by: Ryan <ryanford@poluglottos.com>
    Co-authored-by: rene <41963722+renaynay@users.noreply.github.com>
    (cherry picked from commit 8eef855a1eaefd9d6eb8e10dcfebb48265b225c9)

--- nodebuilder/p2p/ipld.go
+++ nodebuilder/p2p/ipld.go
@@ -1,9 +1,9 @@
 package p2p
 
 import (
+	"github.com/ipfs/boxo/blockstore"
+	"github.com/ipfs/boxo/exchange"
 	"github.com/ipfs/go-blockservice"
-	blockstore "github.com/ipfs/go-ipfs-blockstore"
-	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 )
 
 // blockService constructs IPFS's BlockService for fetching arbitrary Merkle structures.
