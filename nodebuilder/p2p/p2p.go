6c4b334c9794f2529f95200708ba96d4acada19f
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-08-31 16:34:01 +0300

    fix permissions

--- nodebuilder/p2p/p2p.go
+++ nodebuilder/p2p/p2p.go
@@ -194,24 +194,24 @@ func (m *module) PubSubPeers(_ context.Context, topic string) ([]peer.ID, error)
 //nolint:dupl
 type API struct {
 	Internal struct {
-		Info                 func(context.Context) (peer.AddrInfo, error)                         `perm:"public"`
+		Info                 func(context.Context) (peer.AddrInfo, error)                         `perm:"admin"`
 		Peers                func(context.Context) ([]peer.ID, error)                             `perm:"admin"`
 		PeerInfo             func(ctx context.Context, id peer.ID) (peer.AddrInfo, error)         `perm:"admin"`
 		Connect              func(ctx context.Context, pi peer.AddrInfo) error                    `perm:"admin"`
 		ClosePeer            func(ctx context.Context, id peer.ID) error                          `perm:"admin"`
-		Connectedness        func(ctx context.Context, id peer.ID) (network.Connectedness, error) `perm:"read"`
+		Connectedness        func(ctx context.Context, id peer.ID) (network.Connectedness, error) `perm:"admin"`
 		NATStatus            func(context.Context) (network.Reachability, error)                  `perm:"admin"`
 		BlockPeer            func(ctx context.Context, p peer.ID) error                           `perm:"admin"`
 		UnblockPeer          func(ctx context.Context, p peer.ID) error                           `perm:"admin"`
-		ListBlockedPeers     func(context.Context) ([]peer.ID, error)                             `perm:"read"`
+		ListBlockedPeers     func(context.Context) ([]peer.ID, error)                             `perm:"admin"`
 		Protect              func(ctx context.Context, id peer.ID, tag string) error              `perm:"admin"`
 		Unprotect            func(ctx context.Context, id peer.ID, tag string) (bool, error)      `perm:"admin"`
-		IsProtected          func(ctx context.Context, id peer.ID, tag string) (bool, error)      `perm:"read"`
+		IsProtected          func(ctx context.Context, id peer.ID, tag string) (bool, error)      `perm:"admin"`
 		BandwidthStats       func(context.Context) (metrics.Stats, error)                         `perm:"admin"`
 		BandwidthForPeer     func(ctx context.Context, id peer.ID) (metrics.Stats, error)         `perm:"admin"`
 		BandwidthForProtocol func(ctx context.Context, proto protocol.ID) (metrics.Stats, error)  `perm:"admin"`
 		ResourceState        func(context.Context) (rcmgr.ResourceManagerStat, error)             `perm:"admin"`
-		PubSubPeers          func(ctx context.Context, topic string) ([]peer.ID, error)           `perm:"read"`
+		PubSubPeers          func(ctx context.Context, topic string) ([]peer.ID, error)           `perm:"admin"`
 	}
 }
 
