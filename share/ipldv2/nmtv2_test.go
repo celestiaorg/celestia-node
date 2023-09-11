7237ad7c74e4e4bbaeadefeefc8c659c68679db6
Wondertan <hlibwondertan@gmail.com>
2023-09-11 23:03:15 +0200

    feat(modp2p): listen on WebTransport by default

--- share/ipldv2/nmtv2_test.go
+++ share/ipldv2/nmtv2_test.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/celestiaorg/rsmt2d"
 
-	"github.com/celestiaorg/celestia-node/header/headertest"
 	"github.com/celestiaorg/celestia-node/share"
 	availability_test "github.com/celestiaorg/celestia-node/share/availability/test"
 	"github.com/celestiaorg/celestia-node/share/eds"
@@ -23,13 +22,10 @@ func TestV2Roundtrip(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	store := headertest.NewStore(t)
-
 	mh.Register(multihashCode, func() hash.Hash {
-		return &Hasher{get: store}
+		return &Hasher{}
 	})
 
-	height := uint64(1)
 	shrIdx := 5
 
 	dn := availability_test.NewTestDAGNet(ctx, t)
@@ -41,24 +37,19 @@ func TestV2Roundtrip(t *testing.T) {
 	root, err := share.NewRoot(square)
 	require.NoError(t, err)
 
-	hdr, err := store.GetByHeight(ctx, height)
-	require.NoError(t, err)
-	hdr.DAH = root
-	hdr.DataHash = root.Hash()
-
 	file, err := eds.CreateFile(t.TempDir()+"/eds_file", square)
 	require.NoError(t, err)
 
 	shr, prf, err := file.ShareWithProof(shrIdx, rsmt2d.Row)
 	require.NoError(t, err)
 
-	nd, err := MakeNode(5, shr, prf, hdr.DataHash, hdr.Height())
+	nd, err := MakeNode(root, rsmt2d.Row, shrIdx, shr, prf)
 	require.NoError(t, err)
 
 	err = srv1.AddBlock(ctx, nd)
 	require.NoError(t, err)
 
-	cid, err := ShareCID(root.Hash(), hdr.Height(), shrIdx)
+	cid, err := ShareCID(root, shrIdx, rsmt2d.Row)
 	require.NoError(t, err)
 	require.True(t, cid.Equals(nd.Cid()))
 
