43a5ff3f5861f23ed4247dab99f54596c192108e
hkey <81494947+hkey0@users.noreply.github.com>
2023-11-01 03:00:27 +0300

    Update config.go

--- api/gateway/config.go
+++ api/gateway/config.go
@@ -14,7 +14,7 @@ type Config struct {
 func DefaultConfig() Config {
 	return Config{
 		Address: "0.0.0.0",
-		// do NOT expose the same port as celestia-core by default so that both can run on the same machine
+		// do    NOT expose the same port as celestia-core by default so that both can run on the same machine
 		Port: "26658",
 	}
 }
