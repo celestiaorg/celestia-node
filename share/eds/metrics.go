84e8acf34f0b840e9f769c0fda7e800506d78cea
Alireza Abbasnia <cyneptic@gmail.com>
2023-11-19 15:00:29 +0330

    refactor(share/eds): apply DRY principle for context resetting on error

--- share/eds/metrics.go
+++ share/eds/metrics.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/celestiaorg/celestia-node/libs/utils"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
@@ -27,9 +28,7 @@ const (
 	dagstoreShardStatusKey = "shard_status"
 )
 
-var (
-	meter = otel.Meter("eds_store")
-)
+var meter = otel.Meter("eds_store")
 
 type putResult string
 
@@ -163,9 +162,7 @@ func (m *metrics) observeGCtime(ctx context.Context, dur time.Duration, failed b
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 	m.gcTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
 }
@@ -174,9 +171,7 @@ func (m *metrics) observeShardFailure(ctx context.Context, shardKey string) {
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.shardFailureCount.Add(ctx, 1, metric.WithAttributes(attribute.String("shard_key", shardKey)))
 }
@@ -185,9 +180,7 @@ func (m *metrics) observePut(ctx context.Context, dur time.Duration, result putR
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.putTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.String(putResultKey, string(result)),
@@ -198,9 +191,7 @@ func (m *metrics) observeLongOp(ctx context.Context, opName string, dur time.Dur
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.longOpTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.String(opNameKey, opName),
@@ -211,9 +202,7 @@ func (m *metrics) observeGetCAR(ctx context.Context, dur time.Duration, failed b
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.getCARTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
@@ -223,9 +212,7 @@ func (m *metrics) observeCARBlockstore(ctx context.Context, dur time.Duration, f
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.getCARBlockstoreTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
@@ -235,9 +222,7 @@ func (m *metrics) observeGetDAH(ctx context.Context, dur time.Duration, failed b
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.getDAHTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
@@ -247,9 +232,7 @@ func (m *metrics) observeRemove(ctx context.Context, dur time.Duration, failed b
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.removeTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
@@ -259,9 +242,7 @@ func (m *metrics) observeGet(ctx context.Context, dur time.Duration, failed bool
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.getTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
@@ -271,9 +252,7 @@ func (m *metrics) observeHas(ctx context.Context, dur time.Duration, failed bool
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.hasTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
@@ -283,9 +262,7 @@ func (m *metrics) observeList(ctx context.Context, dur time.Duration, failed boo
 	if m == nil {
 		return
 	}
-	if ctx.Err() != nil {
-		ctx = context.Background()
-	}
+	ctx = utils.ResetContextOnError(ctx)
 
 	m.listTime.Record(ctx, dur.Seconds(), metric.WithAttributes(
 		attribute.Bool(failedKey, failed)))
