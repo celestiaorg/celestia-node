ee7552fdc3c7cf3c6e141848d72654afb23aa793
Atreay Kukanur <66585295+ATREAY@users.noreply.github.com>
2023-11-27 09:30:21 +0000

    added network version

--- nodebuilder/p2p/host.go
+++ nodebuilder/p2p/host.go
@@ -41,6 +41,8 @@ func routedHost(base HostBase, r routing.PeerRouting) hst.Host {
 
 // host returns constructor for Host.
 func host(params hostParams) (HostBase, error) {
+	buildInfo := node.GetBuildInfo()
+	userAgent := fmt.Sprintf("%s/%s", params.Net, buildInfo.SemanticVersion)
 	opts := []libp2p.Option{
 		libp2p.NoListenAddrs, // do not listen automatically
 		libp2p.AddrsFactory(params.AddrF),
@@ -48,7 +50,7 @@ func host(params hostParams) (HostBase, error) {
 		libp2p.Peerstore(params.PStore),
 		libp2p.ConnectionManager(params.ConnMngr),
 		libp2p.ConnectionGater(params.ConnGater),
-		libp2p.UserAgent(fmt.Sprintf("%s", params.Net)),
+		libp2p.UserAgent(userAgent),
 		libp2p.NATPortMap(), // enables upnp
 		libp2p.DisableRelay(),
 		libp2p.BandwidthReporter(params.Bandwidth),
