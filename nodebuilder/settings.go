ea627ee1810b949b3f6a4f09700c9a314a034aae
Wondertan <hlibwondertan@gmail.com>
2023-11-29 16:15:04 +0100

    chore(node): tweak pyroscope options

--- nodebuilder/settings.go
+++ nodebuilder/settings.go
@@ -52,6 +52,7 @@ func WithPyroscope(endpoint string, nodeType node.Type) fx.Option {
 	return fx.Options(
 		fx.Invoke(func(peerID peer.ID) error {
 			_, err := pyroscope.Start(pyroscope.Config{
+				UploadRate:      15 * time.Second,
 				ApplicationName: "celestia.da-node",
 				ServerAddress:   endpoint,
 				Tags: map[string]string{
@@ -65,6 +66,7 @@ func WithPyroscope(endpoint string, nodeType node.Type) fx.Option {
 					pyroscope.ProfileAllocSpace,
 					pyroscope.ProfileInuseObjects,
 					pyroscope.ProfileInuseSpace,
+					pyroscope.ProfileGoroutines,
 				},
 			})
 			return err
