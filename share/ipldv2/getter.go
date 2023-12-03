9a8b5ed020d182d19f7342ed4edd742ef0a33a44
Wondertan <hlibwondertan@gmail.com>
2023-12-03 22:50:44 +0100

    implement Getter and tests for it

---
+++ share/ipldv2/getter.go
@@ -0,0 +1,181 @@
+package ipldv2
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/ipfs/boxo/blockservice"
+	"github.com/ipfs/go-cid"
+
+	"github.com/celestiaorg/celestia-app/pkg/wrapper"
+	"github.com/celestiaorg/rsmt2d"
+
+	"github.com/celestiaorg/celestia-node/header"
+	"github.com/celestiaorg/celestia-node/share"
+)
+
+type Getter struct {
+	bget blockservice.BlockGetter
+}
+
+func NewGetter(bget blockservice.BlockGetter) *Getter {
+	return &Getter{bget: bget}
+}
+
+// GetShare
+// TODO: Deprecate this method
+func (g *Getter) GetShare(ctx context.Context, hdr *header.ExtendedHeader, row, col int) (share.Share, error) {
+	shrIdx := row*len(hdr.DAH.RowRoots) + col
+	cid := MustSampleCID(shrIdx, hdr.DAH, hdr.Height())
+	blk, err := g.bget.GetBlock(ctx, cid)
+	if err != nil {
+		return nil, fmt.Errorf("getting block from blockservice: %w", err)
+	}
+
+	smpl, err := SampleFromBlock(blk)
+	if err != nil {
+		return nil, fmt.Errorf("converting block to Sample: %w", err)
+	}
+
+	return smpl.SampleShare, nil
+}
+
+// GetShares
+// Guarantee that the returned shares are in the same order as shrIdxs.
+func (g *Getter) GetShares(ctx context.Context, hdr *header.ExtendedHeader, shrIdxs ...int) ([]share.Share, error) {
+	maxIdx := len(hdr.DAH.RowRoots) * len(hdr.DAH.ColumnRoots)
+	cids := make([]cid.Cid, len(shrIdxs))
+	for i, shrIdx := range shrIdxs {
+		if shrIdx < 0 || shrIdx >= maxIdx {
+			return nil, fmt.Errorf("share index %d is out of bounds", shrIdx)
+		}
+		cids[i] = MustSampleCID(shrIdx, hdr.DAH, hdr.Height())
+	}
+
+	smpls := make(map[int]*Sample, len(shrIdxs))
+	blkCh := g.bget.GetBlocks(ctx, cids)
+	for blk := range blkCh { // NOTE: GetBlocks handles ctx, so we don't have to
+		smpl, err := SampleFromBlock(blk)
+		if err != nil {
+			// NOTE: Should never error in fact, as Hasher already validated the block
+			return nil, fmt.Errorf("converting block to Sample: %w", err)
+		}
+
+		shrIdx := int(smpl.SampleID.AxisIndex)*len(hdr.DAH.RowRoots) + int(smpl.SampleID.ShareIndex)
+		smpls[shrIdx] = smpl
+	}
+
+	if len(smpls) != len(shrIdxs) {
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+		return nil, fmt.Errorf("not all shares were found")
+	}
+
+	shrs := make([]share.Share, len(shrIdxs))
+	for i, shrIdx := range shrIdxs {
+		shrs[i] = smpls[shrIdx].SampleShare
+	}
+
+	return shrs, nil
+}
+
+// GetEDS
+// TODO(@Wondertan): Consider requesting randomized rows and cols instead of ODS only
+func (g *Getter) GetEDS(ctx context.Context, hdr *header.ExtendedHeader) (*rsmt2d.ExtendedDataSquare, error) {
+	sqrLn := len(hdr.DAH.RowRoots)
+	cids := make([]cid.Cid, sqrLn/2)
+	for i := 0; i < sqrLn/2; i++ {
+		cids[i] = MustAxisCID(rsmt2d.Row, i, hdr.DAH, hdr.Height())
+	}
+
+	square, err := rsmt2d.NewExtendedDataSquare(
+		share.DefaultRSMT2DCodec(),
+		wrapper.NewConstructor(uint64(sqrLn/2)), uint(sqrLn),
+		share.Size,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	blkCh := g.bget.GetBlocks(ctx, cids)
+	for blk := range blkCh { // NOTE: GetBlocks handles ctx, so we don't have to
+		axis, err := AxisFromBlock(blk)
+		if err != nil {
+			// NOTE: Should never error in fact, as Hasher already validated the block
+			return nil, fmt.Errorf("converting block to Axis: %w", err)
+		}
+
+		for shrIdx, shr := range axis.AxisShares {
+			err = square.SetCell(uint(axis.AxisIndex), uint(shrIdx), shr)
+			if err != nil {
+				panic(err) // this should never happen and if it is... something is really wrong
+			}
+		}
+	}
+
+	// TODO(@Wondertan): Figure out a way to avoid recompute of what has been already computed
+	//  during verification in AxisHasher
+	err = square.Repair(hdr.DAH.RowRoots, hdr.DAH.ColumnRoots)
+	if err != nil {
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+		return nil, fmt.Errorf("repairing EDS: %w", err)
+	}
+
+	return square, nil
+}
+
+func (g *Getter) GetSharesByNamespace(
+	ctx context.Context,
+	hdr *header.ExtendedHeader,
+	ns share.Namespace,
+) (share.NamespacedShares, error) {
+	if err := ns.ValidateForData(); err != nil {
+		return nil, err
+	}
+
+	var cids []cid.Cid //nolint:prealloc// we don't know how many rows with needed namespace there are
+	for rowIdx, rowRoot := range hdr.DAH.RowRoots {
+		if ns.IsOutsideRange(rowRoot, rowRoot) {
+			continue
+		}
+
+		cids = append(cids, MustDataCID(rowIdx, hdr.DAH, hdr.Height(), ns))
+	}
+	if len(cids) == 0 {
+		return share.NamespacedShares{}, nil
+	}
+
+	datas := make([]*Data, 0, len(cids))
+	blkCh := g.bget.GetBlocks(ctx, cids)
+	for blk := range blkCh { // NOTE: GetBlocks handles ctx, so we don't have to
+		data, err := DataFromBlock(blk)
+		if err != nil {
+			// NOTE: Should never error in fact, as Hasher already validated the block
+			return nil, fmt.Errorf("converting block to Data: %w", err)
+		}
+
+		datas = append(datas, data)
+	}
+
+	slices.SortFunc(datas, func(a, b *Data) int {
+		if a.DataID.AxisIndex < b.DataID.AxisIndex {
+			return -1
+		}
+		return 1
+	})
+
+	nShrs := make(share.NamespacedShares, len(datas))
+	for i, row := range datas {
+		nShrs[i] = share.NamespacedRow{
+			Shares: row.DataShares,
+			Proof:  &row.DataProof,
+		}
+	}
+
+	// NOTE: We don't need to call Verify here as Bitswap already did it for us internal.
+	return nShrs, nil
+}
---
+++ share/ipldv2/getter_test.go
@@ -0,0 +1,146 @@
+package ipldv2
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/celestiaorg/nmt"
+
+	"github.com/celestiaorg/celestia-node/header"
+	"github.com/celestiaorg/celestia-node/share"
+	"github.com/celestiaorg/celestia-node/share/eds/edstest"
+	"github.com/celestiaorg/celestia-node/share/ipld"
+	"github.com/celestiaorg/celestia-node/share/sharetest"
+)
+
+func TestGetter(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ns := sharetest.RandV0Namespace()
+	square, root := edstest.RandEDSWithNamespace(t, ns, 16)
+	hdr := &header.ExtendedHeader{DAH: root}
+
+	b := edsBlockstore(square)
+	bserv := NewBlockService(b, nil)
+	get := NewGetter(bserv)
+
+	t.Run("GetShares", func(t *testing.T) {
+		idxs := rand.Perm(int(square.Width() ^ 2))[:30]
+		shrs, err := get.GetShares(ctx, hdr, idxs...)
+		assert.NoError(t, err)
+
+		for i, shrs := range shrs {
+			idx := idxs[i]
+			x, y := uint(idx)/square.Width(), uint(idx)%square.Width()
+			cell := square.GetCell(x, y)
+			ok := bytes.Equal(cell, shrs)
+			require.True(t, ok)
+		}
+	})
+
+	t.Run("GetEDS", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		t.Cleanup(cancel)
+
+		eds, err := get.GetEDS(ctx, hdr)
+		assert.NoError(t, err)
+		assert.NotNil(t, eds)
+
+		ok := eds.Equals(square)
+		assert.True(t, ok)
+	})
+
+	t.Run("GetSharesByNamespace", func(t *testing.T) {
+		nshrs, err := get.GetSharesByNamespace(ctx, hdr, ns)
+		assert.NoError(t, err)
+		assert.NoError(t, nshrs.Verify(root, ns))
+		assert.NotEmpty(t, nshrs.Flatten())
+
+		t.Run("NamespaceOutsideOfRoot", func(t *testing.T) {
+			randNamespace := sharetest.RandV0Namespace()
+			emptyShares, err := get.GetSharesByNamespace(ctx, hdr, randNamespace)
+			assert.NoError(t, err)
+			assert.Empty(t, emptyShares)
+			assert.NoError(t, emptyShares.Verify(root, randNamespace))
+			assert.Empty(t, emptyShares.Flatten())
+		})
+
+		t.Run("NamespaceInsideOfRoot", func(t *testing.T) {
+			square := edstest.RandEDS(t, 8)
+			root, err := share.NewRoot(square)
+			require.NoError(t, err)
+			hdr := &header.ExtendedHeader{DAH: root}
+
+			b := edsBlockstore(square)
+			bserv := NewBlockService(b, nil)
+			get := NewGetter(bserv)
+
+			maxNs := nmt.MaxNamespace(root.RowRoots[(len(root.RowRoots))/2-1], share.NamespaceSize)
+			ns, err := addToNamespace(maxNs, -1)
+			require.NoError(t, err)
+			require.Len(t, ipld.FilterRootByNamespace(root, ns), 1)
+
+			emptyShares, err := get.GetSharesByNamespace(ctx, hdr, ns)
+			assert.NoError(t, err)
+			assert.NotNil(t, emptyShares[0].Proof)
+			assert.NoError(t, emptyShares.Verify(root, ns))
+			assert.Empty(t, emptyShares.Flatten())
+		})
+	})
+}
+
+// addToNamespace adds arbitrary int value to namespace, treating namespace as big-endian
+// implementation of int
+// TODO: dedup with getters/shrex_test.go
+func addToNamespace(namespace share.Namespace, val int) (share.Namespace, error) {
+	if val == 0 {
+		return namespace, nil
+	}
+	// Convert the input integer to a byte slice and add it to result slice
+	result := make([]byte, len(namespace))
+	if val > 0 {
+		binary.BigEndian.PutUint64(result[len(namespace)-8:], uint64(val))
+	} else {
+		binary.BigEndian.PutUint64(result[len(namespace)-8:], uint64(-val))
+	}
+
+	// Perform addition byte by byte
+	var carry int
+	for i := len(namespace) - 1; i >= 0; i-- {
+		sum := 0
+		if val > 0 {
+			sum = int(namespace[i]) + int(result[i]) + carry
+		} else {
+			sum = int(namespace[i]) - int(result[i]) + carry
+		}
+
+		switch {
+		case sum > 255:
+			carry = 1
+			sum -= 256
+		case sum < 0:
+			carry = -1
+			sum += 256
+		default:
+			carry = 0
+		}
+
+		result[i] = uint8(sum)
+	}
+
+	// Handle any remaining carry
+	if carry != 0 {
+		return nil, fmt.Errorf("namespace overflow")
+	}
+
+	return result, nil
+}
