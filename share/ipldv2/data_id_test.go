a28cfefc537316952c5c64504755a91b748f9da7
Wondertan <hlibwondertan@gmail.com>
2023-12-03 13:25:34 +0100

    fix data id test

--- share/ipldv2/data_id_test.go
+++ share/ipldv2/data_id_test.go
@@ -6,23 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/celestiaorg/celestia-node/share"
 	"github.com/celestiaorg/celestia-node/share/eds/edstest"
 	"github.com/celestiaorg/celestia-node/share/sharetest"
 )
 
 // TODO: Add test that AxisType is not serialized
 func TestDataID(t *testing.T) {
-	square := edstest.RandEDS(t, 2)
-	root, err := share.NewRoot(square)
-	require.NoError(t, err)
+	ns := sharetest.RandV0Namespace()
+	_, root := edstest.RandEDSWithNamespace(t, ns, 4)
 
-	sid := NewDataID(2, root, 1, sharetest.RandV0Namespace())
+	sid := NewDataID(2, root, 1, ns)
 	id, err := sid.Cid()
 	require.NoError(t, err)
 
-	assert.EqualValues(t, sampleCodec, id.Prefix().Codec)
-	assert.EqualValues(t, sampleMultihashCode, id.Prefix().MhType)
+	assert.EqualValues(t, dataCodec, id.Prefix().Codec)
+	assert.EqualValues(t, dataMultihashCode, id.Prefix().MhType)
 	assert.EqualValues(t, DataIDSize, id.Prefix().MhLength)
 
 	data, err := sid.MarshalBinary()
