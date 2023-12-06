82150f296b1d4c8d6301609f04557d2b29491f21
Vlad <13818348+walldiss@users.noreply.github.com>
2023-12-06 23:30:32 +0800

    feat(store/mem_file): add in-memory eds file implementation (#2992)
    Adds in-memory file implementation

--- share/getter.go
+++ share/getter.go
@@ -72,16 +72,19 @@ func (ns NamespacedShares) Verify(root *Root, namespace Namespace) error {
 	}
 
 	for i, row := range ns {
+		if row.Proof == nil && row.Shares == nil {
+			return fmt.Errorf("row verification failed: no proofs and shares")
+		}
 		// verify row data against row hash from original root
-		if !row.verify(originalRoots[i], namespace) {
+		if !row.Verify(originalRoots[i], namespace) {
 			return fmt.Errorf("row verification failed: row %d doesn't match original root: %s", i, root.String())
 		}
 	}
 	return nil
 }
 
-// verify validates the row using nmt inclusion proof.
-func (row *NamespacedRow) verify(rowRoot []byte, namespace Namespace) bool {
+// Verify validates the row using nmt inclusion proof.
+func (row *NamespacedRow) Verify(rowRoot []byte, namespace Namespace) bool {
 	// construct nmt leaves from shares by prepending namespace
 	leaves := make([][]byte, 0, len(row.Shares))
 	for _, shr := range row.Shares {
