f74111da6184b3821a2217d194c65c5a0b894341
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-12-08 13:07:40 +0200

    fix(cmd): explicitly set a string representation of the error

--- cmd/util.go
+++ cmd/util.go
@@ -24,7 +24,7 @@ import (
 func PrintOutput(data interface{}, err error, formatData func(interface{}) interface{}) error {
 	switch {
 	case err != nil:
-		data = err
+		data = err.Error()
 	case formatData != nil:
 		data = formatData(data)
 	}
