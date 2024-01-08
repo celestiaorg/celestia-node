89621d9d18cce122338d0bc089dd6202f1760d66
Ryan <ryanford@poluglottos.com>
2024-01-08 06:57:11 +0100

    fix(pruner/full): short-circuiting on Prune

--- pruner/full/pruner.go
+++ pruner/full/pruner.go
@@ -6,6 +6,7 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/celestiaorg/celestia-node/header"
+	"github.com/celestiaorg/celestia-node/share"
 	"github.com/celestiaorg/celestia-node/share/eds"
 )
 
@@ -22,6 +23,11 @@ func NewPruner(store *eds.Store) *Pruner {
 }
 
 func (p *Pruner) Prune(ctx context.Context, eh *header.ExtendedHeader) error {
+	// short circuit on empty roots
+	if eh.DAH.Equals(share.EmptyRoot()) {
+		return nil
+	}
+
 	log.Debugf("pruning header %s", eh.DAH.Hash())
 	return p.store.Remove(ctx, eh.DAH.Hash())
 }
