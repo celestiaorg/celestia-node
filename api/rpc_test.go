2294805ce4146058e9777976de08600354762ad4
Artem Bogomaz <artembogomaz@gmail.com>
2024-01-19 22:59:38 +0700

    chore(nodebuilder/state): SubmitTx endpoint takes read-level permissions (#3072)
    <!--
    Thank you for submitting a pull request!
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    
    Please ensure you've addressed or included references to any related
    issues.
    
    Tips:
    - Use keywords like "closes" or "fixes" followed by an issue number to
    automatically close related issues when the PR is merged (e.g., "closes
    #123" or "fixes #123").
    - Describe the changes made in the PR.
    - Ensure the PR has one of the required tags (kind:fix, kind:misc,
    kind:break!, kind:refactor, kind:feat, kind:deps, kind:docs, kind:ci,
    kind:chore, kind:testing)
    
    -->
    refactor: changes SubmitTX permissions from write to read.
    
    Following the issue requirement, this PR changes SubmitTX permissions
    from _write_ to _read_
    
    Changes:
    
    * Updated nodebuilder/state/state.go from **write** to **read**
    permissions
    * Updated following tests (api/rpc_test.go) and added extra check for
    SubmitTX rpc call (read permissions check).
    
    closes #2958
    
    ---------
    
    Co-authored-by: [NODERS]TEAM <office@noders.team>
    Co-authored-by: ramin <raminkeene@gmail.com>

--- api/rpc_test.go
+++ api/rpc_test.go
@@ -186,12 +186,15 @@ func TestAuthedRPC(t *testing.T) {
 			// 2. Test method with write-level permissions
 			expectedResp := &state.TxResponse{}
 			if tt.perm > 2 {
-				server.State.EXPECT().SubmitTx(gomock.Any(), gomock.Any()).Return(expectedResp, nil)
-				txResp, err := rpcClient.State.SubmitTx(ctx, []byte{})
+				server.State.EXPECT().Delegate(gomock.Any(), gomock.Any(),
+					gomock.Any(), gomock.Any(), gomock.Any()).Return(expectedResp, nil)
+				txResp, err := rpcClient.State.Delegate(ctx,
+					state.ValAddress{}, state.Int{}, state.Int{}, 0)
 				require.NoError(t, err)
 				require.Equal(t, expectedResp, txResp)
 			} else {
-				_, err := rpcClient.State.SubmitTx(ctx, []byte{})
+				_, err := rpcClient.State.Delegate(ctx,
+					state.ValAddress{}, state.Int{}, state.Int{}, 0)
 				require.Error(t, err)
 				require.ErrorContains(t, err, "missing permission")
 			}
