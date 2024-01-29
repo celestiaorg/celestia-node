85021e9d31c4673f30d9bddbf21746a5674be698
Ryan <ryanford@poluglottos.com>
2024-01-29 09:38:55 +0100

    adding comment

--- nodebuilder/node/admin.go
+++ nodebuilder/node/admin.go
@@ -39,6 +39,10 @@ func (m *module) Info(context.Context) (Info, error) {
 }
 
 func (m *module) Ready(context.Context) (bool, error) {
+	// Because the node uses FX to provide the RPC last, all services' lifecycles have been started by
+	// the point this endpoint is available. It is not 100% guaranteed at this point that all services
+	// are fully ready, but it is very high probability and all endpoints are available at this point
+	// regardless.
 	return true, nil
 }
 
