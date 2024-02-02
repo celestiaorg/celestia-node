2e9bf7fdff5d6777c02540f1279dcf845bd52c81
rene <41963722+renaynay@users.noreply.github.com>
2024-02-02 18:49:32 +0100

    fix | test(pruner): Make TestService more deterministic, Make some other tests deterministic and test one more case for retries

--- pruner/service.go
+++ pruner/service.go
@@ -16,13 +16,7 @@ import (
 
 var log = logging.Logger("pruner/service")
 
-/*
-	TODO:
-		* failed headers retry loop / jobs
-*/
-
-// Service handles the pruning routine for the node using the
-// prune Pruner.
+// Service handles running the pruning cycle for the node.
 type Service struct {
 	pruner Pruner
 	window AvailabilityWindow
@@ -83,7 +77,7 @@ func (s *Service) Start(context.Context) error {
 	}
 	log.Debugw("loaded checkpoint", "lastPruned", s.lastPruned().Height())
 
-	go s.prune()
+	go s.run()
 	return nil
 }
 
@@ -98,7 +92,7 @@ func (s *Service) Stop(ctx context.Context) error {
 	}
 }
 
-func (s *Service) prune() {
+func (s *Service) run() {
 	if s.params.gcCycle == time.Duration(0) {
 		// Service is disabled, exit
 		close(s.doneCh)
@@ -108,9 +102,6 @@ func (s *Service) prune() {
 	ticker := time.NewTicker(s.params.gcCycle)
 	defer ticker.Stop()
 
-	// prioritize retrying failed headers
-	s.retryFailed(s.ctx)
-
 	lastPrunedHeader := s.lastPruned()
 
 	for {
@@ -119,44 +110,55 @@ func (s *Service) prune() {
 			close(s.doneCh)
 			return
 		case <-ticker.C:
-			headers, err := s.findPruneableHeaders(s.ctx)
-			if err != nil || len(headers) == 0 {
-				// TODO @renaynay: record errors properly
-				log.Errorw("failed to find prune-able blocks", "error", err)
-				continue
-			}
-
-			failed := make(map[uint64]error)
-
-			log.Debugw("pruning headers", "from", headers[0].Height(), "to",
-				headers[len(headers)-1].Height())
-
-			for _, eh := range headers {
-				pruneCtx, cancel := context.WithDeadline(s.ctx, time.Now().Add(time.Second))
-
-				err = s.pruner.Prune(pruneCtx, eh)
-				if err != nil {
-					log.Errorw("failed to prune block", "height", eh.Height(), "err", err)
-					failed[eh.Height()] = err
-				} else {
-					// TODO @renaynay: make prettier
-					lastPrunedHeader = eh
-				}
-
-				s.metrics.observePrune(pruneCtx, err != nil)
-				cancel()
-			}
-
-			err = s.updateCheckpoint(s.ctx, lastPrunedHeader, failed)
-			if err != nil {
-				log.Errorw("failed to update checkpoint", "err", err)
-				continue
-			}
-
-			log.Debugw("retrying failed headers", "amount", len(s.checkpoint.FailedHeaders))
-			s.retryFailed(s.ctx) // TODO @renaynay: persist the results of this to disk
+			lastPrunedHeader = s.prune(s.ctx, lastPrunedHeader)
 		}
 	}
+}
+
+func (s *Service) prune(
+	ctx context.Context,
+	lastPrunedHeader *header.ExtendedHeader,
+) *header.ExtendedHeader {
+	// prioritize retrying previously-failed headers
+	s.retryFailed(s.ctx)
+
+	headers, err := s.findPruneableHeaders(ctx)
+	if err != nil || len(headers) == 0 {
+		// TODO @renaynay: record errors properly
+		log.Errorw("failed to find prune-able blocks", "error", err)
+		return lastPrunedHeader
+	}
+
+	failed := make(map[uint64]error)
+
+	log.Debugw("pruning headers", "from", headers[0].Height(), "to",
+		headers[len(headers)-1].Height())
+
+	for _, eh := range headers {
+		pruneCtx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second))
+
+		err = s.pruner.Prune(pruneCtx, eh)
+		if err != nil {
+			log.Errorw("failed to prune block", "height", eh.Height(), "err", err)
+			failed[eh.Height()] = err
+		} else {
+			// TODO @renaynay: make prettier
+			lastPrunedHeader = eh
+		}
+
+		s.metrics.observePrune(pruneCtx, err != nil)
+		cancel()
+	}
+
+	err = s.updateCheckpoint(s.ctx, lastPrunedHeader, failed)
+	if err != nil {
+		log.Errorw("failed to update checkpoint", "err", err)
+		return lastPrunedHeader
+	}
+
+	log.Debugw("retrying failed headers", "amount", len(s.checkpoint.FailedHeaders))
+
+	return lastPrunedHeader
 }
 
 func (s *Service) retryFailed(ctx context.Context) {
