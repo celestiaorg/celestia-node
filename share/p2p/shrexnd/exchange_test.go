ad117d7e21dfea1b69b189decd9e034562fa2b26
Vlad <vlad@celestia.org>
2024-02-08 15:33:27 +0500

    fix shrexnd tests

--- share/p2p/shrexnd/exchange_test.go
+++ share/p2p/shrexnd/exchange_test.go
@@ -2,19 +2,17 @@ package shrexnd
 
 import (
 	"context"
+	"github.com/celestiaorg/celestia-node/share/store"
 	"sync"
 	"testing"
 	"time"
 
-	"github.com/ipfs/go-datastore"
-	ds_sync "github.com/ipfs/go-datastore/sync"
 	libhost "github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/celestiaorg/celestia-node/share"
-	"github.com/celestiaorg/celestia-node/share/eds"
 	"github.com/celestiaorg/celestia-node/share/eds/edstest"
 	"github.com/celestiaorg/celestia-node/share/p2p"
 	"github.com/celestiaorg/celestia-node/share/sharetest"
@@ -24,16 +22,14 @@ func TestExchange_RequestND_NotFound(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	t.Cleanup(cancel)
 	edsStore, client, server := makeExchange(t)
-	require.NoError(t, edsStore.Start(ctx))
 	require.NoError(t, server.Start(ctx))
 
-	t.Run("CAR_not_exist", func(t *testing.T) {
+	t.Run("File not exist", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(ctx, time.Second)
 		t.Cleanup(cancel)
 
-		root := share.Root{}
 		namespace := sharetest.RandV0Namespace()
-		_, err := client.RequestND(ctx, &root, namespace, server.host.ID())
+		_, err := client.RequestND(ctx, 666, 1, 2, namespace, server.host.ID())
 		require.ErrorIs(t, err, p2p.ErrNotFound)
 	})
 
@@ -43,11 +39,15 @@ func TestExchange_RequestND_NotFound(t *testing.T) {
 
 		eds := edstest.RandEDS(t, 4)
 		dah, err := share.NewRoot(eds)
+		height := uint64(42)
 		require.NoError(t, err)
-		require.NoError(t, edsStore.Put(ctx, dah.Hash(), eds))
+		f, err := edsStore.Put(ctx, dah.Hash(), height, eds)
+		require.NoError(t, err)
+		require.NoError(t, f.Close())
 
 		namespace := sharetest.RandV0Namespace()
-		emptyShares, err := client.RequestND(ctx, dah, namespace, server.host.ID())
+		fromRow, toRow := share.RowRangeForNamespace(dah, namespace)
+		emptyShares, err := client.RequestND(ctx, height, fromRow, toRow, namespace, server.host.ID())
 		require.NoError(t, err)
 		require.Empty(t, emptyShares.Flatten())
 	})
@@ -90,23 +90,22 @@ func TestExchange_RequestND(t *testing.T) {
 		// take server concurrency slots with blocked requests
 		for i := 0; i < rateLimit; i++ {
 			go func(i int) {
-				client.RequestND(ctx, nil, sharetest.RandV0Namespace(), server.host.ID()) //nolint:errcheck
+				client.RequestND(ctx, 1, 1, 2, sharetest.RandV0Namespace(), server.host.ID()) //nolint:errcheck
 			}(i)
 		}
 
 		// wait until all server slots are taken
 		wg.Wait()
-		_, err = client.RequestND(ctx, nil, sharetest.RandV0Namespace(), server.host.ID())
+		_, err = client.RequestND(ctx, 1, 1, 2, sharetest.RandV0Namespace(), server.host.ID())
 		require.ErrorIs(t, err, p2p.ErrRateLimited)
 	})
 }
 
-func newStore(t *testing.T) *eds.Store {
+func newStore(t *testing.T) *store.Store {
 	t.Helper()
 
-	storeCfg := eds.DefaultParameters()
-	ds := ds_sync.MutexWrap(datastore.NewMapDatastore())
-	store, err := eds.NewStore(storeCfg, t.TempDir(), ds)
+	storeCfg := store.DefaultParameters()
+	store, err := store.NewStore(storeCfg, t.TempDir())
 	require.NoError(t, err)
 	return store
 }
@@ -120,7 +119,7 @@ func createMocknet(t *testing.T, amount int) []libhost.Host {
 	return net.Hosts()
 }
 
-func makeExchange(t *testing.T) (*eds.Store, *Client, *Server) {
+func makeExchange(t *testing.T) (*store.Store, *Client, *Server) {
 	t.Helper()
 	store := newStore(t)
 	hosts := createMocknet(t, 2)
