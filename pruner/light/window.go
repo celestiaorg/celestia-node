1ddf91f1e4f05bd505f2360bfebd486865cc9b7c
rene <41963722+renaynay@users.noreply.github.com>
2024-02-28 12:23:19 +0100

    TEST!(pruner | go.mod): needed for robusta testing, TODO revert

--- pruner/light/window.go
+++ pruner/light/window.go
@@ -1,9 +1,11 @@
 package light
 
 import (
+	"time"
+
 	"github.com/celestiaorg/celestia-node/pruner"
 )
 
 // Window is the availability window for light nodes in the Celestia
 // network (30 days).
-const Window = pruner.AvailabilityWindow(30 * 24 * 60 * 60)
+const Window = pruner.AvailabilityWindow(time.Hour)
