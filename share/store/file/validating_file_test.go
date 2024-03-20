777564d8bbef50066274ad7318e920fdde69140a
Vlad <vlad@celestia.org>
2024-03-20 21:01:01 +0400

    fix validation file test

--- share/store/file/validating_file_test.go
+++ share/store/file/validating_file_test.go
@@ -24,15 +24,15 @@ func TestValidatingFile_Share(t *testing.T) {
 		{"ValidIndices", 3, 2, 4, false},
 		{"OutOfBoundsX", 8, 3, 4, true},
 		{"OutOfBoundsY", 3, 8, 4, true},
-		{"NegativeX", -1, 4, 6, true},
-		{"NegativeY", 3, -1, 6, true},
+		{"NegativeX", -1, 4, 8, true},
+		{"NegativeY", 3, -1, 8, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			eds := edstest.RandEDS(t, tt.odsSize)
 			file := &MemFile{Eds: eds}
-			vf := NewValidatingFile(file)
+			vf := WithValidation(file)
 
 			_, err := vf.Share(context.Background(), tt.x, tt.y)
 			if tt.expectFail {
@@ -61,7 +61,7 @@ func TestValidatingFile_AxisHalf(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			eds := edstest.RandEDS(t, tt.odsSize)
 			file := &MemFile{Eds: eds}
-			vf := NewValidatingFile(file)
+			vf := WithValidation(file)
 
 			_, err := vf.AxisHalf(context.Background(), tt.axisType, tt.axisIdx)
 			if tt.expectFail {
@@ -89,7 +89,7 @@ func TestValidatingFile_Data(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			eds := edstest.RandEDS(t, tt.odsSize)
 			file := &MemFile{Eds: eds}
-			vf := NewValidatingFile(file)
+			vf := WithValidation(file)
 
 			ns := sharetest.RandV0Namespace()
 			_, err := vf.Data(context.Background(), ns, tt.rowIdx)
