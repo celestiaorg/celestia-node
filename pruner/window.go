26d411a53334454bc03ac5b84cdca2e349d18478
rene <41963722+renaynay@users.noreply.github.com>
2024-04-10 14:23:29 +0200

    feat(core): Bridge node short-circuits storing a historical EDS if pruning is enabled (#3283)
    This PR implements short-circuiting storing EDSes in the case that they
    are outside the sampling window in the case of a syncing bridge node if
    `--experimental-pruning` is enabled.
    
    Overrides #3261

--- pruner/window.go
+++ pruner/window.go
@@ -5,3 +5,13 @@ import (
 )
 
 type AvailabilityWindow time.Duration
+
+// IsWithinAvailabilityWindow checks whether the given timestamp is within the
+// given AvailabilityWindow. If the window is disabled (0), it returns true for
+// every timestamp.
+func IsWithinAvailabilityWindow(t time.Time, window AvailabilityWindow) bool {
+	if window == AvailabilityWindow(time.Duration(0)) {
+		return true
+	}
+	return time.Since(t) <= time.Duration(window)
+}
