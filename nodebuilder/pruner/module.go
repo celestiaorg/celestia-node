ec6999162c6b99a25b22d68eaf8512ce65428990
Wondertan <hlibwondertan@gmail.com>
2024-05-10 14:10:25 +0200

    feat(pruner/light): implement light pruning

--- nodebuilder/pruner/module.go
+++ nodebuilder/pruner/module.go
@@ -43,6 +43,7 @@ func ConstructModule(tp node.Type, cfg *Config) fx.Option {
 	// TODO: Eventually, light nodes will be capable of pruning samples
 	//  in which case, this can be enabled.
 	case node.Light:
+		fx.Provide(light.NewPruner)
 		return fx.Module("prune",
 			baseComponents,
 		)
--- pruner/light/pruner.go
+++ pruner/light/pruner.go
@@ -3,15 +3,30 @@ package light
 import (
 	"context"
 
+	"github.com/ipfs/boxo/blockservice"
+
 	"github.com/celestiaorg/celestia-node/header"
+	"github.com/celestiaorg/celestia-node/share/ipld"
 )
 
-type Pruner struct{}
-
-func NewPruner() *Pruner {
-	return &Pruner{}
+type Pruner struct {
+	bserv blockservice.BlockService
 }
 
-func (p *Pruner) Prune(context.Context, *header.ExtendedHeader) error {
+func NewPruner(bserv blockservice.BlockService) *Pruner {
+	return &Pruner{bserv: bserv}
+}
+
+func (p *Pruner) Prune(ctx context.Context, h *header.ExtendedHeader) error {
+	var roots [][]byte
+	roots = append(roots, h.DAH.RowRoots...)
+	roots = append(roots, h.DAH.ColumnRoots...)
+	for _, root := range roots {
+		cid := ipld.MustCidFromNamespacedSha256(root)
+		if err := ipld.DeleteNode(ctx, p.bserv, cid); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
