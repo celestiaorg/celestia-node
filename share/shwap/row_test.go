f0685c917aefeb5831da5ce017d8e581f1ae1e0c
Vlad <13818348+walldiss@users.noreply.github.com>
2024-05-24 21:25:59 +0300

    feat(share): Shwap types tests (#3423)
    Adds tests for shwap types and associated functions.
    
    There are no non-inclusion tests, because the test function
    `NamespacedRowFromShares` for creating test data does not support the
    creation of non-inclusion proofs. The non-inclusion logic itself exists
    and will be covered at the level of integration tests.
    `NamespacedRowFromShares` is not used in the shwap and may only be
    needed for more convenient testing. The Store implementation will use
    its own proof-building engine, which supports non-inclusion proofs.
    
    For now, I see no reason to fuss with this. To add these unit tests,
    either a lot of duplicative NMT code needs to be written, or access to
    the NMT tree inside the app wrapper must be provided. The app wrapper
    will need to be redesigned later for a “clean” implementation of proof
    caching.
    
    
    Based on https://github.com/celestiaorg/celestia-node/pull/3383

---
+++ share/shwap/row_test.go
@@ -0,0 +1,114 @@
+package shwap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/celestiaorg/celestia-node/share"
+	"github.com/celestiaorg/celestia-node/share/eds/edstest"
+)
+
+func TestNewRowFromEDS(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+
+	for rowIdx := 0; rowIdx < odsSize*2; rowIdx++ {
+		for _, side := range []RowSide{Left, Right} {
+			row := RowFromEDS(eds, rowIdx, side)
+
+			want := eds.Row(uint(rowIdx))
+			shares, err := row.Shares()
+			require.NoError(t, err)
+			require.Equal(t, want, shares)
+
+			var half []share.Share
+			if side == Right {
+				half = want[odsSize:]
+			} else {
+				half = want[:odsSize]
+			}
+			require.Equal(t, half, row.halfShares)
+			require.Equal(t, side, row.side)
+		}
+	}
+}
+
+func TestRowValidate(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+	root, err := share.NewRoot(eds)
+	require.NoError(t, err)
+
+	for rowIdx := 0; rowIdx < odsSize*2; rowIdx++ {
+		for _, side := range []RowSide{Left, Right} {
+			row := RowFromEDS(eds, rowIdx, side)
+
+			err := row.VerifyRoot(root, rowIdx)
+			require.NoError(t, err)
+			err = row.Validate(root, rowIdx)
+			require.NoError(t, err)
+		}
+	}
+}
+
+func TestRowValidateNegativeCases(t *testing.T) {
+	eds := edstest.RandEDS(t, 8) // Generate a random Extended Data Square of size 8
+	root, err := share.NewRoot(eds)
+	require.NoError(t, err)
+	row := RowFromEDS(eds, 0, Left)
+
+	// Test with incorrect side specification
+	invalidSideRow := Row{halfShares: row.halfShares, side: RowSide(999)}
+	err = invalidSideRow.Validate(root, 0)
+	require.Error(t, err, "should error on invalid row side")
+
+	// Test with invalid shares (more shares than expected)
+	incorrectShares := make([]share.Share, (eds.Width()/2)+1) // Adding an extra share
+	for i := range incorrectShares {
+		incorrectShares[i] = eds.GetCell(uint(i), 0)
+	}
+	invalidRow := Row{halfShares: incorrectShares, side: Left}
+	err = invalidRow.Validate(root, 0)
+	require.Error(t, err, "should error on incorrect number of shares")
+
+	// Test with empty shares
+	emptyRow := Row{halfShares: []share.Share{}, side: Left}
+	err = emptyRow.Validate(root, 0)
+	require.Error(t, err, "should error on empty halfShares")
+
+	// Doesn't match root. Corrupt root hash
+	root.RowRoots[0][len(root.RowRoots[0])-1] ^= 0xFF
+	err = row.Validate(root, 0)
+	require.Error(t, err, "should error on invalid root hash")
+}
+
+func TestRowProtoEncoding(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+
+	for rowIdx := 0; rowIdx < odsSize*2; rowIdx++ {
+		for _, side := range []RowSide{Left, Right} {
+			row := RowFromEDS(eds, rowIdx, side)
+
+			pb := row.ToProto()
+			rowOut := RowFromProto(pb)
+			require.Equal(t, row, rowOut)
+		}
+	}
+}
+
+// BenchmarkRowValidate benchmarks the performance of row validation.
+// BenchmarkRowValidate-10    	    9591	    121802 ns/op
+func BenchmarkRowValidate(b *testing.B) {
+	const odsSize = 32
+	eds := edstest.RandEDS(b, odsSize)
+	root, err := share.NewRoot(eds)
+	require.NoError(b, err)
+	row := RowFromEDS(eds, 0, Left)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = row.Validate(root, 0)
+	}
+}
---
+++ share/shwap/sample_test.go
@@ -0,0 +1,127 @@
+package shwap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/celestiaorg/rsmt2d"
+
+	"github.com/celestiaorg/celestia-node/share"
+	"github.com/celestiaorg/celestia-node/share/eds/edstest"
+)
+
+func TestNewSampleFromEDS(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+
+	for _, proofType := range []rsmt2d.Axis{rsmt2d.Row, rsmt2d.Col} {
+		for rowIdx := 0; rowIdx < odsSize*2; rowIdx++ {
+			for colIdx := 0; colIdx < odsSize*2; colIdx++ {
+				sample, err := SampleFromEDS(eds, proofType, rowIdx, colIdx)
+				require.NoError(t, err)
+
+				want := eds.GetCell(uint(rowIdx), uint(colIdx))
+				require.Equal(t, want, sample.Share)
+				require.Equal(t, proofType, sample.ProofType)
+				require.NotNil(t, sample.Proof)
+				require.Equal(t, sample.Proof.End()-sample.Proof.Start(), 1)
+				require.Len(t, sample.Proof.Nodes(), 4)
+			}
+		}
+	}
+}
+
+func TestSampleValidate(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+	root, err := share.NewRoot(eds)
+	require.NoError(t, err)
+
+	for _, proofType := range []rsmt2d.Axis{rsmt2d.Row, rsmt2d.Col} {
+		for rowIdx := 0; rowIdx < odsSize*2; rowIdx++ {
+			for colIdx := 0; colIdx < odsSize*2; colIdx++ {
+				sample, err := SampleFromEDS(eds, proofType, rowIdx, colIdx)
+				require.NoError(t, err)
+
+				require.True(t, sample.VerifyInclusion(root, rowIdx, colIdx))
+				require.NoError(t, sample.Validate(root, rowIdx, colIdx))
+			}
+		}
+	}
+}
+
+// TestSampleNegativeVerifyInclusion checks
+func TestSampleNegativeVerifyInclusion(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+	root, err := share.NewRoot(eds)
+	require.NoError(t, err)
+
+	sample, err := SampleFromEDS(eds, rsmt2d.Row, 0, 0)
+	require.NoError(t, err)
+	included := sample.VerifyInclusion(root, 0, 0)
+	require.True(t, included)
+
+	// incorrect row index
+	included = sample.VerifyInclusion(root, 1, 0)
+	require.False(t, included)
+
+	// incorrect col index is not used in the inclusion proof verification
+	included = sample.VerifyInclusion(root, 0, 1)
+	require.True(t, included)
+
+	// Corrupt the share
+	sample.Share[0] ^= 0xFF
+	included = sample.VerifyInclusion(root, 0, 0)
+	require.False(t, included)
+
+	// incorrect proofType
+	sample, err = SampleFromEDS(eds, rsmt2d.Row, 0, 0)
+	require.NoError(t, err)
+	sample.ProofType = rsmt2d.Col
+	included = sample.VerifyInclusion(root, 0, 0)
+	require.False(t, included)
+
+	// Corrupt the last root hash byte
+	sample, err = SampleFromEDS(eds, rsmt2d.Row, 0, 0)
+	require.NoError(t, err)
+	root.RowRoots[0][len(root.RowRoots[0])-1] ^= 0xFF
+	included = sample.VerifyInclusion(root, 0, 0)
+	require.False(t, included)
+}
+
+func TestSampleProtoEncoding(t *testing.T) {
+	const odsSize = 8
+	eds := edstest.RandEDS(t, odsSize)
+
+	for _, proofType := range []rsmt2d.Axis{rsmt2d.Row, rsmt2d.Col} {
+		for rowIdx := 0; rowIdx < odsSize*2; rowIdx++ {
+			for colIdx := 0; colIdx < odsSize*2; colIdx++ {
+				sample, err := SampleFromEDS(eds, proofType, rowIdx, colIdx)
+				require.NoError(t, err)
+
+				pb := sample.ToProto()
+				sampleOut := SampleFromProto(pb)
+				require.NoError(t, err)
+				require.Equal(t, sample, sampleOut)
+			}
+		}
+	}
+}
+
+// BenchmarkSampleValidate benchmarks the performance of sample validation.
+// BenchmarkSampleValidate-10    	  284829	      3935 ns/op
+func BenchmarkSampleValidate(b *testing.B) {
+	const odsSize = 32
+	eds := edstest.RandEDS(b, odsSize)
+	root, err := share.NewRoot(eds)
+	require.NoError(b, err)
+	sample, err := SampleFromEDS(eds, rsmt2d.Row, 0, 0)
+	require.NoError(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = sample.Validate(root, 0, 0)
+	}
+}
