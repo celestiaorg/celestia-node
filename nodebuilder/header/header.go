2c53cb00bcd6a976d6afd97b7e0d31eda0de1e56
sweexordious <chamirachid1@gmail.com>
2024-07-19 11:57:39 +0100

    docs: document GetDataRootTupleInclusionProof and GetDataRootTupleRoot

--- nodebuilder/header/header.go
+++ nodebuilder/header/header.go
@@ -46,11 +46,13 @@ type Module interface {
 
 	// GetDataRootTupleRoot collects the data roots over a provided ordered range of blocks,
 	// and then creates a new Merkle root of those data roots. The range is end exclusive.
+	// It's in the header module because it only needs access to the headers to generate the proof.
 	GetDataRootTupleRoot(ctx context.Context, start, end uint64) (*DataRootTupleRoot, error)
 
-	// GetDataRootTupleInclusionProof creates an inclusion proof for the data root of block
-	// height `height` in the set of blocks defined by `start` and `end`. The range
+	// GetDataRootTupleInclusionProof creates an inclusion proof, for the data root tuple of block
+	// height `height`, in the set of blocks defined by `start` and `end`. The range
 	// is end exclusive.
+	// It's in the header module because it only needs access to the headers to generate the proof.
 	GetDataRootTupleInclusionProof(
 		ctx context.Context,
 		height int64,
