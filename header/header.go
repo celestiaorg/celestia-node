f6e2d04056a8f4ff73b09a25b844d1b46ef25c83
rene <41963722+renaynay@users.noreply.github.com>
2024-07-23 15:55:06 +0200

    refactor(header): Validate checks that app version is not greater than appconsts latest version

--- header/header.go
+++ header/header.go
@@ -11,8 +11,6 @@ import (
 	"github.com/tendermint/tendermint/light"
 	core "github.com/tendermint/tendermint/types"
 
-	v1 "github.com/celestiaorg/celestia-app/v2/pkg/appconsts/v1"
-	v2 "github.com/celestiaorg/celestia-app/v2/pkg/appconsts/v2"
 	"github.com/celestiaorg/celestia-app/v2/pkg/da"
 	libhead "github.com/celestiaorg/go-header"
 	"github.com/celestiaorg/rsmt2d"
@@ -115,13 +113,9 @@ func (eh *ExtendedHeader) Validate() error {
 		return fmt.Errorf("ValidateBasic error on RawHeader at height %d: %w", eh.Height(), err)
 	}
 
-	if eh.RawHeader.Version.App != v1.Version && eh.RawHeader.Version.App != v2.Version {
-		return fmt.Errorf(
-			"app version mismatch, expected: %d or %d, got %d",
-			v1.Version,
-			v2.Version,
-			eh.RawHeader.Version.App,
-		)
+	if eh.RawHeader.Version.App > appconsts.LatestVersion {
+		return fmt.Errorf("app version must be less than the latest app version %d, got %d",
+			appconsts.LatestVersion, eh.RawHeader.Version.App)
 	}
 
 	err = eh.Commit.ValidateBasic()
