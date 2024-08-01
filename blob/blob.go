afb2293718e9d4413e7761dfe2c697cb83726a77
sweexordious <chamirachid1@gmail.com>
2024-08-01 18:53:47 +0400

    chore: remove errEmptyShares

--- blob/blob.go
+++ blob/blob.go
@@ -18,9 +18,6 @@ import (
 	"github.com/celestiaorg/celestia-node/share"
 )
 
-//nolint:unused
-var errEmptyShares = errors.New("empty shares")
-
 // Proof constructs the proof of a blob to the data root.
 type Proof struct {
 	// ShareToRowRootProof the proofs of the shares to the row roots they belong to.
