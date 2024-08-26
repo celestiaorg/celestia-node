10acab068cac8eac53c737f3317350c06ad1dffb
Vlad <13818348+walldiss@users.noreply.github.com>
2024-08-26 17:38:03 +0200

    refactor(store): remove link to Q4 file and find it by datahash

--- store/store_cache.go
+++ store/store_cache.go
@@ -48,10 +48,10 @@ func (cs *CachedStore) GetByHeight(ctx context.Context, height uint64) (eds.Acce
 }
 
 func (cs *CachedStore) openFile(height uint64) cache.OpenAccessorFn {
-	return func(context.Context) (eds.AccessorStreamer, error) {
+	return func(ctx context.Context) (eds.AccessorStreamer, error) {
 		// open file directly wihout calling GetByHeight of inner getter to
 		// avoid hitting store cache second time
-		path := cs.store.heightToPath(height, "")
-		return cs.store.openAccessor(path)
+		path := cs.store.heightToPath(height, odsFileExt)
+		return cs.store.openAccessor(ctx, path)
 	}
 }
