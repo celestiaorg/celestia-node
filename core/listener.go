8ee3b73536c8139ea7f0e60c69bb81d70acb03d5
Vlad <13818348+walldiss@users.noreply.github.com>
2024-09-25 16:45:44 +0200

    fix(core/listener): wait listener to shutdown before exit (#3775)
    (cherry picked from commit 98a127be070eff51bc53c2679a62f435b9e34aab)

--- core/listener.go
+++ core/listener.go
@@ -51,6 +51,7 @@ type Listener struct {
 
 	listenerTimeout time.Duration
 	cancel          context.CancelFunc
+	closed          chan struct{}
 }
 
 func NewListener(
@@ -99,6 +100,7 @@ func (cl *Listener) Start(context.Context) error {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	cl.cancel = cancel
+	cl.closed = make(chan struct{})
 
 	sub, err := cl.fetcher.SubscribeNewBlockEvent(ctx)
 	if err != nil {
@@ -116,13 +118,25 @@ func (cl *Listener) Stop(ctx context.Context) error {
 	}
 
 	cl.cancel()
-	cl.cancel = nil
-	return cl.metrics.Close()
+	select {
+	case <-cl.closed:
+		cl.cancel = nil
+		cl.closed = nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
+	err = cl.metrics.Close()
+	if err != nil {
+		log.Warnw("listener: closing metrics", "err", err)
+	}
+	return nil
 }
 
 // runSubscriber runs a subscriber to receive event data of new signed blocks. It will attempt to
 // resubscribe in case error happens during listening of subscription
 func (cl *Listener) runSubscriber(ctx context.Context, sub <-chan types.EventDataSignedBlock) {
+	defer close(cl.closed)
 	for {
 		err := cl.listen(ctx, sub)
 		if ctx.Err() != nil {
@@ -131,7 +145,7 @@ func (cl *Listener) runSubscriber(ctx context.Context, sub <-chan types.EventDat
 		}
 		if errors.Is(err, errInvalidSubscription) {
 			// stop node if there is a critical issue with the block subscription
-			log.Fatalf("listener: %v", err)
+			log.Fatalf("listener: %v", err) //nolint:gocritic
 		}
 
 		log.Warnw("listener: subscriber error, resubscribing...", "err", err)
