eb1231d6276ca7eba0bd383a6c97209ad7a3588c
Matthew Sevey <15232757+MSevey@users.noreply.github.com>
2024-10-03 11:31:49 -0400

    chore(docker): refactor init command in entrypoint file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,13 +4,14 @@ set -e
 
 if [ "$1" = 'celestia' ]; then
     echo "Initializing Celestia Node with command:"
-
+    COMMAND="celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
     if [[ -n "$NODE_STORE" ]]; then
-        echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
-        celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
+        COMMAND=${COMMAND}" --node.store "${NODE_STORE}""
+        echo $COMMAND
+        $COMMAND
     else
-        echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
-        celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
+        echo $COMMAND
+        $COMMAND
     fi
 
     echo ""
