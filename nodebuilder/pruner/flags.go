3a25fb20105b07bae06f67a95863fbb3b21124a5
Vlad <13818348+walldiss@users.noreply.github.com>
2024-10-22 16:58:21 +0200

    add archival flag

--- nodebuilder/pruner/flags.go
+++ nodebuilder/pruner/flags.go
@@ -5,16 +5,23 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-const pruningFlag = "experimental-pruning"
+const (
+	pruningFlag  = "experimental-pruning"
+	archivalFlag = "archival"
+)
 
 func Flags() *flag.FlagSet {
 	flags := &flag.FlagSet{}
 
 	flags.Bool(pruningFlag, false, "EXPERIMENTAL: Enables pruning of blocks outside the pruning window.")
+	flags.Bool(archivalFlag, false, "Enables archival mode, which disables pruning and enables the storage of all blocks.")
 
 	return flags
 }
 
 func ParseFlags(cmd *cobra.Command, cfg *Config) {
 	cfg.EnableService = cmd.Flag(pruningFlag).Changed
+	if cfg.EnableService && cmd.Flag(archivalFlag).Changed {
+		log.Fatal("Cannot enable both pruning and archival modes")
+	}
 }
