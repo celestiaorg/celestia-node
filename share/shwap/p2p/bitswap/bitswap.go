dbde072d7a5e60c64a5438b6eed217e4e61d06cc
Wondertan <hlibwondertan@gmail.com>
2024-10-26 04:01:46 +0200

    increase number of bitswap engine workers

--- share/shwap/p2p/bitswap/bitswap.go
+++ share/shwap/p2p/bitswap/bitswap.go
@@ -136,6 +136,7 @@ func NewServer(
 		server.WithTargetMessageSize(targetMessageSize),
 		server.MaxOutstandingBytesPerPeer(outstandingBytesPerPeer),
 		server.WithWantHaveReplaceSize(replaceHasWithBlockMaxSize),
+		server.TaskWorkerCount(32),
 	}
 	return server.New(ctx, net, bstore, opts...)
 }
