2b0ab071740d0aa1497dcfd0967d03dd51d0c570
Wondertan <hlibwondertan@gmail.com>
2024-10-26 04:17:17 +0200

    archival for all

--- pruner/light/window.go
+++ pruner/light/window.go
@@ -1,11 +1,9 @@
 package light
 
 import (
-	"time"
-
 	"github.com/celestiaorg/celestia-node/pruner"
 )
 
 // Window is the availability window for light nodes in the Celestia
 // network (30 days).
-const Window = pruner.AvailabilityWindow(30 * 24 * time.Hour)
+const Window = pruner.AvailabilityWindow(0)
