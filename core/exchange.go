8627559146004e00207f39a4f673256588f59950
sweexordious <chamirachid1@gmail.com>
2024-11-07 16:24:10 +0400

    chore: eds deep txs copy

--- core/exchange.go
+++ core/exchange.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/tendermint/tendermint/types"
 	"time"
 
 	"golang.org/x/sync/errgroup"
@@ -153,7 +152,7 @@ func (ce *Exchange) Get(ctx context.Context, hash libhead.Hash) (*header.Extende
 		return nil, err
 	}
 
-	block.Data = types.Data{}
+	//block.Data = types.Data{}
 
 	return eh, nil
 }
@@ -191,7 +190,7 @@ func (ce *Exchange) getExtendedHeaderByHeight(ctx context.Context, height *int64
 		return nil, err
 	}
 
-	b.Data = types.Data{}
+	//b.Data = types.Data{}
 
 	return eh, nil
 }
