414fa5c8b54947800a0be8ea3c13bf60e8e872c2
vgonkivs <viacheslavgonkivskyi@gmail.com>
2024-11-11 15:06:51 +0200

    improve godoc

--- nodebuilder/core/tls.go
+++ nodebuilder/core/tls.go
@@ -16,8 +16,16 @@ const (
 	xtoken = "xtoken.json"
 )
 
-// TLS parses the tls path and tries to configure the config with tls certificates.
-// In returns an empty config in case the path was not specified.
+// TLS creates a TLS configuration using the certificate and key files from the specified path.
+// It constructs the full paths to the certificate and key files by joining the provided directory path
+// with their respective file names.
+// If either file is missing, it returns an os.ErrNotExist error.
+// If the files exist, it loads the X.509 key pair from the specified files and sets up a tls.Config with a minimum version of TLS 1.2.
+// Parameters:
+// * tlsPath: The directory path where the TLS certificate ("cert.pem") and key ("key.pem") files are located.
+// Returns:
+// * A tls.Config structure configured with the provided certificate and key.
+// * An error if the certificate or key file does not exist, or if loading the key pair fails.
 func TLS(tlsPath string) (*tls.Config, error) {
 	certPath := filepath.Join(tlsPath, cert)
 	keyPath := filepath.Join(tlsPath, key)
@@ -39,6 +47,16 @@ type AuthToken struct {
 	Token string `json:"x-token"`
 }
 
+// XToken retrieves the authentication token from a JSON file at the specified path.
+// It first constructs the full file path by joining the provided directory path with the token file name.
+// If the file does not exist, it returns an os.ErrNotExist error.
+// If the file exists, it reads the content and unmarshals it.
+// If the field in the unmarshaled struct is empty, an error is returned indicating that the token is missing.
+// Parameters:
+// * xtokenPath: The directory path where the JSON file containing the X-Token is located.
+// Returns:
+// * The X-Token as a string if successfully retrieved.
+// * An error if the file does not exist, reading fails, unmarshalling fails, or the token is empty.
 func XToken(xtokenPath string) (string, error) {
 	xtokenPath = filepath.Join(xtokenPath, xtoken)
 	exist := utils.Exists(xtokenPath)
