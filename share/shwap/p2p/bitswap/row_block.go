cf08ca95060f1d6d6c65b5ad72add65b8299d9ed
Hlib Kanunnikov <hlibwondertan@gmail.com>
2024-11-15 05:26:41 +0100

    fix(shwap/bitswap): Blockstore.GetSize: getting size with no compute (#3894)
    Bitswap prioritizes peers based on their active/pending work and the priority that peers set for requests(work) themselves. The prioritization happens on the `Get` operation of `Blockstore` not `GetSize`. However, we currently do the most expensive work in `GetSize` followed by `Get`. It is still part of the same WANT_BLOCK request(since #3813), and `GetSize` work is usually cached for `Get` to catch it. Still, the prioritization makes it so that the time between `GetSize` and `Get` can be pretty long, inducing cache misses and redoing the same work again, which is confirmed by profiles. Also, doing the expensive part in `GetSize` avoids Bitswap server's rate limiting. 
    
    All this brings the need to make `GetSize` as lightweight as possible, leaving actual proof computed to `Get`. This PR achieves this by defining a constant max block size for every type block.
    
    --- 
    
    🎱 mb blonks certified

--- share/shwap/p2p/bitswap/row_block.go
+++ share/shwap/p2p/bitswap/row_block.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ipfs/go-cid"
 
+	libshare "github.com/celestiaorg/go-square/v2/share"
 	"github.com/celestiaorg/rsmt2d"
 
 	"github.com/celestiaorg/celestia-node/share"
@@ -22,10 +23,15 @@ const (
 	rowMultihashCode = 0x7801
 )
 
+// maxRowSize is the maximum size of the RowBlock.
+// It is calculated as half of the square size multiplied by the share size.
+var maxRowSize = share.MaxSquareSize / 2 * libshare.ShareSize
+
 func init() {
 	registerBlock(
 		rowMultihashCode,
 		rowCodec,
+		maxRowSize,
 		shwap.RowIDSize,
 		func(cid cid.Cid) (Block, error) {
 			return EmptyRowBlockFromCID(cid)
