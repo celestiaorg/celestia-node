33677e30bc4ab471c11d1ea2ecadb7a76bd98cde
sweexordious <chamirachid1@gmail.com>
2024-11-27 16:37:05 +0100

    Revert "fix(header)!: tendermint compatible json marshall (#3928)"
    This reverts commit 4a906057

--- header/header.go
+++ header/header.go
@@ -2,6 +2,7 @@ package header
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"time"
@@ -226,22 +227,56 @@ func (eh *ExtendedHeader) UnmarshalBinary(data []byte) error {
 	return nil
 }
 
-// MarshalJSON marshals an ExtendedHeader to JSON.
-// Uses tendermint encoder for tendermint compatibility.
+// MarshalJSON marshals an ExtendedHeader to JSON. The ValidatorSet is wrapped with amino encoding,
+// to be able to unmarshal the crypto.PubKey type back from JSON.
 func (eh *ExtendedHeader) MarshalJSON() ([]byte, error) {
-	// alias the type to avoid going into recursion loop
-	// because tmjson.Marshal invokes custom json marshaling
 	type Alias ExtendedHeader
-	return tmjson.Marshal((*Alias)(eh))
+	validatorSet, err := tmjson.Marshal(eh.ValidatorSet)
+	if err != nil {
+		return nil, err
+	}
+	rawHeader, err := tmjson.Marshal(eh.RawHeader)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(&struct {
+		RawHeader    json.RawMessage `json:"header"`
+		ValidatorSet json.RawMessage `json:"validator_set"`
+		*Alias
+	}{
+		ValidatorSet: validatorSet,
+		RawHeader:    rawHeader,
+		Alias:        (*Alias)(eh),
+	})
 }
 
-// UnmarshalJSON unmarshals an ExtendedHeader from JSON.
-// Uses tendermint decoder for tendermint compatibility.
+// UnmarshalJSON unmarshals an ExtendedHeader from JSON. The ValidatorSet is wrapped with amino
+// encoding, to be able to unmarshal the crypto.PubKey type back from JSON.
 func (eh *ExtendedHeader) UnmarshalJSON(data []byte) error {
-	// alias the type to avoid going into recursion loop
-	// because tmjson.Unmarshal invokes custom json unmarshalling
 	type Alias ExtendedHeader
-	return tmjson.Unmarshal(data, (*Alias)(eh))
+	aux := &struct {
+		RawHeader    json.RawMessage `json:"header"`
+		ValidatorSet json.RawMessage `json:"validator_set"`
+		*Alias
+	}{
+		Alias: (*Alias)(eh),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	valSet := new(core.ValidatorSet)
+	if err := tmjson.Unmarshal(aux.ValidatorSet, valSet); err != nil {
+		return err
+	}
+	rawHeader := new(RawHeader)
+	if err := tmjson.Unmarshal(aux.RawHeader, rawHeader); err != nil {
+		return err
+	}
+
+	eh.ValidatorSet = valSet
+	eh.RawHeader = *rawHeader
+	return nil
 }
 
 var _ libhead.Header[*ExtendedHeader] = &ExtendedHeader{}
