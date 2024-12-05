6c4e1a76ac85a618b7858bde0fbcb6d037c4dca5
vgonkivs <viacheslavgonkivskyi@gmail.com>
2024-12-05 19:56:01 +0200

    update api

--- nodebuilder/share/share.go
+++ nodebuilder/share/share.go
@@ -11,15 +11,14 @@ import (
 	"github.com/celestiaorg/celestia-node/header"
 	headerServ "github.com/celestiaorg/celestia-node/nodebuilder/header"
 	"github.com/celestiaorg/celestia-node/share"
-	"github.com/celestiaorg/celestia-node/share/eds"
 	"github.com/celestiaorg/celestia-node/share/shwap"
 )
 
 var _ Module = (*API)(nil)
 
-// GetRangeResult wraps the return value of the GetRange endpoint
+// Range wraps the return value of the GetRange endpoint
 // because Json-RPC doesn't support more than two return values.
-type GetRangeResult struct {
+type Range struct {
 	Shares []libshare.Share
 	Proof  *types.ShareProof
 }
@@ -58,15 +57,9 @@ type Module interface {
 		ctx context.Context, height uint64, namespace libshare.Namespace,
 	) (shwap.NamespaceData, error)
 	// GetRange gets a list of shares and their corresponding proof.
-	GetRange(ctx context.Context, height uint64, start, end int) (*GetRangeResult, error)
-
-	GetSharesRange(
-		ctx context.Context,
-		ns libshare.Namespace,
-		height uint64,
-		from, length uint32,
-		proofsOnly bool,
-	) (shwap.RangeNamespaceData, error)
+	GetRange(
+		ctx context.Context, ns libshare.Namespace, height uint64, from, to uint32, proofsOnly bool,
+	) (*Range, error)
 }
 
 // API is a wrapper around Module for the RPC.
@@ -99,16 +92,11 @@ type API struct {
 		) (shwap.NamespaceData, error) `perm:"read"`
 		GetRange func(
 			ctx context.Context,
-			height uint64,
-			start, end int,
-		) (*GetRangeResult, error) `perm:"read"`
-		GetSharesRange func(
-			ctx context.Context,
 			ns libshare.Namespace,
 			height uint64,
-			start, length uint32,
+			from, to uint32,
 			proofsOnly bool,
-		) (shwap.RangeNamespaceData, error)
+		) (*Range, error) `perm:"read"`
 	}
 }
 
@@ -134,8 +122,10 @@ func (api *API) GetRow(ctx context.Context, height uint64, rowIdx int) (shwap.Ro
 	return api.Internal.GetRow(ctx, height, rowIdx)
 }
 
-func (api *API) GetRange(ctx context.Context, height uint64, start, end int) (*GetRangeResult, error) {
-	return api.Internal.GetRange(ctx, height, start, end)
+func (api *API) GetRange(
+	ctx context.Context, ns libshare.Namespace, height uint64, from, to uint32, proofsOnly bool,
+) (*Range, error) {
+	return api.Internal.GetRange(ctx, ns, height, from, to, proofsOnly)
 }
 
 func (api *API) GetNamespaceData(
@@ -144,16 +134,6 @@ func (api *API) GetNamespaceData(
 	namespace libshare.Namespace,
 ) (shwap.NamespaceData, error) {
 	return api.Internal.GetNamespaceData(ctx, height, namespace)
-}
-
-func (api *API) GetSharesRange(
-	ctx context.Context,
-	ns libshare.Namespace,
-	height uint64,
-	start, to uint32,
-	proofsOnly bool,
-) (shwap.RangeNamespaceData, error) {
-	return api.Internal.GetSharesRange(ctx, ns, height, start, to, proofsOnly)
 }
 
 type module struct {
@@ -200,25 +180,36 @@ func (m module) SharesAvailable(ctx context.Context, height uint64) error {
 	return m.avail.SharesAvailable(ctx, header)
 }
 
-func (m module) GetRange(ctx context.Context, height uint64, start, end int) (*GetRangeResult, error) {
-	extendedDataSquare, err := m.GetEDS(ctx, height)
+func (m module) GetRange(
+	ctx context.Context, ns libshare.Namespace, height uint64, from, to uint32, proofsOnly bool,
+) (*Range, error) {
+	header, err := m.hs.GetByHeight(ctx, height)
 	if err != nil {
 		return nil, err
 	}
 
-	proof, err := eds.ProveShares(extendedDataSquare, start, end)
+	fromCoords, err := shwap.SampleCoordsFrom1DIndex(int(from), len(header.DAH.RowRoots)/2)
+	if err != nil {
+		return nil, err
+	}
+	toCoords, err := shwap.SampleCoordsFrom1DIndex(int(to), len(header.DAH.RowRoots)/2)
 	if err != nil {
 		return nil, err
 	}
 
-	shares, err := libshare.FromBytes(extendedDataSquare.FlattenedODS()[start:end])
+	nData, err := m.getter.GetSharesRange(ctx, header, ns, fromCoords, toCoords)
 	if err != nil {
 		return nil, err
 	}
-	return &GetRangeResult{
-		Shares: shares,
-		Proof:  proof,
-	}, nil
+
+	res := &Range{
+		Proof: nData.ProveRange(fromCoords.Row, header.DAH.RowRoots, header.DAH.ColumnRoots),
+	}
+
+	if !proofsOnly {
+		res.Shares = nData.Flatten()
+	}
+	return res, nil
 }
 
 func (m module) GetNamespaceData(
