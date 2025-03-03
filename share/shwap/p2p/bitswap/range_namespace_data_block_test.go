9542c57a174704ea1e968b055105bbb5a8ba82e2
vgonkivs <viacheslavgonkivskyi@gmail.com>
2025-03-03 18:30:57 +0200

    feat(share/p2p/bitswap): add range namespace data block

---
+++ share/shwap/p2p/bitswap/range_namespace_data_block_test.go
@@ -0,0 +1,41 @@
+package bitswap
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	libshare "github.com/celestiaorg/go-square/v2/share"
+
+	"github.com/celestiaorg/celestia-node/share/eds/edstest"
+	"github.com/celestiaorg/celestia-node/share/shwap"
+)
+
+func TestRangeNamespaceData_FetchRoundtrip(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
+	defer cancel()
+
+	namespace := libshare.RandomNamespace()
+	eds, root := edstest.RandEDSWithNamespace(t, namespace, 64, 8)
+	exchange := newExchangeOverEDS(ctx, t, eds)
+	blk, err := NewEmptyRangeNamespaceDataBlock(1, namespace,
+		shwap.SampleCoords{Row: 0, Col: 0},
+		shwap.SampleCoords{Row: 2, Col: 2},
+		16,
+		false,
+	)
+	require.NoError(t, err)
+
+	err = Fetch(ctx, exchange, root, []Block{blk})
+	require.NoError(t, err)
+
+	err = blk.Container.Verify(
+		namespace,
+		shwap.SampleCoords{Row: 0, Col: 0},
+		shwap.SampleCoords{Row: 2, Col: 2},
+		root.Hash(),
+	)
+	require.NoError(t, err)
+}
