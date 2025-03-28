bed539f77196e3491277303e87a7c66a49259281
rene <41963722+renaynay@users.noreply.github.com>
2025-03-28 17:12:41 +0100

    doc(state): add doc to opt

--- state/option.go
+++ state/option.go
@@ -2,7 +2,9 @@ package state
 
 type Option func(ca *CoreAccessor)
 
-// WithEstimatorService TODO @renaynay
+// WithEstimatorService indicates to the CoreAccessor to pass the given address
+// for the estimator service to the TxClient to use for all gas price and usage
+// estimation queries.
 func WithEstimatorService(address string) Option {
 	return func(ca *CoreAccessor) {
 		ca.estimatorServiceAddr = address
