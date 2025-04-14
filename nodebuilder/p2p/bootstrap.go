fb0cb125d7a9cd766d6b80f193baf712665e35db
rene <41963722+renaynay@users.noreply.github.com>
2025-04-14 15:00:39 +0200

    remove other BN bootstrappers

--- nodebuilder/p2p/bootstrap.go
+++ nodebuilder/p2p/bootstrap.go
@@ -64,11 +64,6 @@ var bootstrapList = map[Network][]string{
 	},
 	Mammoth: {
 		"/dnsaddr/da-bridge-0.par.mamochain.com/p2p/12D3KooWNc3hDtzLvyKj8xbcE3SFMRy4uX5EojCScCuqYRrz4tzS",
-		"/dnsaddr/da-bridge-1.par.mamochain.com/p2p/12D3KooWQAm6jsEfsAZ8QSs2FRDyNyQBfDHkiC9WCSpyMG58Xc5o",
-		"/dnsaddr/da-bridge-2.ams.mamochain.com/p2p/12D3KooWMtTXAfjXCVNUTfB8n9zdL8XZBq1MteCR7Uoyo348i1xJ",
-		"/dnsaddr/da-bridge-3.ams.mamochain.com/p2p/12D3KooWSJZXE1qGE8yoZBEAEtjt8m4AS3fA7AChuJ9XsgqkDhkB",
-		"/dnsaddr/da-bridge-4.waw.mamochain.com/p2p/12D3KooWAUYfdakPjzY8LuvcXrq8mQQY8QbCXQjxi3SwhmnqtoTJ",
-		"/dnsaddr/da-bridge-5.waw.mamochain.com/p2p/12D3KooWGa4f3kycHgztSYAMRpKYMzSpwxuxNdBYUvs9NHXx9DLN",
 	},
 	Private: {},
 }
