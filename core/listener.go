45082f2544f3bd5b335c15aacaef7fcd5e767d33
Oleg Kovalov <hey@olegk.dev>
2025-05-07 12:48:38 +0200

    try without WithLocalPublication

--- core/listener.go
+++ core/listener.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/cometbft/cometbft/types"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/tendermint/tendermint/types"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 
@@ -208,7 +208,7 @@ func (cl *Listener) handleNewSignedBlock(ctx context.Context, b types.EventDataS
 	}
 
 	// broadcast new ExtendedHeader, but if core is still syncing, notify only local subscribers
-	err = cl.headerBroadcaster.Broadcast(ctx, eh, pubsub.WithLocalPublication(syncing))
+	err = cl.headerBroadcaster.Broadcast(ctx, eh) // pubsub.WithLocalPublication(syncing))
 	if err != nil && !errors.Is(err, context.Canceled) {
 		log.Errorw("listener: broadcasting next header",
 			"height", b.Header.Height,
