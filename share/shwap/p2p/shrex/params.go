b76bbb02ad52615ebcd9f5db714e92a3a670dfdd
vgonkivs <viacheslavgonkivskyi@gmail.com>
2025-05-21 16:30:16 +0300

    add fixes aacording to the @walldiss

--- share/shwap/p2p/shrex/params.go
+++ share/shwap/p2p/shrex/params.go
@@ -8,52 +8,80 @@ import (
 	"github.com/libp2p/go-libp2p/core/protocol"
 )
 
-var log = logging.Logger("shrex/")
+var log = logging.Logger("shrex")
 
 // protocolString is the protocol string for the shrex protocol.
 const protocolString = "/shrex/v0.1.0/"
 
-// Parameters is the set of parameters that must be configured for the shrex protocol.
 type Parameters struct {
-	// ServerReadTimeout sets the timeout for reading messages from the stream.
-	ServerReadTimeout time.Duration
+	// ReadTimeout sets the timeout for reading messages from the stream.
+	ReadTimeout time.Duration
 
-	// ServerWriteTimeout sets the timeout for writing messages to the stream.
-	ServerWriteTimeout time.Duration
-
-	// HandleRequestTimeout defines the deadline for handling request.
-	HandleRequestTimeout time.Duration
-
-	// ConcurrencyLimit is the maximum number of concurrently handled streams
-	ConcurrencyLimit int
+	// WriteTimeout sets the timeout for writing messages to the stream.
+	WriteTimeout time.Duration
 
 	// networkID is prepended to the protocolID and represents the network the protocol is
 	// running on.
 	networkID string
 }
 
-func DefaultParameters() *Parameters {
-	return &Parameters{
-		ServerReadTimeout:    5 * time.Second,
-		ServerWriteTimeout:   time.Minute, // based on max observed sample time for 256 blocks (~50s)
+type ClientParams struct {
+	*Parameters
+}
+
+type ServerParams struct {
+	*Parameters
+
+	// HandleRequestTimeout defines the deadline for handling request.
+	HandleRequestTimeout time.Duration
+
+	// ConcurrencyLimit is the maximum number of concurrently handled streams
+	ConcurrencyLimit int
+}
+
+func DefaultClientParameters() *ClientParams {
+	return &ClientParams{Parameters: defaultParameters()}
+}
+func DefaultServerParameters() *ServerParams {
+	return &ServerParams{
+		Parameters:           defaultParameters(),
 		HandleRequestTimeout: time.Minute,
 		ConcurrencyLimit:     10,
+	}
+}
+
+func defaultParameters() *Parameters {
+	return &Parameters{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: time.Minute, // based on max observed sample time for 256 blocks (~50s)
+
 	}
 }
 
 const errSuffix = "value should be positive and non-zero"
 
 func (p *Parameters) Validate() error {
-	if p.ServerReadTimeout <= 0 {
-		return fmt.Errorf("invalid stream read timeout: %v, %s", p.ServerReadTimeout, errSuffix)
+	if p.ReadTimeout <= 0 {
+		return fmt.Errorf("invalid stream read timeout: %v, %s", p.ReadTimeout, errSuffix)
 	}
-	if p.ServerWriteTimeout <= 0 {
-		return fmt.Errorf("invalid write timeout: %v, %s", p.ServerWriteTimeout, errSuffix)
+	if p.WriteTimeout <= 0 {
+		return fmt.Errorf("invalid write timeout: %v, %s", p.WriteTimeout, errSuffix)
 	}
-	if p.HandleRequestTimeout <= 0 {
-		return fmt.Errorf("invalid handle request timeout: %v, %s", p.HandleRequestTimeout, errSuffix)
+	return nil
+}
+
+func (c *ClientParams) Validate() error {
+	return c.Parameters.Validate()
+}
+
+func (s *ServerParams) Validate() error {
+	if err := s.Parameters.Validate(); err != nil {
+		return err
 	}
-	if p.ConcurrencyLimit <= 0 {
+	if s.HandleRequestTimeout <= 0 {
+		return fmt.Errorf("invalid handle request timeout: %v, %s", s.HandleRequestTimeout, errSuffix)
+	}
+	if s.ConcurrencyLimit <= 0 {
 		return fmt.Errorf("invalid concurrency limit: %s", errSuffix)
 	}
 	return nil
