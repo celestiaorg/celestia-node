c1dea0f849e611a5f407ea212f5fc9976bfd134f
Hlib Kanunnikov <hlibwondertan@gmail.com>
2025-05-22 12:11:59 +0200

    Update core/exchange.go

--- core/exchange.go
+++ core/exchange.go
@@ -132,7 +132,7 @@ func (ce *Exchange) Get(ctx context.Context, hash libhead.Hash) (*header.Extende
 		return nil, fmt.Errorf("fetching block info for height %d: %w", &block.Height, err)
 	}
 
-	eds, err := extendBlock(&block.Data, block.Version.App)
+	eds, err := extendBlock(&block.Data)
 	if err != nil {
 		return nil, fmt.Errorf("extending block data for height %d: %w", &block.Height, err)
 	}
