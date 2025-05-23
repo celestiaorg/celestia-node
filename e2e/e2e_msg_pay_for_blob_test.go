0692701e6e4a8c3ad852e8ae239cbdfaddc19cb9
chatton <github.qpeyb@simplelogin.fr>
2025-05-23 09:17:54 +0100

    refactor: update existing test to work with new setup function

--- e2e/e2e_msg_pay_for_blob_test.go
+++ e2e/e2e_msg_pay_for_blob_test.go
@@ -6,17 +6,14 @@ import (
 	"testing"
 	"time"
 
-	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/celestiaorg/celestia-app/v4/pkg/appconsts"
 	"github.com/celestiaorg/celestia-app/v4/test/util/random"
 	blobtypes "github.com/celestiaorg/celestia-app/v4/x/blob/types"
 	libshare "github.com/celestiaorg/go-square/v2/share"
-	"github.com/celestiaorg/tastora/framework/docker"
 	"github.com/celestiaorg/tastora/framework/testutil/sdkacc"
 	"github.com/celestiaorg/tastora/framework/testutil/wait"
-	"github.com/celestiaorg/tastora/framework/types"
 )
 
 // TestE2EMsgPayForBlob ensures end-to-end functionality of broadcasting and verifying a MsgPayForBlob transaction.
@@ -28,60 +25,13 @@ func (s *CelestiaTestSuite) TestE2EMsgPayForBlob() {
 	}
 
 	ctx := context.TODO()
-	provider := s.CreateDockerProvider()
-
-	celestia, err := provider.GetChain(ctx)
-	s.Require().NoError(err)
-
-	err = celestia.Start(ctx)
-	s.Require().NoError(err)
-
-	// cleanup resources when the test is done
-	t.Cleanup(func() {
-		if err := celestia.Stop(ctx); err != nil {
-			t.Logf("Error stopping chain: %v", err)
-		}
-	})
-
-	// verify the chain is producing blocks
-	height, err := celestia.Height(ctx)
-	s.Require().NoError(err)
-	s.Require().Greater(height, int64(0))
-
 	// wait for some blocks to ensure the bridge node can sync up.
-	s.Require().NoError(wait.ForBlocks(ctx, 10, celestia))
+	s.Require().NoError(wait.ForBlocks(ctx, 10, s.celestia))
 
-	genesisHash := s.getGenesisHash(ctx, celestia)
-	s.Require().NotEmpty(genesisHash, "genesis hash is empty")
-
-	bridgeNode, err := provider.GetDANode(ctx, types.BridgeNode)
-	s.Require().NoError(err, "failed to get bridge node")
-
-	hostname, err := celestia.GetNodes()[0].GetInternalHostName(ctx)
-	s.Require().NoError(err, "failed to get internal hostname")
-
-	err = bridgeNode.Start(ctx,
-		types.WithCoreIP(hostname),
-		types.WithGenesisBlockHash(genesisHash),
-	)
-
-	s.Require().NoError(err, "failed to start bridge node")
-	// cleanup resources when the test is done
-	t.Cleanup(func() {
-		if err := bridgeNode.Stop(ctx); err != nil {
-			t.Logf("Error stopping bridge node: %v", err)
-		}
-	})
-
-	celestiaHeight, err := celestia.Height(ctx)
+	celestiaHeight, err := s.celestia.Height(ctx)
 	s.Require().NoError(err, "failed to get celestia height")
 
-	dockerChain, ok := celestia.(*docker.Chain)
-	s.Require().True(ok, "celestia is not a docker chain")
-
-	wallet, err := docker.CreateAndFundTestWallet(s.T(), ctx, "test", sdkmath.NewInt(100000), dockerChain)
-	s.Require().NoError(err, "failed to create test wallet")
-	s.Require().NotNil(wallet, "wallet is nil")
+	wallet := s.CreateTestWallet(ctx, s.celestia, 100000)
 
 	ns := libshare.RandomBlobNamespace()
 	signer := wallet.GetFormattedAddress()
@@ -91,48 +41,24 @@ func (s *CelestiaTestSuite) TestE2EMsgPayForBlob() {
 
 	msg, blob := randMsgPayForBlobsWithNamespaceAndSigner(signer, signerAddr, ns, 100)
 
-	resp, err := celestia.BroadcastBlobMessage(ctx, wallet, msg, blob)
+	resp, err := s.celestia.BroadcastBlobMessage(ctx, wallet, msg, blob)
 	s.Require().NoError(err, "failed to broadcast blob message")
 	s.Require().NotNil(resp, "broadcast blob message response is nil")
 	s.Require().Equal(uint32(0), resp.Code, "expected successful tx broadcast, got error: %s", resp.RawLog)
 
-	err = wait.ForDANodeToReachHeight(ctx, bridgeNode, uint64(celestiaHeight), time.Second*30)
+	err = wait.ForDANodeToReachHeight(ctx, s.bridgeNode, uint64(celestiaHeight), time.Second*30)
 	s.Require().NoError(err, "failed to wait for bridge node to reach height")
 
-	p2pInfo, err := bridgeNode.GetP2PInfo(ctx)
-	s.Require().NoError(err, "failed to get bridge node p2p info")
+	s.Require().NoError(wait.ForBlocks(ctx, 10, s.celestia), "failed to wait for blocks")
 
-	p2pAddr, err := p2pInfo.GetP2PAddress()
-	s.Require().NoError(err, "failed to get bridge node p2p address")
-
-	t.Logf("P2P Addr: %s", p2pAddr)
-
-	lightNode, err := provider.GetDANode(ctx, types.LightNode)
-	s.Require().NoError(err, "failed to get light node")
-
-	err = lightNode.Start(ctx,
-		types.WithP2PAddress(p2pAddr),
-		types.WithCoreIP(hostname),
-		types.WithGenesisBlockHash(genesisHash),
-	)
-	s.Require().NoError(err, "failed to start light node")
-	// cleanup resources when the test is done
-	t.Cleanup(func() {
-		if err := lightNode.Stop(ctx); err != nil {
-			t.Logf("Error stopping light node: %v", err)
-		}
-	})
-
-	s.Require().NoError(wait.ForBlocks(ctx, 10, celestia), "failed to wait for blocks")
-
-	celestiaHeight, err = celestia.Height(ctx)
+	celestiaHeight, err = s.celestia.Height(ctx)
 	s.Require().NoError(err, "failed to get celestia height")
 
-	err = wait.ForDANodeToReachHeight(ctx, lightNode, uint64(celestiaHeight), time.Second*30)
+	err = wait.ForDANodeToReachHeight(ctx, s.lightNode, uint64(celestiaHeight), time.Second*30)
 	s.Require().NoError(err, "failed to wait for light node to reach height")
 
 	for i := 1; i < int(celestiaHeight); i++ {
-		blobs, err := lightNode.GetAllBlobs(ctx, uint64(i), []libshare.Namespace{ns})
+		blobs, err := s.lightNode.GetAllBlobs(ctx, uint64(i), []libshare.Namespace{ns})
 		s.Require().NoError(err, "failed to get blobs at height %d", i)
 
 		if len(blobs) > 0 {
--- e2e/go.mod
+++ e2e/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/celestiaorg/celestia-app/v4 v4.0.1-mocha.0.20250521112605-9034d466a320
 	github.com/celestiaorg/celestia-node v0.22.2
 	github.com/celestiaorg/go-square/v2 v2.2.0
-	github.com/celestiaorg/tastora v0.0.0-20250515081551-795e795a49c7
+	github.com/celestiaorg/tastora v0.0.2-0.20250522134105-fdbadaac2b0b
 	github.com/cosmos/cosmos-sdk v0.50.13
 	github.com/moby/moby v27.5.1+incompatible
 	github.com/stretchr/testify v1.10.0
@@ -387,7 +387,7 @@ replace (
 	cosmossdk.io/x/upgrade => github.com/celestiaorg/cosmos-sdk/x/upgrade v0.1.0
 	//github.com/celestiaorg/celestia-app/v4 => ../../celestia-app
 	github.com/celestiaorg/celestia-node => ../../celestia-node
-	github.com/celestiaorg/tastora => ../../tastora
+	//github.com/celestiaorg/tastora => ../../tastora
 	github.com/cometbft/cometbft => github.com/celestiaorg/celestia-core v1.53.0-tm-v0.38.17
 	github.com/cosmos/cosmos-sdk => github.com/celestiaorg/cosmos-sdk v1.29.1-sdk-v0.50.12
 	github.com/gogo/protobuf => github.com/gogo/protobuf v1.3.2
