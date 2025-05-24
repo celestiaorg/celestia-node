b0e31793617b8a88c086cf983d8eb5d949df1cd8
Ragnar <rodiondenmark@gmail.com>
2025-05-25 00:54:55 +0200

    Create modname.go

---
+++ nodebuilder/modename/modname.go
@@ -0,0 +1,16 @@
+package modname
+
+// Module name constants to avoid duplication across the codebase
+// See: https://github.com/celestiaorg/celestia-node/issues/1176
+const (
+	Fraud      = "fraud"
+	DAS        = "das"
+	Header     = "header"
+	State      = "state"
+	Share      = "share"
+	P2P        = "p2p"
+	Node       = "node"
+	Blob       = "blob"
+	DA         = "da"
+	Blobstream = "blobstream"
+)
