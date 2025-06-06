458de07f70781970fe2f03d872ad4d7ae107d700
vgonkivs <viacheslavgonkivskyi@gmail.com>
2025-06-06 13:25:32 +0300

    fix build after rebase

--- share/shwap/axis_half.go
+++ share/shwap/axis_half.go
@@ -1,14 +1,10 @@
 package shwap
 
 import (
-	"fmt"
-
 	libshare "github.com/celestiaorg/go-square/v2/share"
 
 	"github.com/celestiaorg/celestia-node/share"
 )
-
-var codec = share.DefaultRSMT2DCodec()
 
 // AxisHalf represents a half of data for a row or column in the EDS.
 type AxisHalf struct {
@@ -29,48 +25,7 @@ func (a AxisHalf) ToRow() Row {
 // Extended returns full axis shares from half axis shares.
 func (a AxisHalf) Extended() ([]libshare.Share, error) {
 	if a.IsParity {
-		return reconstructShares(a.Shares)
+		return share.ReconstructShares(a.Shares)
 	}
-	return extendShares(a.Shares)
-}
-
-// extendShares constructs full axis shares from original half axis shares.
-func extendShares(original []libshare.Share) ([]libshare.Share, error) {
-	if len(original) == 0 {
-		return nil, fmt.Errorf("original shares are empty")
-	}
-
-	parity, err := codec.Encode(libshare.ToBytes(original))
-	if err != nil {
-		return nil, fmt.Errorf("encoding: %w", err)
-	}
-
-	parityShrs, err := libshare.FromBytes(parity)
-	if err != nil {
-		return nil, err
-	}
-
-	sqLen := len(original) * 2
-	shares := make([]libshare.Share, sqLen)
-	copy(shares, original)
-	copy(shares[sqLen/2:], parityShrs)
-	return shares, nil
-}
-
-func reconstructShares(parity []libshare.Share) ([]libshare.Share, error) {
-	if len(parity) == 0 {
-		return nil, fmt.Errorf("parity shares are empty")
-	}
-
-	sqLen := len(parity) * 2
-	shares := make([]libshare.Share, sqLen)
-	for i := sqLen / 2; i < sqLen; i++ {
-		shares[i] = parity[i-sqLen/2]
-	}
-	shrs, err := codec.Decode(libshare.ToBytes(shares))
-	if err != nil {
-		return nil, fmt.Errorf("reconstructing: %w", err)
-	}
-
-	return libshare.FromBytes(shrs)
+	return share.ExtendShares(a.Shares)
 }
