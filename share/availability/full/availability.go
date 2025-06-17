957e4517af4653791bf2f62dc11fd9c0f522ec2d
Wondertan <hlibwondertan@gmail.com>
2025-06-17 20:40:42 +0200

    update full availability

--- share/availability/full/availability.go
+++ share/availability/full/availability.go
@@ -109,11 +109,12 @@ func (fa *ShareAvailability) SharesAvailable(ctx context.Context, header *header
 }
 
 func (fa *ShareAvailability) Prune(ctx context.Context, eh *header.ExtendedHeader) error {
+	height := eh.Height()
 	if fa.archival {
-		log.Debugf("trimming Q4 from block %s at height %d", eh.DAH.String(), eh.Height())
-		return fa.store.RemoveQ4(ctx, eh.Height(), eh.DAH.Hash())
+		log.Debugf("trimming Q4 from block at height %d", height)
+		return fa.store.RemoveQ4(ctx, height)
 	}
 
-	log.Debugf("removing block %s at height %d", eh.DAH.String(), eh.Height())
-	return fa.store.RemoveODSQ4(ctx, eh.Height(), eh.DAH.Hash())
+	log.Debugf("removing block %s at height %d", height)
+	return fa.store.RemoveODSQ4(ctx, height)
 }
