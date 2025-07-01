a0dd98991f55b939bea88b5bcea6469cf603bb77
rene <41963722+renaynay@users.noreply.github.com>
2025-07-01 13:42:07 +0200

    more prints

--- share/eds/nd.go
+++ share/eds/nd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	libshare "github.com/celestiaorg/go-square/v2/share"
+	"time"
 
 	"github.com/celestiaorg/celestia-node/share"
 	"github.com/celestiaorg/celestia-node/share/shwap"
@@ -17,17 +18,24 @@ func NamespaceData(
 	eds Accessor,
 	namespace libshare.Namespace,
 ) (shwap.NamespaceData, error) {
+	startTime := time.Now()
 	roots, err := eds.AxisRoots(ctx)
+	fmt.Println("INSIDE NamespaceData: eds.AxisRoots took  ", time.Since(startTime).Milliseconds())
 	if err != nil {
 		return nil, fmt.Errorf("failed to get AxisRoots: %w", err)
 	}
+
+	startTime = time.Now()
 	rowIdxs, err := share.RowsWithNamespace(roots, namespace)
+	fmt.Println("INSIDE NamespaceData: share.RowsWithNamespace took  ", time.Since(startTime).Milliseconds())
 	if err != nil {
 		return nil, fmt.Errorf("failed to get row indexes: %w", err)
 	}
 	rows := make(shwap.NamespaceData, len(rowIdxs))
 	for i, idx := range rowIdxs {
+		startTime = time.Now()
 		rows[i], err = eds.RowNamespaceData(ctx, namespace, idx)
+		fmt.Println("INSIDE NamespaceData: eds.RowNamespaceData took  ", time.Since(startTime).Milliseconds())
 		if err != nil {
 			return nil, fmt.Errorf("failed to process row %d: %w", idx, err)
 		}
