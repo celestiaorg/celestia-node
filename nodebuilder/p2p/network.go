44710909cf764e28cb6a9e2a80c7b66ade747612
Wondertan <hlibwondertan@gmail.com>
2025-07-04 16:11:38 +0200

    decrease block time to the actual one

--- nodebuilder/p2p/network.go
+++ nodebuilder/p2p/network.go
@@ -23,7 +23,7 @@ const (
 	Mainnet Network = "celestia"
 	// BlockTime is a network block time.
 	// TODO @renaynay @Wondertan (#790)
-	BlockTime = time.Second * 10
+	BlockTime = time.Second * 5
 )
 
 // Network is a type definition for DA network run by Celestia Node.
