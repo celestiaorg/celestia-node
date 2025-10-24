8829f110565f66efa815a8bfe2d054784cf8b734
rene <41963722+renaynay@users.noreply.github.com>
2025-10-24 15:00:11 +0200

    feat(state): Introduce queued submission (#4655)
    Co-authored-by: Evan Forbes <42654277+evan-forbes@users.noreply.github.com>

--- nodebuilder/state/config.go
+++ nodebuilder/state/config.go
@@ -1,6 +1,8 @@
 package state
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 
 	"github.com/celestiaorg/celestia-node/libs/utils"
@@ -19,6 +21,15 @@ type Config struct {
 	// EnableEstimatorTLS specifies whether to use TLS for the gRPC connection to the
 	// estimator service
 	EnableEstimatorTLS bool
+	// TxWorkerAccounts is used for queued submission. It defines how many accounts the
+	// TxClient uses for PayForBlob submissions.
+	//   - Value of 0 submits transactions immediately (without a submission queue).
+	//   - Value of 1 uses synchronous submission (submission queue with default
+	//     signer as author of transactions).
+	//   - Value of > 1 uses parallel submission (submission queue with several accounts
+	//     submitting blobs). Parallel submission is not guaranteed to include blobs
+	//     in the same order as they were submitted.
+	TxWorkerAccounts int
 }
 
 func DefaultConfig() Config {
@@ -26,11 +37,16 @@ func DefaultConfig() Config {
 		DefaultKeyName:     DefaultKeyName,
 		DefaultBackendName: defaultBackendName,
 		EstimatorAddress:   "",
+		TxWorkerAccounts:   0,
 	}
 }
 
 // Validate performs basic validation of the config.
 func (cfg *Config) Validate() error {
+	if cfg.TxWorkerAccounts < 0 {
+		return fmt.Errorf("worker accounts must be non-negative")
+	}
+
 	if cfg.EstimatorAddress == "" {
 		return nil
 	}
