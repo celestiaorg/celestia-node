e01d02b32c322dcb419a037cc140b5ddc611209d
vgonkivs <viacheslavgonkivskyi@gmail.com>
2025-11-20 11:25:03 +0200

    remove requestType

--- share/shwap/p2p/shrex/shrex_getter/metrics.go
+++ share/shwap/p2p/shrex/shrex_getter/metrics.go
@@ -2,7 +2,6 @@ package shrex_getter
 
 import (
 	"context"
-	"fmt"
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
@@ -10,35 +9,8 @@ import (
 	"github.com/celestiaorg/celestia-node/libs/utils"
 )
 
-type requestType int
-
-var (
-	edsRequest           requestType = 1
-	namespaceDataRequest requestType = 2
-	rowRequest           requestType = 3
-	samplesRequest       requestType = 4
-	rangeRequest         requestType = 5
-)
-
 type metrics struct {
 	requestAttempt metric.Int64Histogram
-}
-
-func (r requestType) String() string {
-	switch r {
-	case edsRequest:
-		return "edsRequest"
-	case namespaceDataRequest:
-		return "namespaceDataRequest"
-	case rowRequest:
-		return "rowRequest"
-	case samplesRequest:
-		return "samplesRequest"
-	case rangeRequest:
-		return "rangeRequest"
-	default:
-		panic(fmt.Sprintf("unknown shrex getter requestType: %v", int(r)))
-	}
 }
 
 func (sg *Getter) WithMetrics() error {
@@ -56,7 +28,7 @@ func (sg *Getter) WithMetrics() error {
 	return nil
 }
 
-func (m *metrics) recordAttempts(ctx context.Context, requestType requestType, attempt int, success bool) {
+func (m *metrics) recordAttempts(ctx context.Context, requestName string, attempt int, success bool) {
 	if m == nil {
 		return
 	}
@@ -64,7 +36,7 @@ func (m *metrics) recordAttempts(ctx context.Context, requestType requestType, a
 
 	m.requestAttempt.Record(ctx, int64(attempt),
 		metric.WithAttributes(
-			attribute.String("request_type", requestType.String()),
+			attribute.String("request_type", requestName),
 			attribute.Bool("success", success)),
 	)
 }
