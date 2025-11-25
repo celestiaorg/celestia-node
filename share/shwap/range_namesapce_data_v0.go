13e6df1b76bd9a993dead943f0607b7f982f955c
vgonkivs <viacheslavgonkivskyi@gmail.com>
2025-11-25 12:32:11 +0200

    fix findings

--- share/shwap/range_namesapce_data_v0.go
+++ share/shwap/range_namesapce_data_v0.go
@@ -3,9 +3,10 @@ package shwap
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 )
 
-// RangeNamespaceDataIDSize defines the size of the RangeNamespaceDataIDV0Size in bytes,
+// RangeNamespaceDataIDV0Size defines the size of the RangeNamespaceDataIDV0Size in bytes,
 // combining EdsIDSize size and 4 additional bytes
 // for the start and end ODS indexes of share of the range.
 const RangeNamespaceDataIDV0Size = EdsIDSize + 4
@@ -25,11 +26,11 @@ func NewRangeNamespaceDataIDV0(
 	return RangeNamespaceDataIDV0{RangeNamespaceDataID: rngData}, nil
 }
 
-// RangeNamespaceDataIDFromBinary deserializes a RangeNamespaceDataID from its binary form.
+// RangeNamespaceDataIDV0FromBinary deserializes a RangeNamespaceDataIDV0 from its binary form.
 func RangeNamespaceDataIDV0FromBinary(data []byte) (RangeNamespaceDataIDV0, error) {
-	if len(data) != RangeNamespaceDataIDSize {
+	if len(data) != RangeNamespaceDataIDV0Size {
 		return RangeNamespaceDataIDV0{}, fmt.Errorf(
-			"invalid RangeNamespaceDataID data length: expected %d, got %d", RangeNamespaceDataIDSize, len(data),
+			"invalid RangeNamespaceDataID data length: expected %d, got %d", RangeNamespaceDataIDV0Size, len(data),
 		)
 	}
 
@@ -50,6 +51,32 @@ func RangeNamespaceDataIDV0FromBinary(data []byte) (RangeNamespaceDataIDV0, erro
 func (rngid RangeNamespaceDataIDV0) MarshalBinary() ([]byte, error) {
 	data := make([]byte, 0, RangeNamespaceDataIDV0Size)
 	return rngid.appendTo(data)
+}
+
+// ReadFrom reads the binary form of RangeNamespaceDataIDV0 from the provided reader.
+func (rngid *RangeNamespaceDataIDV0) ReadFrom(r io.Reader) (int64, error) {
+	data := make([]byte, RangeNamespaceDataIDV0Size)
+	n, err := io.ReadFull(r, data)
+	if err != nil {
+		return int64(n), err
+	}
+
+	id, err := RangeNamespaceDataIDV0FromBinary(data)
+	if err != nil {
+		return int64(n), fmt.Errorf("RangeNamespaceDataIDV0FromBinary: %w", err)
+	}
+	*rngid = id
+	return int64(n), nil
+}
+
+// WriteTo writes the binary form of RangeNamespaceDataIDV0 to the provided writer.
+func (rngid RangeNamespaceDataIDV0) WriteTo(w io.Writer) (int64, error) {
+	data, err := rngid.MarshalBinary()
+	if err != nil {
+		return 0, err
+	}
+	n, err := w.Write(data)
+	return int64(n), err
 }
 
 // appendTo helps in constructing the binary representation of RangeNamespaceDataIDV0
