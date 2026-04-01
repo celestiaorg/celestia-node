8af2c3c2c548dbc1fa2c0d8fdc1c23c96da49ce5
Nina Barbakadze <barbakadzeninaa@gmail.com>
2026-04-01 14:27:49 +0200

    chore: bump celestia-app to v8.0.0-rc0 2nd try

--- state/core_access.go
+++ state/core_access.go
@@ -31,11 +31,11 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/celestiaorg/celestia-app/v7/app"
-	"github.com/celestiaorg/celestia-app/v7/app/encoding"
-	apperrors "github.com/celestiaorg/celestia-app/v7/app/errors"
-	"github.com/celestiaorg/celestia-app/v7/pkg/appconsts"
-	"github.com/celestiaorg/celestia-app/v7/pkg/user"
+	"github.com/celestiaorg/celestia-app/v8/app"
+	"github.com/celestiaorg/celestia-app/v8/app/encoding"
+	apperrors "github.com/celestiaorg/celestia-app/v8/app/errors"
+	"github.com/celestiaorg/celestia-app/v8/pkg/appconsts"
+	"github.com/celestiaorg/celestia-app/v8/pkg/user"
 	libhead "github.com/celestiaorg/go-header"
 	libshare "github.com/celestiaorg/go-square/v3/share"
 
--- state/core_access_test.go
+++ state/core_access_test.go
@@ -19,8 +19,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/celestiaorg/celestia-app/v7/test/util/testnode"
-	apptypes "github.com/celestiaorg/celestia-app/v7/x/blob/types"
+	"github.com/celestiaorg/celestia-app/v8/test/util/testnode"
+	apptypes "github.com/celestiaorg/celestia-app/v8/x/blob/types"
 	libshare "github.com/celestiaorg/go-square/v3/share"
 )
 
