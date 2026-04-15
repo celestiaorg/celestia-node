98398b2b851fdab58c5a86fe5d0c64fa7f5006ba
vgonkivs <viacheslavgonkivskyi@gmail.com>
2026-04-15 16:36:34 +0300

    rework blobstream to request from getter instead of a service

--- nodebuilder/blobstream/module.go
+++ nodebuilder/blobstream/module.go
@@ -2,10 +2,17 @@ package blobstream
 
 import (
 	"go.uber.org/fx"
+
+	libhead "github.com/celestiaorg/go-header"
+
+	"github.com/celestiaorg/celestia-node/header"
 )
 
 func ConstructModule() fx.Option {
 	return fx.Module("blobstream",
+		fx.Provide(func(store libhead.Store[*header.ExtendedHeader]) libhead.Getter[*header.ExtendedHeader] {
+			return store
+		}),
 		fx.Provide(NewService),
 		fx.Provide(func(serv *Service) Module {
 			return serv
